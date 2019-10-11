a61edfa9544c43f3dc35779cf2c2b1d31e21dc1b
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-10-11 12:33:48 +0200

    [TASK] Refactor removed methods from GeneralUtility

--- stubs/Core/TypoScript/TypoScriptService.php
+++ stubs/Core/TypoScript/TypoScriptService.php
@@ -1,11 +1,9 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
 
 namespace TYPO3\CMS\Core\TypoScript;
 
-
 final class TypoScriptService
 {
-
 }
--- stubs/Extbase/Service/TypoScriptService.php
+++ stubs/Extbase/Service/TypoScriptService.php
@@ -1,10 +1,9 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
 
 namespace TYPO3\CMS\Extbase\Service;
 
 final class TypoScriptService
 {
-
 }
