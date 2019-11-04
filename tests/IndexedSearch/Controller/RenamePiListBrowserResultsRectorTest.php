8bd408fdfa78475c9c07e882dc9f18b22e446080
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-11-04 22:13:28 +0100

    [TASK] Rename pi_list_browseresults to renderPagination
    - Resolves: #377

---
+++ tests/IndexedSearch/Controller/RenamePiListBrowserResultsRectorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ssch\TYPO3Rector\Tests\IndexedSearch\Controller;
+
+use Iterator;
+use Ssch\TYPO3Rector\IndexedSearch\Controller\RenamePiListBrowserResultsRector;
+use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+
+class RenamePiListBrowserResultsRectorTest extends AbstractRectorWithConfigTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     *
+     * @param string $file
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        yield [__DIR__ . '/Fixtures/rename_pi_list_browser_results_method.php.inc'];
+    }
+
+    protected function getRectorsWithConfiguration(): array
+    {
+        return [
+            RenamePiListBrowserResultsRector::class => [],
+        ];
+    }
+}
