02de039bff5ddeb2b844a646ed61e3b93e2bfacd
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-11-28 22:53:41 +0100

    [TASK] Rename method getUsedLanguagesInPageAndColumn to getUsedLanguagesInPage

---
+++ stubs/Backend/Controller/Page/LocalizationController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Backend\Controller\Page;
+
+final class LocalizationController
+{
+    public function getUsedLanguagesInPageAndColumn(): void
+    {
+    }
+}
