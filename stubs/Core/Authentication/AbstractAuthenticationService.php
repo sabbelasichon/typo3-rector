88575b31585bf1e57794ffcb9d0779a2c9a629a0
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-01-31 21:46:36 +0100

    [TASK] Change namespace for old extension sv
    - Resolves: #931

---
+++ stubs/Core/Authentication/AbstractAuthenticationService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Core\Authentication;
+
+if (class_exists(AbstractAuthenticationService::class)) {
+    return;
+}
+
+class AbstractAuthenticationService
+{
+}
