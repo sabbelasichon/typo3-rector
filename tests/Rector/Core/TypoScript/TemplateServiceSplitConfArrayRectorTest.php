fd608edf922feab8c7180b9d28b7a858d8a4189a
Michiel Roos <michiel@michielroos.com>
2020-01-31 17:12:02 +0100

    issue-778

---
+++ tests/Rector/Core/TypoScript/TemplateServiceSplitConfArrayRectorTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Ssch\TYPO3Rector\Tests\Core\TypoScript;
+
+use Iterator;
+use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+
+class TemplateServiceSplitConfArrayRectorTest extends AbstractRectorWithConfigTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     *
+     * @param string $file
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/template_service_split_conf_array.php.inc'];
+    }
+}
