8966d1b55f50bb33ae620ae759b5251d6d20d70f
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-02-01 21:55:21 +0100

    [TASK] Add new method getControllerConfiguration
    - We add this rector for version 9.5 alreay. Otherwise we would get a fatal error in version 10
    
    Resolves: #1222

---
+++ tests/Rector/Extbase/ConfigurationManagerAddControllerConfigurationMethodRectorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ssch\TYPO3Rector\Tests\Rector\Extbase;
+
+use Iterator;
+use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+
+class ConfigurationManagerAddControllerConfigurationMethodRectorTest extends AbstractRectorWithConfigTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     *
+     * @param string $file
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/configuration_manager_add_controller_configuration_method.php.inc'];
+    }
+}
