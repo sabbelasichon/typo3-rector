193650dca36ffce68e2d4edcb1b06c5b99db495b
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-02-17 12:11:42 +0100

    [TASK] Use ActionController instead of AbstractController
    - Resolves: #1372

---
+++ src/Rector/Extbase/UseActionControllerRector.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\Extbase;
+
+use PhpParser\Node;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\Class_;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+use TYPO3\CMS\Extbase\Mvc\Controller\AbstractController;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+/**
+ * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.2/Deprecation-89554-DeprecateTYPO3CMSExtbaseMvcControllerAbstractController.html
+ */
+final class UseActionControllerRector extends AbstractRector
+{
+    /**
+     * @inheritDoc
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Node|Class_ $node
+     *
+     * @return Node|null
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (null === $node->extends) {
+            return null;
+        }
+
+        $parentClassName = $node->getAttribute(AttributeKey::PARENT_CLASS_NAME);
+        if (AbstractController::class !== $parentClassName) {
+            return null;
+        }
+
+        /** @var string|null $className */
+        $className = $node->getAttribute(AttributeKey::CLASS_NAME);
+        if (null === $className) {
+            return null;
+        }
+
+        $node->extends = new FullyQualified(ActionController::class);
+
+        return $node;
+    }
+
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Use ActionController class instead of AbstractController if used', [
+            new CodeSample(
+                <<<'PHP'
+class MyController extends AbstractController
+{
+}
+PHP
+                ,
+                <<<'PHP'
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MyController extends ActionController
+{
+}
+PHP
+            ),
+        ]);
+    }
+}
---
+++ tests/Rector/Extbase/UseActionControllerRectorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ssch\TYPO3Rector\Tests\Rector\Extbase;
+
+use Iterator;
+use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+
+final class UseActionControllerRectorTest extends AbstractRectorWithConfigTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     *
+     * @param string $file
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/use_action_controller_class.php.inc'];
+    }
+}
