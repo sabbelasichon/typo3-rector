ae2985f36298fcea6cc5012997965bdc771d3a94
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-02-25 21:42:01 +0100

    [TASK] Inject EnvironmentService in subclass of Response
    - Resolves: #1303

---
+++ tests/Rector/Extbase/InjectEnvironmentServiceIfNeededInResponseRectorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ssch\TYPO3Rector\Tests\Rector\Extbase;
+
+use Iterator;
+use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+
+class InjectEnvironmentServiceIfNeededInResponseRectorTest extends AbstractRectorWithConfigTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/inject_environment_service_if_needed.php.inc'];
+    }
+}
