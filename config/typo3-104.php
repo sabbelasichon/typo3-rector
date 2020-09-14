ec7b4052475cee0b90b5cdebe80e8ecd9d4101e5
Tymoteusz Motylewski <t.motylewski@gmail.com>
2020-09-14 17:58:11 +0200

    Migrate rector to RenameStaticMethodRector

--- config/typo3-104.php
+++ config/typo3-104.php
@@ -2,14 +2,19 @@
 
 declare(strict_types=1);
 
-use Ssch\TYPO3Rector\Rector\Core\Utility\MoveIsRunningOnCgiServerApiToEnvironmentApiRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Renaming\Rector\StaticCall\RenameStaticMethodRector;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/services.php');
 
     $services = $containerConfigurator->services();
 
-    $services->set(MoveIsRunningOnCgiServerApiToEnvironmentApiRector::class);
+    $services->set(RenameStaticMethodRector::class)
+        ->call('configure', [[
+            RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => [
+                new Rector\Renaming\ValueObject\RenameStaticMethod('TYPO3\CMS\Core\Utility\GeneralUtility', 'isRunningOnCgiServerApi', 'TYPO3\CMS\Core\Core\Environment', 'isRunningOnCgiServer')]
+        ]]);
+
 
 };
