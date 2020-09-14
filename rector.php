b7e9d7ee5aa97f901d227fa71d10dd4bdb7fefd4
sabbelasichon <sabbelasichon@users.noreply.github.com>
2020-09-14 07:58:41 +0000

    Apply coding standard changes

--- rector.php
+++ rector.php
@@ -10,7 +10,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
     $parameters->set(Option::SETS, [
-        SetList::PRIVATIZATION, SetList::SOLID, SetList::DEAD_CODE, SetList::CODING_STYLE, SetList::CODE_QUALITY]
+        SetList::PRIVATIZATION, SetList::SOLID, SetList::DEAD_CODE, SetList::CODING_STYLE, SetList::CODE_QUALITY, ]
     );
 
     $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
