34e8de632d7bd8be468d112a4489633544fb1af6
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-09-28 16:44:52 +0200

    [TASK] Simplify dependency injection for database refactorings

--- src/Helper/Database/Refactorings/DatabaseConnectionToDbalRefactoring.php
+++ src/Helper/Database/Refactorings/DatabaseConnectionToDbalRefactoring.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ssch\TYPO3Rector\Rector\Core\Database\Refactorings;
+namespace Ssch\TYPO3Rector\Helper\Database\Refactorings;
 
 use PhpParser\Node\Expr\MethodCall;
 
