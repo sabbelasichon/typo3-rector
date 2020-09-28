6ef0b891ecc19e47f2d665f5f4498defe5522aaf
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-09-28 17:34:43 +0200

    [TASK] Refactor class RteHtmlParser
    - Resolves: #471

---
+++ stubs/Core/Html/RteHtmlParser.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Core\Html;
+
+if (class_exists(RteHtmlParser::class)) {
+    return;
+}
+
+
+final class RteHtmlParser
+{
+    public function  HTMLcleaner_db($content): string
+    {
+        return '';
+    }
+
+    public function getKeepTags($direction = 'rte'): array
+    {
+        return [];
+    }
+
+    public function getUrl($url): string
+    {
+        return '';
+    }
+
+    public function siteUrl(): string
+    {
+        return '';
+    }
+}
---
+++ tests/Rector/Core/Html/RteHtmlParserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\Core\Html;
+
+use Iterator;
+use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class RteHtmlParserTest extends AbstractRectorWithConfigTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+}
