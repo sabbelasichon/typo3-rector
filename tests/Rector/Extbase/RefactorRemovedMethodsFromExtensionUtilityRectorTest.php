a8ac2e3bca34f1a21d1d6b1ff8422df19c71b3df
Maik Peuser <maik.peuser@verdure.de>
2020-10-05 23:35:11 +0200

    [TASK] rector complains about unecessary fqdns

--- tests/Rector/Extbase/RefactorRemovedMethodsFromExtensionUtilityRectorTest.php
+++ tests/Rector/Extbase/RefactorRemovedMethodsFromExtensionUtilityRectorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ssch\TYPO3Rector\Tests\Rector\Extbase;
 
+use Iterator;
 use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
@@ -12,12 +13,12 @@ final class RefactorRemovedMethodsFromExtensionUtilityRectorTest extends Abstrac
     /**
      * @dataProvider provideData()
      */
-    public function test(\Symplify\SmartFileSystem\SmartFileInfo $fileInfo): void
+    public function test(SmartFileInfo $fileInfo): void
     {
         $this->doTestFileInfo($fileInfo);
     }
 
-    public function provideData(): \Iterator
+    public function provideData(): Iterator
     {
         return yield [new SmartFileInfo(__DIR__ . '/Fixture/removed_methods_from_extension_utility.php.inc')];
     }
