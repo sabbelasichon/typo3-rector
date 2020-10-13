fd2bbff40494781692334e2c0480f7bfe2ac7bae
Maik Peuser <maik.peuser@verdure.de>
2020-10-13 22:58:24 +0200

    [TASK] refactor get/set forcetemplate in tsfe

--- stubs/Core/TypoScript/TemplateService.php
+++ stubs/Core/TypoScript/TemplateService.php
@@ -4,4 +4,9 @@ namespace TYPO3\CMS\Core\TypoScript;
 
 class TemplateService
 {
+    /**
+     * Passed to TypoScript template class and tells it to force template rendering
+     * @var bool
+     */
+    public $forceTemplateParsing = false;
 }
