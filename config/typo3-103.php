ab28538b435897b0e3e4ecefe26dce715e201e26
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-10-16 20:41:33 +0200

    [TASK] Change Request and Response classes from extbase (#1518)
    Resolves: #1421

--- config/typo3-103.php
+++ config/typo3-103.php
@@ -3,10 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Renaming\ValueObject\MethodCallRename;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
 use Ssch\TYPO3Rector\Rector\Core\Resource\SubstituteResourceFactoryRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Extbase\Mvc\Request;
+use TYPO3\CMS\Extbase\Mvc\Response;
+use TYPO3\CMS\Extbase\Mvc\Web\Request as WebRequest;
+use TYPO3\CMS\Extbase\Mvc\Web\Response as WebResponse;
 use TYPO3\CMS\Linkvalidator\Repository\BrokenLinkRepository;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -15,15 +20,27 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(RenameMethodRector::class)
-        ->call('configure', [[
-            RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
-                new MethodCallRename(
-                   BrokenLinkRepository::class,
-                   'getNumberOfBrokenLinks',
-                   'isLinkTargetBrokenLink'
-               ),
-            ]),
-        ]]);
+        ->call('configure', [
+            [
+                RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+                    new MethodCallRename(
+                        BrokenLinkRepository::class,
+                        'getNumberOfBrokenLinks',
+                        'isLinkTargetBrokenLink'
+                    ),
+                ]),
+            ],
+        ]);
 
     $services->set(SubstituteResourceFactoryRector::class);
+
+    $services->set(RenameClassRector::class)
+        ->call('configure', [
+            [
+                RenameClassRector::OLD_TO_NEW_CLASSES => [
+                    WebRequest::class => Request::class,
+                    WebResponse::class => Response::class,
+                ],
+            ],
+        ]);
 };
