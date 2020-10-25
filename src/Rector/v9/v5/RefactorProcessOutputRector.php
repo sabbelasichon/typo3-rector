a5f22dba28f9d6fdaa1ed72f760ae98629ec8554
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-10-25 20:40:10 +0100

    [TASK] Refactor method processOutput of TSFE (#1526)
    Resolves: #1167

---
+++ src/Rector/v9/v5/RefactorProcessOutputRector.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v9\v5;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+use Ssch\TYPO3Rector\Helper\Typo3NodeResolver;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+
+/**
+ * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.5/Deprecation-86486-TypoScriptFrontendController-processOutput.html
+ */
+final class RefactorProcessOutputRector extends AbstractRector
+{
+    /**
+     * @var Typo3NodeResolver
+     */
+    private $typo3NodeResolver;
+
+    public function __construct(Typo3NodeResolver $typo3NodeResolver)
+    {
+        $this->typo3NodeResolver = $typo3NodeResolver;
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if ($this->typo3NodeResolver->isMethodCallOnGlobals(
+            $node,
+            'processOutput',
+            Typo3NodeResolver::TYPO_SCRIPT_FRONTEND_CONTROLLER
+        )) {
+            $this->refactorToNewMethodCalls($node);
+
+            return null;
+        }
+
+        if (! $this->isMethodStaticCallOrClassMethodObjectType($node, TypoScriptFrontendController::class)) {
+            return null;
+        }
+
+        if (! $this->isName($node->name, 'processOutput')) {
+            return null;
+        }
+
+        $this->refactorToNewMethodCalls($node);
+
+        return null;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition(
+            'TypoScriptFrontendController->processOutput() to TypoScriptFrontendController->applyHttpHeadersToResponse() and TypoScriptFrontendController->processContentForOutput()',
+            [
+                new CodeSample(
+                    <<<'PHP'
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+
+$tsfe = GeneralUtility::makeInstance(TypoScriptFrontendController::class);
+$tsfe->processOutput();
+PHP
+                    ,
+                    <<<'PHP'
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+
+$tsfe = GeneralUtility::makeInstance(TypoScriptFrontendController::class);
+$tsfe->applyHttpHeadersToResponse();
+$tsfe->processContentForOutput();
+PHP
+                ),
+            ]
+        );
+    }
+
+    private function refactorToNewMethodCalls(MethodCall $node): void
+    {
+        $node->name = new Identifier('applyHttpHeadersToResponse');
+        $newNode = $this->createMethodCall($node->var, 'processContentForOutput');
+        $this->addNodeAfterNode($newNode, $node);
+    }
+}
---
+++ tests/Rector/v9/v5/ProcessOutput/RefactorProcessOutputRectorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v9\v5\ProcessOutput;
+
+use Iterator;
+use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class RefactorProcessOutputRectorTest extends AbstractRectorWithConfigTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+}
