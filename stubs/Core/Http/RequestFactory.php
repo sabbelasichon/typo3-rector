3889e4aa5888be196506695951da3ff819fbba5a
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-10-29 12:17:40 +0100

    [TASK] Refactor GeneralUtility::getUrl if possible (#1533)
    Resolves: #1455

---
+++ stubs/Core/Http/RequestFactory.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Core\Http;
+
+if (class_exists(RequestFactory::class)) {
+    return;
+}
+
+final class RequestFactory
+{
+    public function request(string $uri, string $method = 'GET', array $options = []): void
+    {
+    }
+}
