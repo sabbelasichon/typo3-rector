dd71ee807322708059b025b52a3597a32e4d384a
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-01 21:24:53 +0100

    [TASK] Create individual tests for annotations (#1547)
    Resolves: #1546

--- tests/Rector/Annotation/Source/InjectionClass.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Ssch\TYPO3Rector\Tests\Rector\Annotation\Source;
-
-final class InjectionClass
-{
-}
---
+++ tests/Rector/v9/v0/ReplaceAnnotation/ReplaceAnnotationRectorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v9\v0\ReplaceAnnotation;
+
+use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
+use Ssch\TYPO3Rector\Rector\v9\v0\ReplaceAnnotationRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ReplaceAnnotationRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorsWithConfiguration(): array
+    {
+        return [
+            ReplaceAnnotationRector::class => [
+                ReplaceAnnotationRector::OLD_TO_NEW_ANNOTATIONS => [
+                    'lazy' => 'TYPO3\CMS\Extbase\Annotation\ORM\Lazy',
+                    'cascade' => 'TYPO3\CMS\Extbase\Annotation\ORM\Cascade("remove")',
+                    'transient' => 'TYPO3\CMS\Extbase\Annotation\ORM\Transient',
+                ],
+            ],
+        ];
+    }
+}
