1619f44a7a9c2e2c45a07244d0e52113c1e26363
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-01 21:20:04 +0100

    [TASK] Create individual tests for annotations
    Resolves: #1546

---
+++ tests/Rector/v9/v0/InjectAnnotation/InjectAnnotationRectorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v9\v0\InjectAnnotation;
+
+use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
+use Ssch\TYPO3Rector\Rector\v9\v0\InjectAnnotationRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class InjectAnnotationRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return InjectAnnotationRector::class;
+    }
+}
--- tests/Rector/v9/v3/ValidateAnnotation/ValidateAnnotationRectorTest.php
+++ tests/Rector/v9/v3/ValidateAnnotation/ValidateAnnotationRectorTest.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Ssch\TYPO3Rector\Tests\Rector\Annotation;
+namespace Ssch\TYPO3Rector\Tests\Rector\v9\v3\ValidateAnnotation;
 
 use Iterator;
-use Ssch\TYPO3Rector\Tests\AbstractRectorWithConfigTestCase;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
+use Ssch\TYPO3Rector\Rector\v9\v3\ValidateAnnotationRector;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
-final class AnnotationTest extends AbstractRectorWithConfigTestCase
+final class ValidateAnnotationRectorTest extends AbstractRectorTestCase
 {
     /**
      * @dataProvider provideDataForTest()
@@ -21,5 +22,10 @@ final class AnnotationTest extends AbstractRectorWithConfigTestCase
     public function provideDataForTest(): Iterator
     {
         return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return ValidateAnnotationRector::class;
     }
 }
