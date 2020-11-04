575fea5dff9e1b16e426ee779cd71e54deef65ca
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-04 19:17:39 +0100

    Create phar file for typo3-rector (#1548)
    [TASK] Add github workflow to create phar file

--- rector.php
+++ rector.php
@@ -13,13 +13,18 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
     $parameters->set(Option::SETS, [
-        SetList::PRIVATIZATION, SetList::SOLID, SetList::DEAD_CODE, SetList::CODING_STYLE, SetList::CODE_QUALITY, ]
+        SetList::PRIVATIZATION,
+        SetList::SOLID,
+        SetList::DEAD_CODE,
+        SetList::CODING_STYLE,
+        SetList::CODE_QUALITY,
+    ]
     );
     $services = $containerConfigurator->services();
 
     $services->set(AddCodeCoverageIgnoreToMethodRectorDefinitionRector::class);
 
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $parameters->set(Option::PATHS, [__DIR__ . '/scoper.inc.php', __DIR__ . '/src', __DIR__ . '/tests']);
     # so Rector code is still PHP 7.2 compatible
     $parameters->set(Option::PHP_VERSION_FEATURES, '7.2');
 };
