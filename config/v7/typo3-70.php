c5216111147ac5d63f7643446b2e2d1f1b9c8dd1
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-06 21:15:05 +0100

    [TASK] Use DocumentTemplate for deprecated template classes (#1566)
    Resolves: #16, #19

---
+++ config/v7/typo3-70.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Renaming\Rector\Name\RenameClassRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Backend\Template\BigDocumentTemplate;
+use TYPO3\CMS\Backend\Template\DocumentTemplate;
+use TYPO3\CMS\Backend\Template\MediumDocumentTemplate;
+use TYPO3\CMS\Backend\Template\SmallDocumentTemplate;
+use TYPO3\CMS\Backend\Template\StandardDocumentTemplate;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../services.php');
+
+    $services = $containerConfigurator->services();
+
+    $services->set(RenameClassRector::class)
+        ->call('configure', [[
+            RenameClassRector::OLD_TO_NEW_CLASSES => [
+                MediumDocumentTemplate::class => DocumentTemplate::class,
+                SmallDocumentTemplate::class => DocumentTemplate::class,
+                StandardDocumentTemplate::class => DocumentTemplate::class,
+                BigDocumentTemplate::class => DocumentTemplate::class,
+            ],
+             ]]);
+};
---
+++ stubs/Backend/Template/SmallDocumentTemplate.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Backend\Template;
+
+if (class_exists(SmallDocumentTemplate::class)) {
+    return;
+}
+
+final class SmallDocumentTemplate
+{
+}
