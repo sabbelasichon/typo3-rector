fdcc8ec6cd1530d73bd44c7228e2acd4457d7182
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-08 21:55:18 +0100

    [TASK] Method issueCommand to getLinkToDataHandlerAction (#1575)
    Resolves: #345

--- config/v7/typo3-76.php
+++ config/v7/typo3-76.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
+use function Rector\SymfonyPhpConfig\inline_value_objects;
+use Rector\Transform\Rector\MethodCall\MethodCallToStaticCallRector;
+use Rector\Transform\ValueObject\MethodCallToStaticCall;
 use Ssch\TYPO3Rector\Rector\v7\v6\RenamePiListBrowserResultsRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Backend\Template\DocumentTemplate;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
@@ -11,4 +16,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(RenamePiListBrowserResultsRector::class);
+
+    $services->set(MethodCallToStaticCallRector::class)
+        ->call('configure', [[
+            MethodCallToStaticCallRector::METHOD_CALLS_TO_STATIC_CALLS => inline_value_objects([
+                new MethodCallToStaticCall(
+                   DocumentTemplate::class,
+                   'issueCommand',
+                   BackendUtility::class,
+                   'getLinkToDataHandlerAction'
+                    ),
+            ]),
+        ]]);
 };
