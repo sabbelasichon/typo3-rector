b64833e136d97988d05bbfa76c9feca70370e788
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-08 22:07:44 +0100

    [TASK] Escape strings properly (#1576)
    Resolves: #358

--- config/v7/typo3-76.php
+++ config/v7/typo3-76.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstantRector;
+use Rector\Renaming\ValueObject\RenameClassConstant;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
 use Rector\Transform\Rector\MethodCall\MethodCallToStaticCallRector;
 use Rector\Transform\ValueObject\MethodCallToStaticCall;
@@ -9,6 +11,9 @@ use Ssch\TYPO3Rector\Rector\v7\v6\RenamePiListBrowserResultsRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TYPO3\CMS\Backend\Template\DocumentTemplate;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\IndexedSearch\Controller\SearchFormController;
+use TYPO3\CMS\IndexedSearch\Domain\Repository\IndexSearchRepository;
+use TYPO3\CMS\IndexedSearch\Utility\LikeWildcard;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
@@ -18,14 +23,44 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(RenamePiListBrowserResultsRector::class);
 
     $services->set(MethodCallToStaticCallRector::class)
-        ->call('configure', [[
-            MethodCallToStaticCallRector::METHOD_CALLS_TO_STATIC_CALLS => inline_value_objects([
-                new MethodCallToStaticCall(
-                   DocumentTemplate::class,
-                   'issueCommand',
-                   BackendUtility::class,
-                   'getLinkToDataHandlerAction'
+        ->call('configure', [
+            [
+                MethodCallToStaticCallRector::METHOD_CALLS_TO_STATIC_CALLS => inline_value_objects([
+                    new MethodCallToStaticCall(
+                        DocumentTemplate::class,
+                        'issueCommand',
+                        BackendUtility::class,
+                        'getLinkToDataHandlerAction'
                     ),
-            ]),
-        ]]);
+                ]),
+            ],
+        ]);
+
+    $services->set(RenameClassConstantRector::class)
+        ->call('configure', [
+            [
+                RenameClassConstantRector::CLASS_CONSTANT_RENAME => inline_value_objects([
+                    new RenameClassConstant(
+                       SearchFormController::class,
+                       'WILDCARD_LEFT',
+                       LikeWildcard::class . '::WILDCARD_LEFT'
+                        ),
+                    new RenameClassConstant(
+                       SearchFormController::class,
+                       'WILDCARD_RIGHT',
+                       LikeWildcard::class . '::WILDCARD_RIGHT'
+                        ),
+                    new RenameClassConstant(
+                       IndexSearchRepository::class,
+                       'WILDCARD_LEFT',
+                       LikeWildcard::class . '::WILDCARD_LEFT'
+                        ),
+                    new RenameClassConstant(
+                       IndexSearchRepository::class,
+                       'WILDCARD_RIGHT',
+                       LikeWildcard::class . '::WILDCARD_RIGHT'
+                        ),
+                ]),
+            ],
+        ]);
 };
---
+++ stubs/IndexedSearch/Utility/LikeWildcard.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\IndexedSearch\Utility;
+
+if (class_exists(LikeWildcard::class)) {
+    return;
+}
+
+final class LikeWildcard
+{
+    public const WILDCARD_LEFT = 'foo';
+    public const WILDCARD_RIGHT = 'foo';
+}
