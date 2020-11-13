57fb10e825ec4aac1df7e737293552c7ed94520f
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-13 15:02:54 +0100

    [TASK] Change class callee of tablesDefinitionIsBeingBuilt (#1596)
    Resolves: #1085

--- config/v9/typo3-94.php
+++ config/v9/typo3-94.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\Renaming\Rector\Name\RenameClassRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\BackendUtilityShortcutExistsRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\CallEnableFieldsFromPageRepositoryRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\ConstantToEnvironmentCallRector;
@@ -14,8 +13,8 @@ use Ssch\TYPO3Rector\Rector\v9\v4\SystemEnvironmentBuilderConstantsRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\UseContextApiForVersioningWorkspaceIdRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\UseContextApiRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\UseLanguageAspectForTsfeLanguagePropertiesRector;
+use Ssch\TYPO3Rector\Rector\v9\v4\UseSignalTablesDefinitionIsBeingBuiltSqlExpectedSchemaServiceRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use TYPO3\CMS\Saltedpasswords\Salt\Argon2iSalt;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
@@ -36,28 +35,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(SystemEnvironmentBuilderConstantsRector::class);
 
-    $services->set(RenameClassRector::class)
-        ->call('configure', [[
-            RenameClassRector::OLD_TO_NEW_CLASSES => [
-                Argon2iSalt::class => 'TYPO3\CMS\Core\Crypto\PasswordHashing\Argon2iPasswordHash',
-                'TYPO3\CMS\Saltedpasswords\Salt\BcryptSalt' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\BcryptPasswordHash',
-                'TYPO3\CMS\Saltedpasswords\Salt\BlowfishSalt' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\BlowfishPasswordHash',
-                'TYPO3\CMS\Saltedpasswords\Exception\InvalidSaltException' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\InvalidPasswordHashException',
-                'TYPO3\CMS\Saltedpasswords\Salt\Md5Salt' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\Md5PasswordHash',
-                'TYPO3\CMS\Saltedpasswords\Salt\SaltFactory' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory',
-                'TYPO3\CMS\Saltedpasswords\Salt\SaltInterface' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashInterface',
-                'TYPO3\CMS\Saltedpasswords\Salt\Pbkdf2Salt' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\Pbkdf2PasswordHash',
-                'TYPO3\CMS\Saltedpasswords\Salt\PhpassSalt' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\PhpassPasswordHash',
-                'TYPO3\CMS\Saltedpasswords\Salt\AbstractComposedSalt' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\AbstractComposedSalt',
-                'TYPO3\CMS\Saltedpasswords\Salt\ComposedSaltInterface' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\ComposedPasswordHashInterface',
-                'TYPO3\CMS\Saltedpasswords\Utility\ExensionManagerConfigurationUtility' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\ExtensionManagerConfigurationUtility',
-                'TYPO3\CMS\Saltedpasswords\SaltedPasswordService' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\SaltedPasswordService',
-                'TYPO3\CMS\Saltedpasswords\Utility\SaltedPasswordsUtility' => 'TYPO3\CMS\Core\Crypto\PasswordHashing\SaltedPasswordsUtility',
-            ],
-        ]]);
-
     $services->set(UseContextApiForVersioningWorkspaceIdRector::class);
+
     $services->set(DocumentTemplateAddStyleSheetRector::class);
+
     $services->set(UseLanguageAspectForTsfeLanguagePropertiesRector::class);
+
     $services->set(BackendUtilityShortcutExistsRector::class);
+
+    $services->set(UseSignalTablesDefinitionIsBeingBuiltSqlExpectedSchemaServiceRector::class);
 };
---
+++ stubs/Extensionmanager/Utility/InstallUtility.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Extensionmanager\Utility;
+
+if(class_exists(InstallUtility::class)) {
+    return;
+}
+
+final class InstallUtility
+{
+
+}
