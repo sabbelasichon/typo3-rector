370fa1a31da0b46f402553df445351c2bcbe4016
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-13 21:25:19 +0100

    Issues 1087, 1089 (#1598)
    * [TASK] Remove method init of GraphicalFunctions
    
    Resolves: #1087
    
    * [TASK] Remove method init of TemplateService
    
    Resolves: #1089

--- config/v9/typo3-94.php
+++ config/v9/typo3-94.php
@@ -8,6 +8,8 @@ use Ssch\TYPO3Rector\Rector\v9\v4\ConstantToEnvironmentCallRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\DocumentTemplateAddStyleSheetRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\RefactorDeprecatedConcatenateMethodsPageRendererRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\RefactorExplodeUrl2ArrayFromGeneralUtilityRector;
+use Ssch\TYPO3Rector\Rector\v9\v4\RemoveInitMethodGraphicalFunctionsRector;
+use Ssch\TYPO3Rector\Rector\v9\v4\RemoveInitMethodTemplateServiceRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\RemoveInitTemplateMethodCallRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\SystemEnvironmentBuilderConstantsRector;
 use Ssch\TYPO3Rector\Rector\v9\v4\UseContextApiForVersioningWorkspaceIdRector;
@@ -47,4 +49,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(UseSignalTablesDefinitionIsBeingBuiltSqlExpectedSchemaServiceRector::class);
 
     $services->set(UseGetMenuInsteadOfGetFirstWebPageRector::class);
+
+    $services->set(RemoveInitMethodGraphicalFunctionsRector::class);
+
+    $services->set(RemoveInitMethodTemplateServiceRector::class);
 };
--- stubs/Core/Imaging/GraphicalFunctions.php
+++ stubs/Core/Imaging/GraphicalFunctions.php
@@ -19,4 +19,9 @@ final class GraphicalFunctions
     {
         return 'foo';
     }
+
+    public function init(): void
+    {
+
+    }
 }
