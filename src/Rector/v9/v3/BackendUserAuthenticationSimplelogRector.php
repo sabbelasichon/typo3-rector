5065c6ef72423f8637463d8a1f15ce833873bac0
Guido Schmechel <guido@ayacoo.de>
2020-11-14 16:26:59 +0100

    [TASK] Fix coding guidelines

--- src/Rector/v9/v3/BackendUserAuthenticationSimplelogRector.php
+++ src/Rector/v9/v3/BackendUserAuthenticationSimplelogRector.php
@@ -4,17 +4,13 @@ declare(strict_types=1);
 
 namespace Ssch\TYPO3Rector\Rector\v9\v3;
 
-use PhpParser\BuilderHelpers;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
-use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Identifier;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\RectorDefinition\CodeSample;
 use Rector\Core\RectorDefinition\RectorDefinition;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.3/Deprecation-84981-BackendUserAuthentication-simplelogDeprecated.html
@@ -60,16 +56,19 @@ final class BackendUserAuthenticationSimplelogRector extends AbstractRector
      */
     public function getDefinition(): RectorDefinition
     {
-        return new RectorDefinition('Migrate the method BackendUserAuthentication->simplelog() to BackendUserAuthentication->writelog()', [
-            new CodeSample(<<<'PHP'
+        return new RectorDefinition(
+            'Migrate the method BackendUserAuthentication->simplelog() to BackendUserAuthentication->writelog()',
+            [
+                new CodeSample(<<<'PHP'
 $someObject = GeneralUtility::makeInstance(TYPO3\CMS\Core\Authentication\BackendUserAuthentication::class);
 $someObject->simplelog($message, $extKey, $error);
 PHP
-                , <<<'PHP'
+                    , <<<'PHP'
 $someObject = GeneralUtility::makeInstance(TYPO3\CMS\Core\Authentication\BackendUserAuthentication::class);
 $someObject->writelog(4, 0, $error, 0, ($extKey ? '[' . $extKey . '] ' : '') . $message, []);
 PHP
-            ),
-        ]);
+                ),
+            ]
+        );
     }
 }
