1ab23bef4881f8c96c0ac83694651912e116b283
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-16 21:01:10 +0100

    [TASK] Use VariableFrontend instead of StringFrontend
    Resolves: #980

--- config/v9/typo3-92.php
+++ config/v9/typo3-92.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use Rector\Renaming\Rector\Name\RenameClassRector;
 use Ssch\TYPO3Rector\Rector\v9\v2\RenameMethodCallToEnvironmentMethodCallRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Core\Cache\Frontend\StringFrontend;
+use TYPO3\CMS\Core\Cache\Frontend\VariableFrontend;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
@@ -11,4 +14,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(RenameMethodCallToEnvironmentMethodCallRector::class);
+
+    $services->set(RenameClassRector::class)
+        ->call('configure', [[
+            RenameClassRector::OLD_TO_NEW_CLASSES => [
+                StringFrontend::class => VariableFrontend::class,
+            ],
+             ]]);
 };
---
+++ stubs/Core/Cache/Frontend/VariableFrontend.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+
+namespace TYPO3\CMS\Core\Cache\Frontend;
+
+if (interface_exists(VariableFrontend::class)) {
+    return;
+}
+
+final class VariableFrontend
+{
+
+}
