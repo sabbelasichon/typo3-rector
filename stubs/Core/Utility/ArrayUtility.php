eda00ff17467b2ded6054b68efca6aa50b09caf7
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-19 23:44:27 +0100

    [TASK] Resolves methods of extbase ArrayUtility class (#1616)
    - Use the appropriate methods from ArrayUtility of core
    - Method arrayMergeRecursiveOverrule is not refactored
    
    Resolves: #631

--- stubs/Core/Utility/ArrayUtility.php
+++ stubs/Core/Utility/ArrayUtility.php
@@ -10,6 +10,30 @@ if (class_exists(ArrayUtility::class)) {
 
 final class ArrayUtility
 {
+    public static function mergeRecursiveWithOverrule(array &$original, array $overrule, $addKeys = true, $includeEmptyValues = true, $enableUnsetFeature = true): void
+    {
+
+    }
+
+    public static function getValueByPath(): void
+    {
+
+    }
+
+    public static function setValueByPath(): void
+    {
+
+    }
+
+    public static function removeByPath(): void
+    {
+
+    }
+
+    public static function sortArrayWithIntegerKeys(): void
+    {
+
+    }
     public static function inArray(array $in_array, $item): bool
     {
         return true;
