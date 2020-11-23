f7b29b9df676db17a6fa4c5a302813988ebee6dd
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-11-23 18:13:48 +0100

    [TASK] Refactor QueryView->tableWrap to <pre> (#578)
    Resolves #578

--- config/v8/typo3-83.php
+++ config/v8/typo3-83.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Ssch\TYPO3Rector\Rector\v8\v3\RefactorMethodFileContentRector;
+use Ssch\TYPO3Rector\Rector\v8\v3\RefactorQueryViewTableWrapRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -11,4 +12,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(RefactorMethodFileContentRector::class);
+
+    $services->set(RefactorQueryViewTableWrapRector::class);
 };
