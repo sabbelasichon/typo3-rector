7f2c517430b208fef83b00634656d80c91866646
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-24 00:09:56 +0100

    [TASK] Deprecated extbase code is removed
    Resolves: #58

--- config/v7/typo3-70.php
+++ config/v7/typo3-70.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\Rector\Name\RenameClassRector;
 use Rector\Renaming\Rector\StaticCall\RenameStaticMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
 use Rector\Renaming\ValueObject\RenameStaticMethod;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
 use Ssch\TYPO3Rector\Rector\v7\v0\RemoveMethodCallConnectDbRector;
@@ -17,6 +19,8 @@ use TYPO3\CMS\Backend\Template\SmallDocumentTemplate;
 use TYPO3\CMS\Backend\Template\StandardDocumentTemplate;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
@@ -26,26 +30,41 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(RemoveMethodCallConnectDbRector::class);
     $services->set(RemoveMethodCallLoadTcaRector::class);
     $services->set(RenameClassRector::class)
-        ->call('configure', [[
-            RenameClassRector::OLD_TO_NEW_CLASSES => [
-                MediumDocumentTemplate::class => DocumentTemplate::class,
-                SmallDocumentTemplate::class => DocumentTemplate::class,
-                StandardDocumentTemplate::class => DocumentTemplate::class,
-                BigDocumentTemplate::class => DocumentTemplate::class,
-            ],
-             ]]);
+        ->call('configure', [
+            [
+                RenameClassRector::OLD_TO_NEW_CLASSES => [
+                    MediumDocumentTemplate::class => DocumentTemplate::class,
+                    SmallDocumentTemplate::class => DocumentTemplate::class,
+                    StandardDocumentTemplate::class => DocumentTemplate::class,
+                    BigDocumentTemplate::class => DocumentTemplate::class,
+                ],
+                 ],
+             ]);
 
     $services->set(RenameStaticMethodRector::class)
-        ->call('configure', [[
-            RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => inline_value_objects([
-                new RenameStaticMethod(
-                   GeneralUtility::class,
-                   'int_from_ver',
-                   VersionNumberUtility::class,
-                   'convertVersionNumberToInteger'
+        ->call('configure', [
+            [
+                RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => inline_value_objects([
+                    new RenameStaticMethod(
+                        GeneralUtility::class,
+                        'int_from_ver',
+                        VersionNumberUtility::class,
+                        'convertVersionNumberToInteger'
                     ),
-            ]),
-        ]]);
+                ]),
+            ],
+        ]);
 
     $services->set(TypeHandlingServiceToTypeHandlingUtilityRector::class);
+
+    $services->set(RenameMethodRector::class)
+        ->call('configure', [
+            [
+                RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+                    new MethodCallRename(Typo3QuerySettings::class, 'setSysLanguageUid', 'setLanguageUid'),
+                    new MethodCallRename(Typo3QuerySettings::class, 'getSysLanguageUid', 'getLanguageUid'),
+                    new MethodCallRename(ObjectManager::class, 'create', 'get'),
+                ]),
+            ],
+        ]);
 };
---
+++ stubs/Extbase/Persistence/Generic/Typo3QuerySettings.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Extbase\Persistence\Generic;
+
+if (class_exists(Typo3QuerySettings::class)) {
+    return;
+}
+
+final class Typo3QuerySettings
+{
+
+}
