8faafdeaa86a8ec1ee5ef463333e7034fa2443bf
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-11-23 20:12:35 +0100

    [TASK] DataHandler Various methods and method arguments (#762) (#1637)
    * [TASK] Concat the PATH_site with $firstArgument

---
+++ src/Rector/v8/v7/DataHandlerVariousMethodsAndMethodArgumentsRector.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v8\v7;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\BinaryOp\Concat;
+use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Name;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+
+/**
+ * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/8.7/Deprecation-80513-DataHandlerVariousMethodsAndMethodArguments.html
+ */
+final class DataHandlerVariousMethodsAndMethodArgumentsRector extends AbstractRector
+{
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param StaticCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isMethodStaticCallOrClassMethodObjectType($node, DataHandler::class)) {
+            return null;
+        }
+
+        if ($this->isName($node->name, 'destPathFromUploadFolder')) {
+
+            /** @var Arg[] $args */
+            $args = $node->args;
+            $firstArgument = array_shift($args);
+
+            if (null === $firstArgument) {
+                return null;
+            }
+
+            return new Concat(new ConstFetch(new Name('PATH_site')), $firstArgument->value);
+        }
+
+        if ($this->isName($node->name, 'extFileFunctions') && 4 === count($node->args)) {
+            $this->removeNode($node->args[3]);
+            return $node;
+        }
+
+        return null;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition(
+            'Remove CharsetConvertParameters',
+            [
+                new CodeSample(<<<'PHP'
+$dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+$dest = $dataHandler->destPathFromUploadFolder('uploadFolder');
+$dataHandler->extFileFunctions('table', 'field', 'theField', 'deleteAll');
+PHP
+                    , <<<'PHP'
+$dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+$dest = PATH_site . 'uploadFolder';
+$dataHandler->extFileFunctions('table', 'field', 'theField');
+PHP
+                ),
+            ]
+        );
+    }
+}
