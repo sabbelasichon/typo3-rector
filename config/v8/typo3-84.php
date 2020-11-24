dbc72750001b54b4a553475da25d940e60b5b59e
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-11-24 20:52:35 +0100

    [TASK] Replace JStop with addCssFiles in FormResultCompiler (#624) (#1650)
    Resolves #624

---
+++ config/v8/typo3-84.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Backend\Routing\FormResultCompiler;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../services.php');
+    $services = $containerConfigurator->services();
+
+    // @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/8.4/Deprecation-75363-DeprecateFormResultCompilerJStop.html
+    $services->set(RenameMethodRector::class)
+        ->call('configure', [[
+            RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+                new MethodCallRename(FormResultCompiler::class, 'JStop', 'addCssFiles'),
+            ]),
+        ]]);
+};
---
+++ stubs/Backend/Form/FormResultCompiler.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Backend\Routing;
+
+if (class_exists(FormResultCompiler::class)) {
+    return;
+}
+
+final class FormResultCompiler
+{
+
+}
