08d457b589fd87cc4cf92fcd391c33cb03e21f40
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-24 22:55:14 +0100

    [HOTFIX] Missing inline_value_objects in configuration

--- config/v8/typo3-84.php
+++ config/v8/typo3-84.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
+use function Rector\SymfonyPhpConfig\inline_value_objects;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TYPO3\CMS\Backend\Routing\FormResultCompiler;
 
