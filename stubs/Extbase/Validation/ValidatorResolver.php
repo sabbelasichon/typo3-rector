38bee5f936db3a61f2d94ae37c31f57daabf350e
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-25 00:55:57 +0100

    [TASK] Add ValidatorResolverDynamicReturnTypeExtension

---
+++ stubs/Extbase/Validation/ValidatorResolver.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Extbase\Validation\ValidatorResolver;
+
+if (class_exists(ValidatorResolver::class)) {
+    return;
+}
+
+final class ValidatorResolver
+{
+
+}
---
+++ utils/phpstan/src/Type/ValidatorResolverDynamicReturnTypeExtension.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Ssch\TYPO3Rector\PHPStan\Type;
+
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Name;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use TYPO3\CMS\Extbase\Validation\ValidatorResolver;
+
+final class ValidatorResolverDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return ValidatorResolver::class;
+    }
+
+    public function isMethodSupported(
+        MethodReflection $methodReflection
+    ): bool
+    {
+        return $methodReflection->getName() === 'createValidator';
+    }
+
+    public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type
+    {
+        $arg = $methodCall->args[0]->value;
+        if (!($arg instanceof \PhpParser\Node\Expr\ClassConstFetch)) {
+            return ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+        }
+        /** @var Name $class */
+        $class = $arg->class;
+
+        return TypeCombinator::addNull(new ObjectType((string) $class));
+    }
+}
