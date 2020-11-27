f12115c9205188955c891ed6d61df1636deca0f4
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-11-27 09:25:19 +0100

    [TASK] Add custom NameImportingPostRector
    Resolves: #1592

---
+++ src/Configuration/Typo3Option.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Configuration;
+
+final class Typo3Option
+{
+    /**
+     * @var string
+     */
+    public const AUTO_IMPORT_NAMES = 'typo3_auto_import_names';
+}
