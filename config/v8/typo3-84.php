6eb741ab993677d4c2e5b2d873633d564dbbb4ff
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-11-29 12:54:22 +0100

    [TASK] Change RTEHtmlArea Spellchecker entrypoint (#622)
    Resovles: #622

--- config/v8/typo3-84.php
+++ config/v8/typo3-84.php
@@ -7,6 +7,7 @@ use Rector\Renaming\ValueObject\MethodCallRename;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TYPO3\CMS\Backend\Routing\FormResultCompiler;
+use TYPO3\CMS\Saltedpasswords\Salt\SpellCheckingController;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
@@ -17,6 +18,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->call('configure', [[
             RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
                 new MethodCallRename(FormResultCompiler::class, 'JStop', 'addCssFiles'),
+            ]),
+        ]]);
+
+    // @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/8.4/Deprecation-77826-RTEHtmlAreaSpellcheckerEntrypoint.html
+    $services->set(RenameMethodRector::class)
+        ->call('configurre', [[
+            RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+                new MethodCallRename(SpellCheckingController::class, 'main', 'processRequest'),
             ]),
         ]]);
 };
