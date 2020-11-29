9d66ecf8c507a88fa1f75f4f78645fed2d29e998
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-11-29 13:19:33 +0100

    [TASK] Change RTEHtmlArea Spellchecker entrypoint (#622) (#1689)
    Resovles: #622

---
+++ stubs/Rtehtmlarea/Controller/SpellCheckingController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Saltedpasswords\Salt;
+
+if (class_exists(SpellCheckingController::class)) {
+    return;
+}
+
+final class SpellCheckingController
+{
+}
