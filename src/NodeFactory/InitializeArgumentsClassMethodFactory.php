5a2926d9cb7245fdc5f1a0fa31b13e322415f03a
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-12-02 18:26:33 +0100

    [BUGFIX] Add correct type infered by type declarations
    Resolves: #1701

--- src/NodeFactory/InitializeArgumentsClassMethodFactory.php
+++ src/NodeFactory/InitializeArgumentsClassMethodFactory.php
@@ -5,30 +5,49 @@ declare(strict_types=1);
 namespace Ssch\TYPO3Rector\NodeFactory;
 
 use PhpParser\BuilderFactory;
+use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\NullableType;
 use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Nop;
+use PhpParser\Node\UnionType;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeWithClassName;
 use Rector\AttributeAwarePhpDoc\Ast\PhpDoc\AttributeAwareParamTagValueNode;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\Core\PhpParser\Node\NodeFactory;
+use Rector\Core\Rector\AbstractRector\NameResolverTrait;
 use Rector\NodeNameResolver\NodeNameResolver;
 use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\PHPStan\Type\ShortenedObjectType;
+use Rector\PHPStanStaticTypeMapper\PHPStanStaticTypeMapper;
 use Rector\StaticTypeMapper\StaticTypeMapper;
+use Rector\TypeDeclaration\TypeInferer\ParamTypeInferer;
+use ReflectionClass;
 
 final class InitializeArgumentsClassMethodFactory
 {
+    use NameResolverTrait;
+
     /**
      * @var string
      */
     private const METHOD_NAME = 'initializeArguments';
+
+    /**
+     * @var string
+     */
+    private const MIXED = 'mixed';
 
     /**
      * @var BuilderFactory
@@ -41,25 +60,27 @@ final class InitializeArgumentsClassMethodFactory
     private $nodeFactory;
 
     /**
-     * @var NodeNameResolver
-     */
-    private $nodeNameResolver;
-
-    /**
      * @var StaticTypeMapper
      */
     private $staticTypeMapper;
+
+    /**
+     * @var ParamTypeInferer
+     */
+    private $paramTypeInferer;
 
     public function __construct(
         BuilderFactory $builderFactory,
         NodeFactory $nodeFactory,
         NodeNameResolver $nodeNameResolver,
-        StaticTypeMapper $staticTypeMapper
+        StaticTypeMapper $staticTypeMapper,
+        ParamTypeInferer $paramTypeInferer
     ) {
         $this->builderFactory = $builderFactory;
         $this->nodeFactory = $nodeFactory;
         $this->nodeNameResolver = $nodeNameResolver;
         $this->staticTypeMapper = $staticTypeMapper;
+        $this->paramTypeInferer = $paramTypeInferer;
     }
 
     public function decorateClass(Class_ $class): void
@@ -171,8 +192,38 @@ final class InitializeArgumentsClassMethodFactory
 
     private function createTypeInString(?ParamTagValueNode $paramTagValueNode, Param $param): string
     {
+        if (null !== $param->type) {
+            return $this->resolveParamType($param->type);
+        }
+
+        $inferedType = $this->paramTypeInferer->inferParam($param);
+        if ($inferedType instanceof MixedType) {
+            return self::MIXED;
+        }
+
+        if ($this->isTraitType($inferedType)) {
+            return self::MIXED;
+        }
+
+        $paramTypeNode = $this->staticTypeMapper->mapPHPStanTypeToPhpParserNode(
+            $inferedType,
+            PHPStanStaticTypeMapper::KIND_PARAM
+        );
+
+        if ($paramTypeNode instanceof UnionType) {
+            return self::MIXED;
+        }
+
+        if ($paramTypeNode instanceof NullableType) {
+            return self::MIXED;
+        }
+
+        if (null !== $paramTypeNode) {
+            return $paramTypeNode->toString();
+        }
+
         if (null === $paramTagValueNode) {
-            return 'mixed';
+            return self::MIXED;
         }
 
         $docString = $this->staticTypeMapper->mapPHPStanPhpDocTypeNodeToPhpDocString($paramTagValueNode->type, $param);
@@ -181,5 +232,40 @@ final class InitializeArgumentsClassMethodFactory
         }
 
         return $docString;
+    }
+
+    private function isTraitType(Type $type): bool
+    {
+        if (! $type instanceof TypeWithClassName) {
+            return false;
+        }
+
+        $fullyQualifiedName = $this->getFullyQualifiedName($type);
+
+        if (! class_exists($fullyQualifiedName)) {
+            return false;
+        }
+
+        $reflectionClass = new ReflectionClass($fullyQualifiedName);
+
+        return $reflectionClass->isTrait();
+    }
+
+    private function getFullyQualifiedName(TypeWithClassName $typeWithClassName): string
+    {
+        if ($typeWithClassName instanceof ShortenedObjectType) {
+            return $typeWithClassName->getFullyQualifiedName();
+        }
+
+        return $typeWithClassName->getClassName();
+    }
+
+    private function resolveParamType(Node $paramType): string
+    {
+        if ($paramType instanceof FullyQualified) {
+            return $paramType->toCodeString();
+        }
+
+        return $this->getName($paramType) ?? self::MIXED;
     }
 }
