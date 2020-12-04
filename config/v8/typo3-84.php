e57f9f76ce692f9fe5f41ffa946fea6eee8731ad
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-12-04 13:48:46 +0100

    [TASK] Substitute wizard icons with new ones
    Resolves: #600

--- config/v8/typo3-84.php
+++ config/v8/typo3-84.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\ValueObject\MethodCallRename;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
+use Ssch\TYPO3Rector\Rector\v8\v4\SubstituteOldWizardIconsRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TYPO3\CMS\Backend\Routing\FormResultCompiler;
 use TYPO3\CMS\Saltedpasswords\Salt\SpellCheckingController;
@@ -12,20 +13,34 @@ use TYPO3\CMS\Saltedpasswords\Salt\SpellCheckingController;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
     $services = $containerConfigurator->services();
-
     // @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/8.4/Deprecation-75363-DeprecateFormResultCompilerJStop.html
-    $services->set(RenameMethodRector::class)
-        ->call('configure', [[
-            RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
-                new MethodCallRename(FormResultCompiler::class, 'JStop', 'addCssFiles'),
-            ]),
-        ]]);
-
+    $services->set(RenameMethodRector::class)->call('configure', [[
+        RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+            new MethodCallRename(FormResultCompiler::class, 'JStop', 'addCssFiles'),
+        ]),
+    ]]);
     // @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/8.4/Deprecation-77826-RTEHtmlAreaSpellcheckerEntrypoint.html
-    $services->set(RenameMethodRector::class)
-        ->call('configurre', [[
-            RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
-                new MethodCallRename(SpellCheckingController::class, 'main', 'processRequest'),
-            ]),
-        ]]);
+    $services->set(RenameMethodRector::class)->call('configure', [[
+        RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+            new MethodCallRename(SpellCheckingController::class, 'main', 'processRequest'),
+        ]),
+    ]]);
+    $services->set(SubstituteOldWizardIconsRector::class)->call('configure', [[
+        SubstituteOldWizardIconsRector::OLD_TO_NEW_FILE_LOCATIONS => [
+            'add.gif' => 'actions-add',
+            'link_popup.gif' => 'actions-wizard-link',
+            'wizard_rte2.gif' => 'actions-wizard-rte',
+            'wizard_link.gif' => 'actions-wizard-rte',
+            'wizard_table.gif' => 'content-table',
+            'edit2.gif' => 'actions-open',
+            'list.gif' => 'actions-system-list-open',
+            'wizard_forms.gif' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_forms.gif',
+            'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_add.gif' => 'actions-add',
+            'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_table.gif' => 'content-table',
+            'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_edit.gif' => 'actions-open',
+            'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_list.gif' => 'actions-system-list-open',
+            'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_link.gif' => 'actions-wizard-link',
+            'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_rte.gif' => 'actions-wizard-rte',
+        ],
+    ]]);
 };
---
+++ tests/Rector/v8/v4/SubstituteOldWizardIcons/SubstituteOldWizardIconsRectorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v8\v4\SubstituteOldWizardIcons;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Ssch\TYPO3Rector\Rector\v8\v4\SubstituteOldWizardIconsRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class SubstituteOldWizardIconsRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorsWithConfiguration(): array
+    {
+        return [
+            SubstituteOldWizardIconsRector::class => [
+                SubstituteOldWizardIconsRector::OLD_TO_NEW_FILE_LOCATIONS => [
+                    'add.gif' => 'actions-add',
+                    'link_popup.gif' => 'actions-wizard-link',
+                    'wizard_rte2.gif' => 'actions-wizard-rte',
+                    'wizard_link.gif' => 'actions-wizard-rte',
+                    'wizard_table.gif' => 'content-table',
+                    'edit2.gif' => 'actions-open',
+                    'list.gif' => 'actions-system-list-open',
+                    'wizard_forms.gif' => 'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_forms.gif',
+                    'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_add.gif' => 'actions-add',
+                    'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_table.gif' => 'content-table',
+                    'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_edit.gif' => 'actions-open',
+                    'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_list.gif' => 'actions-system-list-open',
+                    'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_link.gif' => 'actions-wizard-link',
+                    'EXT:backend/Resources/Public/Images/FormFieldWizard/wizard_rte.gif' => 'actions-wizard-rte',
+                ],
+            ],
+        ];
+    }
+}
