8fdea7cf95e6dc61d34e2ee73bc56a08dc8345ea
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-12-07 13:03:07 +0100

    [TASK] Removed TCA tree options: width, allowRecursiveMode, autoSizeMax
    Resolves: #573

--- config/v8/typo3-83.php
+++ config/v8/typo3-83.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 use Ssch\TYPO3Rector\Rector\v8\v3\RefactorMethodFileContentRector;
 use Ssch\TYPO3Rector\Rector\v8\v3\RefactorQueryViewTableWrapRector;
+use Ssch\TYPO3Rector\Rector\v8\v3\RemovedTcaSelectTreeOptionsRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
-
     $services = $containerConfigurator->services();
-
     $services->set(RefactorMethodFileContentRector::class);
-
     $services->set(RefactorQueryViewTableWrapRector::class);
+    $services->set(RemovedTcaSelectTreeOptionsRector::class);
 };
