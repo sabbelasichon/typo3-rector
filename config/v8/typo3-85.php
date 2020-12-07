44bf01fb97b046bb08623b3b82505a5b04f58054
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-12-07 22:08:37 +0100

    [TASK] TCA option versioning_followPages removed
    Resolves: #655

--- config/v8/typo3-85.php
+++ config/v8/typo3-85.php
@@ -8,6 +8,7 @@ use Rector\Renaming\ValueObject\MethodCallRename;
 use Rector\Renaming\ValueObject\RenameStaticMethod;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
 use Ssch\TYPO3Rector\Rector\v8\v5\CharsetConverterToMultiByteFunctionsRector;
+use Ssch\TYPO3Rector\Rector\v8\v5\RemoveOptionVersioningFollowPagesRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TYPO3\CMS\Backend\Clipboard\ClipBoard;
 use TYPO3\CMS\Core\Utility\ArrayUtility as CoreArrayUtility;
@@ -16,48 +17,41 @@ use TYPO3\CMS\Extbase\Utility\ArrayUtility;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-
-    $services->set(RenameMethodRector::class)
-        ->call(
-                 'configure',
-                 [[
-                     RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects(
-                         [new MethodCallRename(ClipBoard::class, 'printContentFromTab', 'getContentFromTab')]
-                     ),
-                 ]]
-             );
-
+    $services->set(RenameMethodRector::class)->call('configure', [[
+        RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+            new MethodCallRename(ClipBoard::class, 'printContentFromTab', 'getContentFromTab'),
+        ]),
+    ]]);
     $services->set(CharsetConverterToMultiByteFunctionsRector::class);
-
-    $services->set(RenameStaticMethodRector::class)
-        ->call('configure', [[
-            RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => inline_value_objects([
-                new RenameStaticMethod(ArrayUtility::class, 'integerExplode', GeneralUtility::class, 'intExplode'),
-                new RenameStaticMethod(ArrayUtility::class, 'trimExplode', GeneralUtility::class, 'trimExplode'),
-                new RenameStaticMethod(
-                    ArrayUtility::class,
-                    'getValueByPath',
-                    CoreArrayUtility::class,
-                    'getValueByPath'
-                ),
-                new RenameStaticMethod(
-                    ArrayUtility::class,
-                    'setValueByPath',
-                    CoreArrayUtility::class,
-                    'setValueByPath'
-                ),
-                new RenameStaticMethod(
-                    ArrayUtility::class,
-                    'unsetValueByPath',
-                    CoreArrayUtility::class,
-                    'removeByPath'
-                ),
-                new RenameStaticMethod(
-                    ArrayUtility::class,
-                    'sortArrayWithIntegerKeys',
-                    CoreArrayUtility::class,
-                    'sortArrayWithIntegerKeys'
-                ),
-            ]),
-        ]]);
+    $services->set(RenameStaticMethodRector::class)->call('configure', [[
+        RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => inline_value_objects([
+            new RenameStaticMethod(ArrayUtility::class, 'integerExplode', GeneralUtility::class, 'intExplode'),
+            new RenameStaticMethod(ArrayUtility::class, 'trimExplode', GeneralUtility::class, 'trimExplode'),
+            new RenameStaticMethod(
+                ArrayUtility::class,
+                'getValueByPath',
+                CoreArrayUtility::class,
+                'getValueByPath'
+            ),
+            new RenameStaticMethod(
+                ArrayUtility::class,
+                'setValueByPath',
+                CoreArrayUtility::class,
+                'setValueByPath'
+            ),
+            new RenameStaticMethod(
+                ArrayUtility::class,
+                'unsetValueByPath',
+                CoreArrayUtility::class,
+                'removeByPath'
+            ),
+            new RenameStaticMethod(
+                ArrayUtility::class,
+                'sortArrayWithIntegerKeys',
+                CoreArrayUtility::class,
+                'sortArrayWithIntegerKeys'
+            ),
+        ]),
+    ]]);
+    $services->set(RemoveOptionVersioningFollowPagesRector::class);
 };
