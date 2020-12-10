011d74eb25f3b9aeeb2f31a832c1a6cf901b24bb
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-12-10 21:17:38 +0100

    [TASK] Remove divider2tabs
    Resolves: #24

--- config/v7/typo3-70.php
+++ config/v7/typo3-70.php
@@ -8,6 +8,7 @@ use Rector\Renaming\Rector\StaticCall\RenameStaticMethodRector;
 use Rector\Renaming\ValueObject\MethodCallRename;
 use Rector\Renaming\ValueObject\RenameStaticMethod;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
+use Ssch\TYPO3Rector\Rector\v7\v0\RemoveDivider2TabsConfigurationRector;
 use Ssch\TYPO3Rector\Rector\v7\v0\RemoveMethodCallConnectDbRector;
 use Ssch\TYPO3Rector\Rector\v7\v0\RemoveMethodCallLoadTcaRector;
 use Ssch\TYPO3Rector\Rector\v7\v0\TypeHandlingServiceToTypeHandlingUtilityRector;
@@ -24,47 +25,34 @@ use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
-
     $services = $containerConfigurator->services();
-
     $services->set(RemoveMethodCallConnectDbRector::class);
     $services->set(RemoveMethodCallLoadTcaRector::class);
-    $services->set(RenameClassRector::class)
-        ->call('configure', [
-            [
-                RenameClassRector::OLD_TO_NEW_CLASSES => [
-                    MediumDocumentTemplate::class => DocumentTemplate::class,
-                    SmallDocumentTemplate::class => DocumentTemplate::class,
-                    StandardDocumentTemplate::class => DocumentTemplate::class,
-                    BigDocumentTemplate::class => DocumentTemplate::class,
-                ],
-            ],
-        ]);
-
-    $services->set(RenameStaticMethodRector::class)
-        ->call('configure', [
-            [
-                RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => inline_value_objects([
-                    new RenameStaticMethod(
-                        GeneralUtility::class,
-                        'int_from_ver',
-                        VersionNumberUtility::class,
-                        'convertVersionNumberToInteger'
-                    ),
-                ]),
-            ],
-        ]);
-
+    $services->set(RenameClassRector::class)->call('configure', [[
+        RenameClassRector::OLD_TO_NEW_CLASSES => [
+            MediumDocumentTemplate::class => DocumentTemplate::class,
+            SmallDocumentTemplate::class => DocumentTemplate::class,
+            StandardDocumentTemplate::class => DocumentTemplate::class,
+            BigDocumentTemplate::class => DocumentTemplate::class,
+        ],
+    ]]);
+    $services->set(RenameStaticMethodRector::class)->call('configure', [[
+        RenameStaticMethodRector::OLD_TO_NEW_METHODS_BY_CLASSES => inline_value_objects([
+            new RenameStaticMethod(
+                GeneralUtility::class,
+                'int_from_ver',
+                VersionNumberUtility::class,
+                'convertVersionNumberToInteger'
+            ),
+        ]),
+    ]]);
     $services->set(TypeHandlingServiceToTypeHandlingUtilityRector::class);
-
-    $services->set(RenameMethodRector::class)
-        ->call('configure', [
-            [
-                RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
-                    new MethodCallRename(Typo3QuerySettings::class, 'setSysLanguageUid', 'setLanguageUid'),
-                    new MethodCallRename(Typo3QuerySettings::class, 'getSysLanguageUid', 'getLanguageUid'),
-                    new MethodCallRename(ObjectManager::class, 'create', 'get'),
-                ]),
-            ],
-        ]);
+    $services->set(RenameMethodRector::class)->call('configure', [[
+        RenameMethodRector::METHOD_CALL_RENAMES => inline_value_objects([
+            new MethodCallRename(Typo3QuerySettings::class, 'setSysLanguageUid', 'setLanguageUid'),
+            new MethodCallRename(Typo3QuerySettings::class, 'getSysLanguageUid', 'getLanguageUid'),
+            new MethodCallRename(ObjectManager::class, 'create', 'get'),
+        ]),
+    ]]);
+    $services->set(RemoveDivider2TabsConfigurationRector::class);
 };
