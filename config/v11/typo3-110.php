7fd92003e7eca0e0a72984b4c39396ecbdbc4a14
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-12-29 21:02:44 +0100

    [TASK] Return timestamp as integer in DateTimeAspect
    Resolves: #1750

--- config/v11/typo3-110.php
+++ config/v11/typo3-110.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Ssch\TYPO3Rector\Rector\v11\v0\DateTimeAspectInsteadOfGlobalsExecTimeRector;
 use Ssch\TYPO3Rector\Rector\v11\v0\ForwardResponseInsteadOfForwardMethodRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -9,4 +10,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
     $services = $containerConfigurator->services();
     $services->set(ForwardResponseInsteadOfForwardMethodRector::class);
+    $services->set(DateTimeAspectInsteadOfGlobalsExecTimeRector::class);
 };
