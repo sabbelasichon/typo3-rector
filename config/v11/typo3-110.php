200bf1bdece11a72ee5fad3201ed718bde04c91b
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-12-31 13:11:32 +0100

    [TASK] Use BackendUtility::getClickMenuOnIconTagParameters() if needed
    Resolves: #1831

--- config/v11/typo3-110.php
+++ config/v11/typo3-110.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Ssch\TYPO3Rector\Rector\v11\v0\DateTimeAspectInsteadOfGlobalsExecTimeRector;
 use Ssch\TYPO3Rector\Rector\v11\v0\ForwardResponseInsteadOfForwardMethodRector;
+use Ssch\TYPO3Rector\Rector\v11\v0\GetClickMenuOnIconTagParametersRector;
 use Ssch\TYPO3Rector\Rector\v11\v0\UniqueListFromStringUtilityRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -13,4 +14,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(ForwardResponseInsteadOfForwardMethodRector::class);
     $services->set(DateTimeAspectInsteadOfGlobalsExecTimeRector::class);
     $services->set(UniqueListFromStringUtilityRector::class);
+    $services->set(GetClickMenuOnIconTagParametersRector::class);
 };
---
+++ src/Rector/v11/v0/GetClickMenuOnIconTagParametersRector.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v11\v0;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\StaticCall;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+
+/**
+ * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/11.0/Deprecation-92583-DeprecateLastArgumentsOfWrapClickMenuOnIcon.html
+ */
+final class GetClickMenuOnIconTagParametersRector extends AbstractRector
+{
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [StaticCall::class];
+    }
+
+    /**
+     * @param StaticCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isMethodStaticCallOrClassMethodObjectType($node, BackendUtility::class)) {
+            return null;
+        }
+
+        if (! $this->isName($node->name, 'wrapClickMenuOnIcon')) {
+            return null;
+        }
+
+        if (! count($node->args) > 3) {
+            return null;
+        }
+
+        $returnTagParameters = isset($node->args[6]) ? $this->getValue($node->args[6]->value) : false;
+
+        if (null === $returnTagParameters) {
+            return null;
+        }
+
+        if (false === $returnTagParameters) {
+            unset($node->args[3], $node->args[4], $node->args[5], $node->args[6]);
+            return $node;
+        }
+
+        return $this->createStaticCall(BackendUtility::class, 'getClickMenuOnIconTagParameters', [
+            $node->args[0],
+            $node->args[1],
+            $node->args[2],
+        ]);
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Use BackendUtility::getClickMenuOnIconTagParameters() instead BackendUtility::wrapClickMenuOnIcon() if needed',
+            [new CodeSample(<<<'PHP'
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+$returnTagParameters = true;
+BackendUtility::wrapClickMenuOnIcon('pages', 1, 'foo', '', '', '', $returnTagParameters);
+PHP
+                , <<<'PHP'
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+$returnTagParameters = true;
+BackendUtility::getClickMenuOnIconTagParameters('pages', 1, 'foo');
+PHP
+        )]);
+    }
+}
