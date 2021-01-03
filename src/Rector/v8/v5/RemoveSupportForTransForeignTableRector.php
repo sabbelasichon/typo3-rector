95e7429cca8e9269b7fc38bbc337bec5a41f2b03
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-01-03 18:21:23 +0100

    [TASK] Remove support for transForeignTable in TCA
    Resolves: #670

---
+++ src/Rector/v8/v5/RemoveSupportForTransForeignTableRector.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v8\v5;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Stmt\Return_;
+use Rector\Core\Rector\AbstractRector;
+use Ssch\TYPO3Rector\Helper\TcaHelperTrait;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/8.5/Breaking-78191-RemoveSupportForTransForeignTableInTCA.html
+ */
+final class RemoveSupportForTransForeignTableRector extends AbstractRector
+{
+    use TcaHelperTrait;
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Return_::class];
+    }
+
+    /**
+     * @param Return_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isTca($node)) {
+            return null;
+        }
+
+        $ctrl = $this->extractCtrl($node);
+
+        if (! $ctrl instanceof ArrayItem) {
+            return null;
+        }
+
+        $items = $ctrl->value;
+
+        if (! $items instanceof Array_) {
+            return null;
+        }
+
+        foreach ($items->items as $fieldValue) {
+            if (! $fieldValue instanceof ArrayItem) {
+                continue;
+            }
+
+            if (null === $fieldValue->key) {
+                continue;
+            }
+
+            if ($this->isValues($fieldValue->key, ['transForeignTable', 'transOrigPointerTable'])) {
+                $this->removeNode($fieldValue);
+            }
+        }
+
+        return $node;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Remove support for transForeignTable in TCA', [new CodeSample(<<<'PHP'
+return [
+    'ctrl' => [
+        'transForeignTable' => 'l10n_parent',
+        'transOrigPointerTable' => 'l10n_parent',
+    ],
+];
+PHP
+            , <<<'PHP'
+return [
+    'ctrl' => [],
+];
+PHP
+        )]);
+    }
+}
