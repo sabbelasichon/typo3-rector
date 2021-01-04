21e05ebdabed419785cdd5fa88146b1740d27b31
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-01-04 18:34:46 +0100

    [TASK] Add config type if not exists
    Relates: #705

--- config/v8/typo3-86.php
+++ config/v8/typo3-86.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Renaming\Rector\Namespace_\RenameNamespaceRector;
+use Ssch\TYPO3Rector\Rector\v8\v6\AddTypeToColumnConfigRector;
 use Ssch\TYPO3Rector\Rector\v8\v6\MigrateSelectShowIconTableRector;
 use Ssch\TYPO3Rector\Rector\v8\v6\MoveRequestUpdateOptionFromControlToColumnsRector;
 use Ssch\TYPO3Rector\Rector\v8\v6\RefactorTCARector;
@@ -22,4 +23,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(RefactorTCARector::class);
     $services->set(MigrateSelectShowIconTableRector::class);
     $services->set(RemoveL10nModeNoCopyRector::class);
+    $services->set(AddTypeToColumnConfigRector::class);
 };
