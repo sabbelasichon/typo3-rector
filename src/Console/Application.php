ae1b8d7d6e0c2bcc468de011ca3706f2c79027c7
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-01-19 15:59:43 +0100

    [TASK] Prepare release 0.8.8

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -32,7 +32,7 @@ final class Application extends SymfonyApplication
     /**
      * @var string
      */
-    private const VERSION = '0.8.7';
+    private const VERSION = '0.8.8';
 
     /**
      * @var Configuration
