85e8c7fe1857f25eb67ef5337b0f98a2217d5209
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-02-09 23:13:29 +0100

    [TASK] Enhance OptionalConstructorToHardRequirementRector
    Resolves: #1955

--- stubs/Core/Database/ConnectionPool.php
+++ stubs/Core/Database/ConnectionPool.php
@@ -10,6 +10,11 @@ if (class_exists(ConnectionPool::class)) {
 
 final class ConnectionPool
 {
+    public function getConnectionForTable(string $table): Connection
+    {
+        return new Connection();
+    }
+
     public function getQueryBuilderForTable($table): void
     {
     }
