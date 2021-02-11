b7cafc83a1a29cf5eacef34efbd6702607fd97c3
TomasVotruba <tomas.vot@gmail.com>
2021-02-11 02:57:57 +0100

    make use of configurable phpstan for rector path

--- tests/Rector/v10/v0/ChangeDefaultCachingFrameworkNames/config/configured_rule.php
+++ tests/Rector/v10/v0/ChangeDefaultCachingFrameworkNames/config/configured_rule.php
@@ -2,11 +2,14 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
 use Ssch\TYPO3Rector\Rector\v10\v0\ChangeDefaultCachingFrameworkNamesRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, __DIR__ . '/../../../../../../utils/phpstan/config/extension.neon');
 
+    $services = $containerConfigurator->services();
     $services->set(ChangeDefaultCachingFrameworkNamesRector::class);
 };
