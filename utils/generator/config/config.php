cfa1bd7485d4475b6343dfbe05ab4fd63e95ca90
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-02-15 09:57:56 +0100

    [BUGFIX] Load config for utils correctly
    Resolves: #1969

--- utils/generator/config/config.php
+++ utils/generator/config/config.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/../../../../../../config/services.php');
-
     $services = $containerConfigurator->services();
     $services->defaults()
         ->public()
