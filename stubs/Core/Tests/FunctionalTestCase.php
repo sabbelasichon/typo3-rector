58df99aa6b7a0490bc9cd398296b18a21ac2fb10
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-02-28 21:51:08 +0100

    [BUGFIX] Core tests to TestingFramework use classes (#2007)
    Resolves: #2002

---
+++ stubs/Core/Tests/FunctionalTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Core\Tests;
+
+if (class_exists(FunctionalTestCase::class)) {
+    return;
+}
+
+class FunctionalTestCase
+{
+}
---
+++ tests/Rector/v8/v6/CoreTestsToTestingFramework/CoreTestsToTestingFramework.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v8\v6\CoreTestsToTestingFramework;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractCommunityRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class CoreTestsToTestingFramework extends AbstractCommunityRectorTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
