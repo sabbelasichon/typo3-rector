c734bd59494e14268b932ab2d55b4e5f01ce6061
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-03-03 15:47:08 +0100

    [TASK] Migrate last pieces of default extras
    Resolves: #1989

--- config/v8/typo3-86.php
+++ config/v8/typo3-86.php
@@ -2,28 +2,20 @@
 
 declare(strict_types=1);
 
-use Rector\Renaming\Rector\Name\RenameClassRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use TYPO3\CMS\Core\Tests\FunctionalTestCase as CoreFunctionalTestCase;
-use TYPO3\CMS\Core\Tests\UnitTestCase as CoreUnitTestCase;
-use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
-use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
-
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (
+    \Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator
+): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
-
     $services = $containerConfigurator->services();
     $services->set('namespace_typo3_cms_core_tests_to__typo3_testing_framework_core')
-        ->class(RenameClassRector::class)
-        ->call(
-                 'configure',
-                 [
-                     [
-                         RenameClassRector::OLD_TO_NEW_CLASSES => [
-                             CoreUnitTestCase::class => UnitTestCase::class,
-                             CoreFunctionalTestCase::class => FunctionalTestCase::class,
-                         ],
-                     ],
-                 ]
-             );
+        ->class(\Rector\Renaming\Rector\Name\RenameClassRector::class)->call(
+        'configure',
+        [[
+            \Rector\Renaming\Rector\Name\RenameClassRector::OLD_TO_NEW_CLASSES => [
+                \TYPO3\CMS\Core\Tests\UnitTestCase::class => \TYPO3\TestingFramework\Core\Unit\UnitTestCase::class,
+                \TYPO3\CMS\Core\Tests\FunctionalTestCase::class => \TYPO3\TestingFramework\Core\Functional\FunctionalTestCase::class,
+            ],
+        ]]
+    );
+    $services->set(\Ssch\TYPO3Rector\Rector\v8\v6\MigrateLastPiecesOfDefaultExtrasRector::class);
 };
