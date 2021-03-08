2aaaab04afba3e58148bb9f429c1fb402019d777
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-03-08 22:55:28 +0100

    [BUGFIX] Keep PascalCase in extensionName
    Resolves: #2022

--- src/Rector/v10/v1/RegisterPluginWithVendorNameRector.php
+++ src/Rector/v10/v1/RegisterPluginWithVendorNameRector.php
@@ -14,6 +14,7 @@ use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 use Symplify\SmartFileSystem\SmartFileInfo;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use UnexpectedValueException;
 
 /**
  * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.1/Deprecation-88995-CallingRegisterPluginWithVendorName.html
@@ -111,6 +112,13 @@ final class RegisterPluginWithVendorNameRector extends AbstractRector
     private function prepareExtensionName(string $extensionName, int $delimiterPosition): string
     {
         $extensionName = substr($extensionName, $delimiterPosition + 1);
-        return str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($extensionName))));
+
+        $underScoredExtensionName = preg_replace('#[A-Z]#', '_\\0', lcfirst($extensionName));
+
+        if (! is_string($underScoredExtensionName)) {
+            throw new UnexpectedValueException('The extension name could not be parsed');
+        }
+
+        return str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($underScoredExtensionName))));
     }
 }
