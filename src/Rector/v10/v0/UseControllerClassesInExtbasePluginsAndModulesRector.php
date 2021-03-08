c7db1e24c342a97bcc2fd255bf0ace7fb428a459
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-03-08 23:25:18 +0100

    [BUGFIX] No empty extensionKey in configurePlugin methd (#2028)
    Resolves: #2024

--- src/Rector/v10/v0/UseControllerClassesInExtbasePluginsAndModulesRector.php
+++ src/Rector/v10/v0/UseControllerClassesInExtbasePluginsAndModulesRector.php
@@ -16,6 +16,7 @@ use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 use Symplify\SmartFileSystem\SmartFileInfo;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use UnexpectedValueException;
 
 /**
  * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Deprecation-87550-UseControllerClassesWhenRegisteringPluginsmodules.html
@@ -66,6 +67,10 @@ final class UseControllerClassesInExtbasePluginsAndModulesRector extends Abstrac
 
         $vendorName = $this->prepareVendorName($extensionName, $delimiterPosition);
         $extensionName = $this->prepareExtensionName($extensionName, $delimiterPosition);
+
+        if ('' === $extensionName) {
+            return null;
+        }
 
         $node->args[0] = $this->nodeFactory->createArg($extensionName);
 
@@ -182,13 +187,21 @@ CODE_SAMPLE
         if (null !== $this->valueResolver->getValue($extensionNameArgumentValue->right)) {
             return false;
         }
-        return $this->isNames($extensionNameArgumentValue->right, ['_EXTKEY', 'extensionKey']);
+
+        return true;
     }
 
     private function prepareExtensionName(string $extensionName, int $delimiterPosition): string
     {
         $extensionName = substr($extensionName, $delimiterPosition + 1);
-        return str_replace(' ', '', ucwords(str_replace('_', ' ', $extensionName)));
+
+        $underScoredExtensionName = preg_replace('#[A-Z]#', '_\\0', lcfirst($extensionName));
+
+        if (! is_string($underScoredExtensionName)) {
+            throw new UnexpectedValueException('The extension name could not be parsed');
+        }
+
+        return str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($underScoredExtensionName))));
     }
 
     private function prepareVendorName(string $extensionName, int $delimiterPosition): string
