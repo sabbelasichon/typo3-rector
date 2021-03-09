3f5e3844a654756a8fbf55cae970171489a2f65c
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-03-10 00:06:35 +0100

    [TASK] Add command to fix composer.json (#2030)
    Resolves: #2016

---
+++ tests/Rector/Composer/ExtensionComposerRector/config/configured_rule.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Ssch\TYPO3Rector\Rector\Composer\ExtensionComposerRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../../../config/services.php');
+    $services = $containerConfigurator->services();
+
+    $services->set(ExtensionComposerRector::class)
+        ->call('configure', [
+            [
+                ExtensionComposerRector::TYPO3_VERSION_CONSTRAINT => '^9.5',
+            ],
+        ]);
+};
