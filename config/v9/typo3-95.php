5686f6c72967936877f2fe9e178227170510ffcd
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-03-29 22:14:45 +0200

    [TASK] Refactor TCA type group internal_type file and file_reference
    Resolves: #2100

--- config/v9/typo3-95.php
+++ config/v9/typo3-95.php
@@ -7,6 +7,7 @@ use Rector\Renaming\ValueObject\MethodCallRename;
 use Ssch\TYPO3Rector\Rector\Migrations\RenameClassMapAliasRector;
 use Ssch\TYPO3Rector\Rector\v9\v5\RefactorProcessOutputRector;
 use Ssch\TYPO3Rector\Rector\v9\v5\RefactorPropertiesOfTypoScriptFrontendControllerRector;
+use Ssch\TYPO3Rector\Rector\v9\v5\RefactorTypeInternalTypeFileAndFileReferenceToFalRector;
 use Ssch\TYPO3Rector\Rector\v9\v5\RemoveFlushCachesRector;
 use Ssch\TYPO3Rector\Rector\v9\v5\RemoveInitMethodFromPageRepositoryRector;
 use Ssch\TYPO3Rector\Rector\v9\v5\RemoveInternalAnnotationRector;
@@ -16,51 +17,35 @@ use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../services.php');
-
     $services = $containerConfigurator->services();
-
     $services->set(UsePackageManagerActivePackagesRector::class);
-
     $services->set('resource_storage_dump_file_contents_to_stream_file')
         ->class(RenameMethodRector::class)
-        ->call('configure', [
-            [
-                RenameMethodRector::METHOD_CALL_RENAMES => ValueObjectInliner::inline([
-                    new MethodCallRename(
-                       'TYPO3\CMS\Core\Resource\ResourceStorage',
-                       'dumpFileContents',
-                       'streamFile'
-                   ),
-                ]),
-            ],
-        ]);
-
+        ->call('configure', [[
+            RenameMethodRector::METHOD_CALL_RENAMES => ValueObjectInliner::inline([
+                new MethodCallRename('TYPO3\CMS\Core\Resource\ResourceStorage', 'dumpFileContents', 'streamFile'),
+            ]),
+        ]]);
     $services->set(RemoveFlushCachesRector::class);
-
     $services->set(RemoveInternalAnnotationRector::class);
-
     $services->set('rename_class_alias_maps_version_95')
         ->class(RenameClassMapAliasRector::class)
-        ->call('configure', [
-            [
-                RenameClassMapAliasRector::CLASS_ALIAS_MAPS => [
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/adminpanel/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/backend/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/core/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/fluid/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/info/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/lowlevel/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/recordlist/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/reports/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/t3editor/Migrations/Code/ClassAliasMap.php',
-                    __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/workspaces/Migrations/Code/ClassAliasMap.php',
-                ],
+        ->call('configure', [[
+            RenameClassMapAliasRector::CLASS_ALIAS_MAPS => [
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/adminpanel/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/backend/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/core/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/fluid/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/info/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/lowlevel/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/recordlist/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/reports/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/t3editor/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../Migrations/TYPO3/9.5/typo3/sysext/workspaces/Migrations/Code/ClassAliasMap.php',
             ],
-        ]);
-
+        ]]);
     $services->set(RemoveInitMethodFromPageRepositoryRector::class);
-
     $services->set(RefactorProcessOutputRector::class);
-
     $services->set(RefactorPropertiesOfTypoScriptFrontendControllerRector::class);
+    $services->set(RefactorTypeInternalTypeFileAndFileReferenceToFalRector::class);
 };
---
+++ src/Rector/v9/v5/RefactorTypeInternalTypeFileAndFileReferenceToFalRector.php
@@ -0,0 +1,188 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v9\v5;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Stmt\Return_;
+use Rector\Core\Rector\AbstractRector;
+use Ssch\TYPO3Rector\Helper\TcaHelperTrait;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+/**
+ * @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.5/Deprecation-86406-TCATypeGroupInternal_typeFileAndFile_reference.html
+ */
+final class RefactorTypeInternalTypeFileAndFileReferenceToFalRector extends AbstractRector
+{
+    use TcaHelperTrait;
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Return_::class];
+    }
+
+    /**
+     * @param Return_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isTca($node)) {
+            return null;
+        }
+
+        $columns = $this->extractColumns($node);
+
+        if (! $columns instanceof ArrayItem) {
+            return null;
+        }
+
+        $columnItems = $columns->value;
+
+        if (! $columnItems instanceof Array_) {
+            return null;
+        }
+
+        $hasAstBeenChanged = false;
+        foreach ($columnItems->items as $columnItem) {
+            if (! $columnItem instanceof ArrayItem) {
+                continue;
+            }
+
+            if (null === $columnItem->key) {
+                continue;
+            }
+
+            if (! $columnItem->value instanceof Array_) {
+                continue;
+            }
+
+            foreach ($columnItem->value->items as $configValue) {
+                if (null === $configValue) {
+                    continue;
+                }
+
+                if (null === $configValue->key) {
+                    continue;
+                }
+
+                if (! $configValue->value instanceof Array_) {
+                    continue;
+                }
+
+                if (! $this->valueResolver->isValue($configValue->key, 'config')) {
+                    continue;
+                }
+
+                if (! $this->isConfigType($configValue->value, 'group')) {
+                    continue;
+                }
+
+                if (! $this->configIsOfInternalType($configValue->value, 'file') &&
+                   ! $this->configIsOfInternalType($configValue->value, 'file_reference')
+                ) {
+                    continue;
+                }
+
+                $newConfig = new Array_();
+                $allowed = null;
+                foreach ($configValue->value->items as $configItemValue) {
+                    if (! $configItemValue instanceof ArrayItem) {
+                        continue;
+                    }
+
+                    if (null === $configItemValue->key) {
+                        continue;
+                    }
+
+                    if ($this->valueResolver->isValues(
+                        $configItemValue->key,
+                        ['max_size', 'uploadfolder', 'maxitems']
+                    )) {
+                        $newConfig->items[] = new ArrayItem($configItemValue->value, $configItemValue->key);
+                        continue;
+                    }
+
+                    if ($this->valueResolver->isValue($configItemValue->key, 'allowed')) {
+                        $allowed = $configItemValue->value;
+                    }
+                }
+
+                $hasAstBeenChanged = true;
+
+                $args = [$columnItem->key, $newConfig];
+
+                if (null !== $allowed) {
+                    $args[] = $allowed;
+                }
+
+                $configValue->value = $this->nodeFactory->createStaticCall(
+                    ExtensionManagementUtility::class,
+                    'getFileFieldTCAConfig',
+                    $args
+                );
+            }
+        }
+
+        return $hasAstBeenChanged ? $node : null;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Move TCA type group internal_type file and file_reference to FAL configuration', [
+            new CodeSample(<<<'CODE_SAMPLE'
+return [
+            'ctrl' => [],
+            'columns' => [
+                'feedback_image' => [
+                    'exclude' => 1,
+                    'label' => 'FeedbackImage',
+                    'config' => [
+                        'type' => 'group',
+                        'internal_type' => 'file',
+                        'allowed' => $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'],
+                        'max_size' => '20000',
+                        'uploadfolder' => 'fileadmin/feedbacks',
+                        'maxitems' => '1',
+                    ],
+                ],
+            ],
+        ];
+CODE_SAMPLE
+            , <<<'CODE_SAMPLE'
+return [
+            'ctrl' => [],
+            'columns' => [
+                'feedback_image' => [
+                    'exclude' => 1,
+                    'label' => 'FeedbackImage',
+                    'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+                        'feedback_image',
+                        [
+                            'max_size' => '20000',
+                            'uploadfolder' => 'fileadmin/feedbacks',
+                            'maxitems' => 1,
+                            'appearance' => [
+                                'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference',
+                            ],
+                        ],
+                        $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+                    ),
+                ],
+            ],
+        ];
+CODE_SAMPLE
+        ),
+        ]);
+    }
+}
