7f70e262845bd90c282e60529b899d8928e93040
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-04-18 21:12:47 +0200

    [WIP] Add failing test for issue #2168

--- tests/Rector/Migrations/config/configured_rule.php
+++ tests/Rector/Migrations/config/configured_rule.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 use Ssch\TYPO3Rector\Rector\Migrations\RenameClassMapAliasRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Extbase\Mvc\Response;
+use TYPO3\CMS\Extbase\Mvc\Web\Response as WebResponse;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../config/config.php');
@@ -20,4 +23,15 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                 __DIR__ . '/../../../../Migrations/TYPO3/9.5/typo3/sysext/fluid/Migrations/Code/ClassAliasMap.php',
             ],
         ]]);
+
+    $services->set('web_request_to_request_web_response_to_response_foo')
+        ->class(RenameClassRector::class)
+        ->call(
+                 'configure',
+                 [[
+                     RenameClassRector::OLD_TO_NEW_CLASSES => [
+                         WebResponse::class => Response::class,
+                     ],
+                 ]]
+             );
 };
