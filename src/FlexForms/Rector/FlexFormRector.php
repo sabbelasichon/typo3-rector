8d68809fdd14bbe301b2d46a8cbfa682f5ef6a6b
TomasVotruba <tomas.vot@gmail.com>
2021-05-12 19:02:03 +0100

    fixup! Rector naming

--- src/FlexForms/Rector/FlexFormRector.php
+++ src/FlexForms/Rector/FlexFormRector.php
@@ -2,11 +2,7 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD:src/FlexForms/Transformer/FlexFormTransformer.php
-namespace Ssch\TYPO3Rector\FlexForms\Transformer;
-=======
 namespace Ssch\TYPO3Rector\FlexForms\Rector;
->>>>>>> 3cd3053a (refresh flex form rectors, apply cs with strict types):src/FlexForms/Rector/FlexFormRector.php
 
 use DOMDocument;
 use Rector\Core\Contract\Rector\RectorInterface;
