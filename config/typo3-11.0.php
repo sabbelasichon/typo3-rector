d30dac1daed8591e270d44ceb23ef7a61ac30560
Sebastian Schreiber <me@schreibersebastian.de>
2021-05-17 23:01:12 +0200

    [TASK] Resolve general folder
    Resolves: #2360

--- config/typo3-11.0.php
+++ config/typo3-11.0.php
@@ -5,6 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/general/*');
     $containerConfigurator->import(__DIR__ . '/v11/*');
 };
