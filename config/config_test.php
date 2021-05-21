6381762da4bfaed49c3c774b55cabba27c03ebfb
Sebastian Schreiber <me@schreibersebastian.de>
2021-05-21 15:24:20 +0200

    [TASK] Add neon to typo3 template
    Resolves: #2403

--- config/config_test.php
+++ config/config_test.php
@@ -3,11 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Ssch\TYPO3Rector\Configuration\Typo3Option;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/config.php');
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, __DIR__ . '/../utils/phpstan/config/extension.neon');
+    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, Typo3Option::PHPSTAN_FOR_RECTOR_PATH);
 };
