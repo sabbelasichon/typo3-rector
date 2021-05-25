d93c36dba1c64a4c0656186c127e1294adaa2fd1
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-05-25 22:14:41 +0200

    [TASK] Add new condition transformation adminUser (#2413)
    Resolves: #2411

---
+++ src/FileProcessor/TypoScript/Conditions/AdminUserConditionMatcher.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\FileProcessor\TypoScript\Conditions;
+
+use Nette\Utils\Strings;
+use Ssch\TYPO3Rector\Contract\FileProcessor\TypoScript\Conditions\TyposcriptConditionMatcher;
+
+final class AdminUserConditionMatcher implements TyposcriptConditionMatcher
+{
+    /**
+     * @var string
+     */
+    private const TYPE = 'adminUser';
+
+    public function change(string $condition): ?string
+    {
+        preg_match('#^' . self::TYPE . '\s*=\s*(?<value>[0-1])$#iUm', $condition, $matches);
+
+        if (! is_string($matches['value'])) {
+            return $condition;
+        }
+
+        $value = (int) $matches['value'];
+
+        if (1 === $value) {
+            return 'backend.user.isAdmin';
+        }
+
+        return 'backend.user.isAdmin == 0';
+    }
+
+    public function shouldApply(string $condition): bool
+    {
+        return Strings::startsWith($condition, self::TYPE);
+    }
+}
