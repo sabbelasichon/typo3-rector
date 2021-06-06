212b6cc86ed3bc85cedfe1a0359be432ec35b2be
Sebastian Schreiber <me@schreibersebastian.de>
2021-06-06 22:53:25 +0200

    [TASK] Refactor AdditionalFieldProvider
    Resolves: #2435

---
+++ stubs/TYPO3/CMS/Scheduler/AbstractAdditionalFieldProvider.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+
+namespace TYPO3\CMS\Scheduler;
+
+if(class_exists('TYPO3\CMS\Scheduler\AbstractAdditionalFieldProvider')) {
+    return;
+}
+
+abstract class AbstractAdditionalFieldProvider
+{
+
+}
---
+++ stubs/TYPO3/CMS/Scheduler/AdditionalFieldProviderInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Scheduler;
+
+use TYPO3\CMS\Scheduler\Controller\SchedulerModuleController;
+
+if(interface_exists('TYPO3\CMS\Scheduler\AdditionalFieldProviderInterface')) {
+    return;
+}
+
+interface AdditionalFieldProviderInterface
+{
+    public function getAdditionalFields(array &$taskInfo, $task, SchedulerModuleController $schedulerModule);
+}
