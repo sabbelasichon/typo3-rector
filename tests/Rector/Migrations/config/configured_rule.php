a4ed6396d2c916bf5e7f412e59db838b6f4a0dbc
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-06-24 14:18:42 +0200

    [TASK] Change old class names even if defined in string (#2472)
    Resolves: #2431

--- tests/Rector/Migrations/config/configured_rule.php
+++ tests/Rector/Migrations/config/configured_rule.php
@@ -21,6 +21,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             RenameClassMapAliasRector::CLASS_ALIAS_MAPS => [
                 __DIR__ . '/../../../../Migrations/TYPO3/9.5/typo3/sysext/fluid/Migrations/Code/ClassAliasMap.php',
                 __DIR__ . '/../../../../Migrations/TYPO3/9.5/typo3/sysext/core/Migrations/Code/ClassAliasMap.php',
+                __DIR__ . '/../../../../Migrations/Code/ClassAliasMap.php',
             ],
         ]]);
 
