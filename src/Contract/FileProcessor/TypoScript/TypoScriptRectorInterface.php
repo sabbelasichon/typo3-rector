a995e3dbe5a245a0cbe7aecef5ebc446c933c078
Sebastian Schreiber <me@schreibersebastian.de>
2021-08-10 19:08:57 +0200

    Extend Visitor explicitly
    Relates: #2510

--- src/Contract/FileProcessor/TypoScript/TypoScriptRectorInterface.php
+++ src/Contract/FileProcessor/TypoScript/TypoScriptRectorInterface.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace Ssch\TYPO3Rector\Contract\FileProcessor\TypoScript;
 
-use Helmich\TypoScriptParser\Parser\Traverser\Visitor;
 use Rector\Core\Contract\Rector\RectorInterface;
 
-interface TypoScriptRectorInterface extends Visitor, RectorInterface
+interface TypoScriptRectorInterface extends RectorInterface
 {
 }
