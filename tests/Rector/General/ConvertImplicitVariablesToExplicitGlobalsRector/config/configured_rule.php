aa0e84dd7c6ef43afb6323a6d6dd1093084b6b99
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-09-13 22:03:18 +0200

    [TASK] Use $GLOBALS['TBE_MODULES'] instead of $TBE_MODULES (#2565)
    Resolves: #2548

--- tests/Rector/General/ConvertImplicitVariablesToExplicitGlobalsRector/config/configured_rule.php
+++ tests/Rector/General/ConvertImplicitVariablesToExplicitGlobalsRector/config/configured_rule.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use Ssch\TYPO3Rector\Rector\General\ConvertTypo3ConfVarsRector;
+use Ssch\TYPO3Rector\Rector\General\ConvertImplicitVariablesToExplicitGlobalsRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../../config/config_test.php');
     $services = $containerConfigurator->services();
 
-    $services->set(ConvertTypo3ConfVarsRector::class);
+    $services->set(ConvertImplicitVariablesToExplicitGlobalsRector::class);
 };
