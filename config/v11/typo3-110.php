f73b5dcfcee5d06345bb22220b3b88af78f29cbb
Sebastian Schreiber <me@schreibersebastian.de>
2021-09-23 11:39:57 +0200

    [TASK] Use php native string functions
    Resolves: #2574

--- config/v11/typo3-110.php
+++ config/v11/typo3-110.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Rector\Transform\Rector\StaticCall\StaticCallToFuncCallRector;
+use Rector\Transform\ValueObject\StaticCallToFuncCall;
 use Ssch\TYPO3Rector\Rector\v11\v0\DateTimeAspectInsteadOfGlobalsExecTimeRector;
 use Ssch\TYPO3Rector\Rector\v11\v0\ExtbaseControllerActionsMustReturnResponseInterfaceRector;
 use Ssch\TYPO3Rector\Rector\v11\v0\ForwardResponseInsteadOfForwardMethodRector;
@@ -11,6 +13,7 @@ use Ssch\TYPO3Rector\Rector\v11\v0\RemoveLanguageModeMethodsFromTypo3QuerySettin
 use Ssch\TYPO3Rector\Rector\v11\v0\SubstituteConstantsModeAndRequestTypeRector;
 use Ssch\TYPO3Rector\Rector\v11\v0\UniqueListFromStringUtilityRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../config.php');
@@ -23,4 +26,19 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(ExtbaseControllerActionsMustReturnResponseInterfaceRector::class);
     $services->set(SubstituteConstantsModeAndRequestTypeRector::class);
     $services->set(RemoveLanguageModeMethodsFromTypo3QuerySettingsRector::class);
+    $services = $containerConfigurator->services();
+    $services->set(StaticCallToFuncCallRector::class)
+        ->call('configure', [
+            [
+                StaticCallToFuncCallRector::STATIC_CALLS_TO_FUNCTIONS => ValueObjectInliner::inline([
+                    new StaticCallToFuncCall('TYPO3\CMS\Core\Utility\StringUtility', 'beginsWith', 'str_starts_with'),
+                    new StaticCallToFuncCall('TYPO3\CMS\Core\Utility\StringUtility', 'endsWith', 'str_ends_with'),
+                    new StaticCallToFuncCall(
+                        'TYPO3\CMS\Core\Utility\GeneralUtility',
+                        'isFirstPartOfStr',
+                        'str_starts_with'
+                    ),
+                ]),
+            ],
+        ]);
 };
