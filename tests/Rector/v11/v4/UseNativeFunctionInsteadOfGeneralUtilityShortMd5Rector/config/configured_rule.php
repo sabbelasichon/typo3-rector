5111edb5e9dc0611039ae68abbbb1d1ec0c4ddd2
GitHub Action <action@github.com>
2021-09-26 19:55:18 +0000

    [ci-review] Rector Rectify

--- tests/Rector/v11/v4/UseNativeFunctionInsteadOfGeneralUtilityShortMd5Rector/config/configured_rule.php
+++ tests/Rector/v11/v4/UseNativeFunctionInsteadOfGeneralUtilityShortMd5Rector/config/configured_rule.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use Ssch\TYPO3Rector\Rector\v11\v4\UseNativeFunctionInsteadOfGeneralUtilityShortMd5Rector;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../../../config/config_test.php');
 
     $services = $containerConfigurator->services();
-    $services->set(\Ssch\TYPO3Rector\Rector\v11\v4\UseNativeFunctionInsteadOfGeneralUtilityShortMd5Rector::class);
+    $services->set(UseNativeFunctionInsteadOfGeneralUtilityShortMd5Rector::class);
 };
