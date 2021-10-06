66a75b250d46c1b380b1e1674ce681d83d08825f
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-10-06 10:15:47 +0200

    [TASK] Use icons from subfolder (#2659)
    * [TASK] Use icons from subfolder
    
    Resolves: #2560
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

--- ecs.php
+++ ecs.php
@@ -38,6 +38,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::COMMON);
     $containerConfigurator->import(SetList::CLEAN_CODE);
 
+    $parameters->set(Option::PARALLEL, true);
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
         __DIR__ . '/tests',
