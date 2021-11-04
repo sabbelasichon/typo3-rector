a8c25e3a78f3b3aa6b32df175a76efb03cb52014
Henrik Elsner <helsner@dfau.de>
2021-11-04 12:17:32 +0100

    feat: switch behavior of ArrayUtility arrayDiff- methods
    Resolves: #2589

--- tests/Rector/v11/v3/SwitchBehaviorOfArrayUtilityMethodsRector/config/configured_rule.php
+++ tests/Rector/v11/v3/SwitchBehaviorOfArrayUtilityMethodsRector/config/configured_rule.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/../config.php');
+    $containerConfigurator->import(__DIR__ . '/../../../../../../config/config_test.php');
+
     $services = $containerConfigurator->services();
-    $services->set(\Ssch\TYPO3Rector\Rector\v11\v3\SubstituteMethodRmFromListOfGeneralUtilityRector::class);
     $services->set(\Ssch\TYPO3Rector\Rector\v11\v3\SwitchBehaviorOfArrayUtilityMethodsRector::class);
 };
