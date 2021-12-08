83e4eb922f3d6fb733e8893d1f870ec2731adcfb
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-12-08 13:56:39 +0100

    Remove usage of Stringy (#2746)
    * Remove usage of Stringy
    
    Resolves: #2745
    
    * Add symfony/string explicitly
    
    * [ci-review] Rector Rectify
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/Helper/StringUtility.php
+++ src/Helper/StringUtility.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Ssch\TYPO3Rector\Helper;
 
-use Stringy\Stringy;
+use function Symfony\Component\String\u;
 
 final class StringUtility
 {
@@ -12,12 +12,17 @@ final class StringUtility
     {
         $extensionName = substr($extensionName, $delimiterPosition + 1);
 
-        $stringy = new Stringy($extensionName);
-        $underScoredExtensionName = (string) $stringy->underscored()
-            ->toLowerCase()
-            ->humanize();
+        $stringy = u($extensionName);
+        $underscores = $stringy->snake();
+        $lower = $underscores->lower();
 
-        $underScoredExtensionName = ucwords($underScoredExtensionName);
+        $underScoredExtensionName = str_replace('_', ' ', $lower->toString());
+
+        $stringy = u($underScoredExtensionName);
+        $trimmed = $stringy->trim();
+        $uppercase = $trimmed->title();
+
+        $underScoredExtensionName = ucwords($uppercase->toString());
 
         return str_replace(' ', '', $underScoredExtensionName);
     }
