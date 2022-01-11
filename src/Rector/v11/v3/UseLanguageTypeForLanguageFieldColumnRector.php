69f182aaf1c54fb0c9c8b6de910b073ab763e18c
Sebastian Schreiber <me@schreibersebastian.de>
2022-01-11 10:27:15 +0100

    Add rector for language TCA type
    Resolves: #2590

---
+++ src/Rector/v11/v3/UseLanguageTypeForLanguageFieldColumnRector.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v11\v3;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+use Ssch\TYPO3Rector\Rector\Tca\AbstractTcaRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/11.3/Deprecation-94165-SysLanguageDatabaseTable.html
+ * @see \Ssch\TYPO3Rector\Tests\Rector\v11\v3\UseLanguageTypeForLanguageFieldColumnRector\UseLanguageTypeForLanguageFieldColumnRectorTest
+ */
+final class UseLanguageTypeForLanguageFieldColumnRector extends AbstractTcaRector
+{
+    private ?string $languageField = null;
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'use the new TCA type language instead of foreign_table => sys_language for selecting a records',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+return [
+    'ctrl' => [
+        'languageField' => 'sys_language_uid',
+    ],
+    'columns' => [
+        'sys_language_uid' => [
+            'exclude' => 1,
+            'label' => 'Language',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'foreign_table' => 'sys_language',
+                'foreign_table_where' => 'ORDER BY sys_language.title',
+                'eval' => 'int',
+                'items' => [
+                    [$_LLL_general . ':LGL.allLanguages', -1],
+                    [$_LLL_general . ':LGL.default_value', 0]
+
+                ],
+            ],
+        ],
+    ],
+];
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+return [
+    'ctrl' => [
+        'languageField' => 'sys_language_uid',
+    ],
+    'columns' => [
+        'sys_language_uid' => [
+            'exclude' => 1,
+            'label' => 'Language',
+            'config' => [
+                'type' => 'language'
+            ],
+        ],
+    ],
+];
+CODE_SAMPLE
+                ),
+
+            ]
+        );
+    }
+
+    public function refactor(Node $node): ?Node
+    {
+        $this->hasAstBeenChanged = false;
+        if (! $this->isFullTcaDefinition($node)) {
+            return null;
+        }
+
+        $ctrl = $this->extractSubArrayByKey($node, 'ctrl');
+        if (null === $ctrl) {
+            return null;
+        }
+
+        $value = $this->extractArrayValueByKey($ctrl, 'languageField');
+
+        if (! $value instanceof Node\Scalar\String_) {
+            return null;
+        }
+
+        $this->languageField = $this->valueResolver->getValue($value);
+
+        if (null === $this->languageField) {
+            return null;
+        }
+
+        // we found a tca definition of a full table. Process it as a whole:
+        $columns = $this->extractSubArrayByKey($node, 'columns');
+        if (null !== $columns) {
+            $this->refactorColumnList($columns);
+        }
+
+        return $this->hasAstBeenChanged ? $node : null;
+    }
+
+    protected function refactorColumn(Expr $columnName, Expr $columnTca): void
+    {
+        $column = $this->valueResolver->getValue($columnName);
+
+        if ($column !== $this->languageField) {
+            return;
+        }
+
+        $configuration = $this->extractArrayItemByKey($columnTca, 'config');
+
+        if (null === $configuration) {
+            return;
+        }
+
+        $foreignTable = $this->extractArrayItemByKey($configuration->value, 'foreign_table');
+
+        if (null === $foreignTable) {
+            return;
+        }
+
+        if (! $this->valueResolver->isValue($foreignTable->value, 'sys_language')) {
+            return;
+        }
+
+        $configuration->value = $this->nodeFactory->createArray([
+            'type' => 'language',
+        ]);
+
+        $this->hasAstBeenChanged = true;
+    }
+}
