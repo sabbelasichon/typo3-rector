1890a842375798928fdef6cb009a27a3eb0c80aa
GitHub Action <action@github.com>
2022-01-12 22:31:02 +0000

    [ci-review] Rector Rectify

--- tests/Rector/v11/v5/SubstituteGetIconFactoryAndGetPageRendererFromModuleTemplateRector/config/configured_rule.php
+++ tests/Rector/v11/v5/SubstituteGetIconFactoryAndGetPageRendererFromModuleTemplateRector/config/configured_rule.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+
 use Rector\Core\ValueObject\PhpVersion;
+use Ssch\TYPO3Rector\Rector\v11\v5\SubstituteGetIconFactoryAndGetPageRendererFromModuleTemplateRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -13,7 +15,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../../../config/config_test.php');
 
     $services = $containerConfigurator->services();
-    $services->set(
-        \Ssch\TYPO3Rector\Rector\v11\v5\SubstituteGetIconFactoryAndGetPageRendererFromModuleTemplateRector::class
-    );
+    $services->set(SubstituteGetIconFactoryAndGetPageRendererFromModuleTemplateRector::class);
 };
