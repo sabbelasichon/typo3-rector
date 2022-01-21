4be62c41cc0f8cdcdfdf4c116f1f6e9dda266b93
helsner <helsner@dfau.de>
2022-01-21 22:47:49 +0100

    feat: provide general inject replacement rector (#2793)
    * feat: provide general inject replacement rector
    
    Resolves: #2724
    
    * [ci-review] Rector Rectify
    
    * Fix phpstan errors
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Sebastian Schreiber <me@schreibersebastian.de>

---
+++ src/Rector/General/InjectMethodToConstructorInjectionRector.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\General;
+
+use PhpParser\Node;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Class_;
+use PHPStan\Type\ObjectType;
+use Rector\Core\NodeManipulator\ClassDependencyManipulator;
+use Rector\Core\Rector\AbstractRector;
+use Rector\PostRector\ValueObject\PropertyMetadata;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ApiOverview/DependencyInjection/Index.html
+ * @see \Ssch\TYPO3Rector\Tests\Rector\General\InjectMethodToConstructorInjectionRector\InjectMethodToConstructorInjectionRectorTest
+ */
+final class InjectMethodToConstructorInjectionRector extends AbstractRector
+{
+    public function __construct(
+        private ClassDependencyManipulator $classDependencyManipulator
+    ) {
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            '',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+namespace App\Service;
+use \TYPO3\CMS\Core\Cache\CacheManager;
+class Service
+{
+    private CacheManager $cacheManager;
+    public function injectCacheManager(CacheManager $cacheManager): void
+    {
+        $this->cacheManager = $cacheManager;
+    }
+}
+CODE_SAMPLE
+,
+                    <<<'CODE_SAMPLE'
+namespace App\Service;
+use \TYPO3\CMS\Core\Cache\CacheManager;
+class Service
+{
+    private CacheManager $cacheManager;
+    public function __construct(CacheManager $cacheManager)
+    {
+        $this->cacheManager = $cacheManager;
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * List of nodes this class checks, classes that implements \PhpParser\Node See beautiful map of all nodes
+     * https://github.com/rectorphp/rector/blob/master/docs/NodesOverview.md
+     *
+     * @return class-string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * Process Node of matched type
+     */
+    public function refactor(Node $node): ?Node
+    {
+        /** @var Class_ $node */
+        if ($this->shouldSkip($node)) {
+            return null;
+        }
+
+        $injectMethods = array_filter(
+            $node->getMethods(),
+            fn ($classMethod) => str_starts_with((string) $classMethod->name, 'inject')
+        );
+
+        if ([] === $injectMethods) {
+            return null;
+        }
+
+        foreach ($injectMethods as $injectMethod) {
+            $params = $injectMethod->getParams();
+            if ([] === $params) {
+                continue;
+            }
+
+            reset($params);
+
+            /** @var Param $param */
+            $param = current($params);
+
+            if (! $param->type instanceof FullyQualified) {
+                continue;
+            }
+
+            $paramName = $this->getName($param->var);
+
+            if (null === $paramName) {
+                continue;
+            }
+
+            $this->classDependencyManipulator->addConstructorDependency(
+                $node,
+                new PropertyMetadata($paramName, new ObjectType((string) $param->type), Class_::MODIFIER_PROTECTED)
+            );
+
+            $this->removeNodeFromStatements($node, $injectMethod);
+        }
+
+        return $node;
+    }
+
+    private function shouldSkip(Class_ $class): bool
+    {
+        return [] === $class->getMethods();
+    }
+}
