ba0e09724f29d48fc6589629ca998da84c1368c9
Tomas Votruba <tomas.vot@gmail.com>
2022-02-20 12:05:47 +0000

    update deps

--- utils/composer-packages/tests/Rector/AddPackageVersionRector/config/configured_rule.php
+++ utils/composer-packages/tests/Rector/AddPackageVersionRector/config/configured_rule.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 use Rector\Composer\ValueObject\PackageAndVersion;
 use Rector\Core\Configuration\Option;
+use Rector\Core\Configuration\ValueObjectInliner;
 use Ssch\TYPO3Rector\ComposerPackages\Rector\AddPackageVersionRector;
 use Ssch\TYPO3Rector\ComposerPackages\ValueObject\ExtensionVersion;
 use Ssch\TYPO3Rector\ComposerPackages\ValueObject\Typo3Version;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../config/config.php');
