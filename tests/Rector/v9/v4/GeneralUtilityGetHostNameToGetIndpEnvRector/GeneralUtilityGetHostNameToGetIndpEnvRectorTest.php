3f76d017351d40476b37357850b290654298597f
Sebastian Schreiber <me@schreibersebastian.de>
2022-02-27 11:08:23 +0100

    Use getIndpEnv('HTTP_HOST')
    Resolves: #1110

---
+++ tests/Rector/v9/v4/GeneralUtilityGetHostNameToGetIndpEnvRector/GeneralUtilityGetHostNameToGetIndpEnvRectorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v9\v4\GeneralUtilityGetHostNameToGetIndpEnvRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class GeneralUtilityGetHostNameToGetIndpEnvRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return Iterator<SmartFileInfo>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
