75bb28a4cbeb61ace2314d6bc375b2bbc2f5cfd4
Tomas Votruba <tomas.vot@gmail.com>
2022-05-11 01:18:33 +0300

    refactor AddIconsToReturnRector to IconArrayItemFactory

--- config/register-icon-to-icons-file/register-icon-to-icons-file.php
+++ config/register-icon-to-icons-file/register-icon-to-icons-file.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Rector\v11\v5\RegisterIconToIconFileRector;
-use Ssch\TYPO3Rector\Rector\v11\v5\RegisterIconToIconFileRector\AddIconsToReturnRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
 
-    $rectorConfig->rule(AddIconsToReturnRector::class);
     $rectorConfig->rule(RegisterIconToIconFileRector::class);
 };
