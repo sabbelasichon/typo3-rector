5b6afdf4e6f5ffc12ae8fc85e96bc52827aaf10a
Dominik Dörr <foss@dominikdoerr.de>
2022-05-11 17:55:33 +0200

    feat: migrate cols to size for TCA type none
    Size has been used as a fallback previously, so we need to drop this before renaming cols if both exist.
    
    See: https://docs.typo3.org/m/typo3/reference-tca/11.5/en-us/ColumnsConfig/Type/None/Properties/Size.html

---
+++ src/Rector/v12/v0/MigrateColsToSizeForTcaTypeNoneRector.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v12\v0;
+
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Scalar\String_;
+use Ssch\TYPO3Rector\Helper\TcaHelperTrait;
+use Ssch\TYPO3Rector\Rector\Tca\AbstractTcaRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Deprecation-97109-TCATypeNoneColsOption.html
+ * @see \Ssch\TYPO3Rector\Tests\Rector\v12\v0\MigrateColsToSizeForTcaTypeNoneRector\MigrateColsToSizeForTcaTypeNoneRectorTest
+ */
+final class MigrateColsToSizeForTcaTypeNoneRector extends AbstractTcaRector
+{
+    use TcaHelperTrait;
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Migrates option cols to size for TCA type none', [new CodeSample(
+            <<<'CODE_SAMPLE'
+'columns' => [
+    'aColumn' => [
+        'config' => [
+            'type' => 'none',
+            'cols' => 20,
+        ],
+    ],
+],
+CODE_SAMPLE
+            ,
+            <<<'CODE_SAMPLE'
+'columns' => [
+    'aColumn' => [
+        'config' => [
+            'type' => 'none',
+            'size' => 20,
+        ],
+    ],
+],
+CODE_SAMPLE
+        )]);
+    }
+
+    public function refactorColumn(Expr $columnName, Expr $columnTca): void
+    {
+        $configArray = $this->extractSubArrayByKey($columnTca, self::CONFIG);
+        if (! $configArray instanceof Array_) {
+            return;
+        }
+        
+        if (! $this->isConfigType($configArray, 'none')) {
+            return;
+        }
+
+        $arrayItemToChange = $this->extractArrayItemByKey($configArray, 'cols');
+        if(! $arrayItemToChange instanceof ArrayItem) {
+            return;
+        }
+
+        $arrayItemToRemove = $this->extractArrayItemByKey($configArray, 'size');
+        if ($arrayItemToRemove instanceof ArrayItem) {
+            $this->removeNode($arrayItemToRemove);
+        }
+
+        $arrayItemToChange->key = new String_('size');
+
+        $this->hasAstBeenChanged = true;
+    }
+}
