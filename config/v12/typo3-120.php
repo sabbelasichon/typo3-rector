ade53a90bf9c06b7a18e7c17c63d828eca959b4e
Dominik Dörr <foss@dominikdoerr.de>
2022-05-11 07:37:24 +0200

    feat: add namespace migration for TypoLinkCodecService

--- config/v12/typo3-120.php
+++ config/v12/typo3-120.php
@@ -3,9 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Ssch\TYPO3Rector\Rector\Migrations\RenameClassMapAliasRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\MigrateInternalTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
     $rectorConfig->rule(MigrateInternalTypeRector::class);
+    $rectorConfig
+        ->ruleWithConfiguration(RenameClassMapAliasRector::class, [
+            __DIR__ . '/../../Migrations/TYPO3/12.0/typo3/sysext/core/Migrations/Code/ClassAliasMap.php',
+        ]);
 };
