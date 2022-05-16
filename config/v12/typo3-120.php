4879a171339ca4db77360c6a39ad52300665221a
Dominik Dörr <26066118+dominikdoerr@users.noreply.github.com>
2022-05-16 08:43:40 +0200

    feat: migrate cols to size for TCA type none (#2970)

--- config/v12/typo3-120.php
+++ config/v12/typo3-120.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Rector\Migrations\RenameClassMapAliasRector;
+use Ssch\TYPO3Rector\Rector\v12\v0\MigrateColsToSizeForTcaTypeNoneRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\MigrateInternalTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
+    $rectorConfig->rule(MigrateColsToSizeForTcaTypeNoneRector::class);
     $rectorConfig->rule(MigrateInternalTypeRector::class);
 
     $rectorConfig
