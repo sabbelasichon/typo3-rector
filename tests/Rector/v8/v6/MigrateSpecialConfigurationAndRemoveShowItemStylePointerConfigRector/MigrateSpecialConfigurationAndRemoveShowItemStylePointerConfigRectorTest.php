e34d2fb760d85c4da2a26e9ec62792ed2dd769e6
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2022-05-22 23:37:02 +0200

    [BUGFIX] Use correct namespace for test files (#2988)

--- tests/Rector/v8/v6/MigrateSpecialConfigurationAndRemoveShowItemStylePointerConfigRector/MigrateSpecialConfigurationAndRemoveShowItemStylePointerConfigRectorTest.php
+++ tests/Rector/v8/v6/MigrateSpecialConfigurationAndRemoveShowItemStylePointerConfigRector/MigrateSpecialConfigurationAndRemoveShowItemStylePointerConfigRectorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ssch\TYPO3Rector\Tests\Rector\v8\v6\MigrateSpecialConfigurationAndRemoveShowItemStylePointerConfigRectorTest;
+namespace Ssch\TYPO3Rector\Tests\Rector\v8\v6\MigrateSpecialConfigurationAndRemoveShowItemStylePointerConfigRector;
 
 use Iterator;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
