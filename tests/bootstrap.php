ea2568ecbb0de6a440c04aa7e8419c711c238d8b
Tomas Votruba <tomas.vot@gmail.com>
2022-06-05 09:50:58 +0200

    add local build preload to allow testing of dev php-parser

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -24,3 +24,12 @@ $GLOBALS['TYPO3_CONF_VARS']['BE']['fileDenyPattern'] = '\\.(php[3-8]?|phpsh|phtm
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['foo'] = 'a:6:{s:9:"loginLogo";s:8:"logo.jpg";s:19:"loginHighlightColor";s:7:"#000000";s:20:"loginBackgroundImage";s:8:"logo.jpg";s:13:"loginFootnote";s:8:"Footnote";s:11:"backendLogo";s:0:"";s:14:"backendFavicon";s:0:"";}';
 // for dump() function
 Debugger::$maxDepth = 2;
+
+
+// autoload rector php-parser first with local paths
+// build preload file to autoload local php-parser instead of phpstan one, e.g. in case of early upgrade
+exec('php vendor/rector/rector-src/build/build-preload.php .');
+sleep(1);
+
+require __DIR__ . '/../preload.php';
+unlink(__DIR__ . '/../preload.php');
