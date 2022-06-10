f7587e096012564d76e201f97d147f9b10aef415
Henrik Elsner <helsner@dfau.de>
2022-06-10 22:48:13 +0200

    [FEATURE] Remove redundant ['interface']['always_description']
    Resolves: #2921

--- config/v12/tca-120.php
+++ config/v12/tca-120.php
@@ -10,4 +10,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
     $rectorConfig->rule(MigrateNullFlagRector::class);
     $rectorConfig->rule(MigrateRequiredFlagRector::class);
+    $rectorConfig->rule(\Ssch\TYPO3Rector\Rector\v12\v0\tca\RemoveTCAInterfaceAlwaysDescriptionRector::class);
 };
