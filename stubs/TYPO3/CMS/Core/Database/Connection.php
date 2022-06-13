c3a8fde4c046bb54c3ad20c5ce060f676183c57a
Henrik Elsner <helsner@dfau.de>
2022-06-13 22:45:55 +0200

    [FEATURE] Substitute ExpressionBuilder methods andX() & orX()
    Resolves: #2953

--- stubs/TYPO3/CMS/Core/Database/Connection.php
+++ stubs/TYPO3/CMS/Core/Database/Connection.php
@@ -2,11 +2,22 @@
 
 namespace TYPO3\CMS\Core\Database;
 
+use TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder;
+
 if (class_exists('TYPO3\CMS\Core\Database\Connection')) {
     return;
 }
 
 class Connection
 {
+    /** @var ExpressionBuilder */
+    protected $_expr;
 
+    /**
+     * @return ExpressionBuilder
+     */
+    public function getExpressionBuilder()
+    {
+        return $this->_expr;
+    }
 }
