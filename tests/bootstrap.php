02e7ff8f317c61ab1d30d9aafcfd448de538775c
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2022-06-18 19:16:47 +0200

    [TASK] Remove empty lines

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/bootstrap-phpstan.php';
 
-
-
 // autoload rector php-parser first with local paths
 // build preload file to autoload local php-parser instead of phpstan one, e.g. in case of early upgrade
 exec('php vendor/rector/rector-src/build/build-preload.php .');
