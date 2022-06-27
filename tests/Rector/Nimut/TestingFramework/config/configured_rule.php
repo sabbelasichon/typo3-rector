46b35100b6b3b7f8c8d0febc19e1f6ea2734f858
Sebastian Schreiber <me@schreibersebastian.de>
2022-06-27 22:02:48 +0200

    TASK: Remove deprecated sets

--- tests/Rector/Nimut/TestingFramework/config/configured_rule.php
+++ tests/Rector/Nimut/TestingFramework/config/configured_rule.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\Set\Typo3SetList;
+use Ssch\TYPO3Rector\Set\Extension\NimutTestingFrameworkSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->disableImportNames();
-    $rectorConfig->sets([Typo3SetList::NIMUT_TESTING_FRAMEWORK_TO_TYPO3_TESTING_FRAMEWORK]);
+    $rectorConfig->sets([NimutTestingFrameworkSetList::NIMUT_TESTING_FRAMEWORK_TO_TYPO3_TESTING_FRAMEWORK]);
 };
