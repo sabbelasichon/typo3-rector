d0b590c4d2f69b567490bcadc476a9e3f123921a
Sebastian Schreiber <me@schreibersebastian.de>
2022-07-02 03:47:00 +0200

    [TASK] Move RegisterIconToIconFileRector
    Resolves: #3056

--- config/register-icon-to-icons-file/register-icon-to-icons-file.php
+++ config/register-icon-to-icons-file/register-icon-to-icons-file.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\Rector\v11\v5\RegisterIconToIconFileRector;
+use Ssch\TYPO3Rector\Rector\v11\v4\RegisterIconToIconFileRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
