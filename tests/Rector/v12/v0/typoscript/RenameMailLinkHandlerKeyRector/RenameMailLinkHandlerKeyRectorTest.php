2dbf0f5625757f975cca95fc4d93914bb10564f1
Sebastian Schreiber <me@schreibersebastian.de>
2022-10-24 09:11:13 +0200

    TASK: Rename key from mail to email
    Resolves: #2961

---
+++ tests/Rector/v12/v0/typoscript/RenameMailLinkHandlerKeyRector/RenameMailLinkHandlerKeyRectorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v12\v0\typoscript\RenameMailLinkHandlerKeyRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class RenameMailLinkHandlerKeyRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @return Iterator<array<string>>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture', '*.typoscript');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
