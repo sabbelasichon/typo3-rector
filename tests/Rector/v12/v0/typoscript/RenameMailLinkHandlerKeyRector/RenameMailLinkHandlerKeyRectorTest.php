08ea3678e0f17533e33370575d17196de0788948
sabbelasichon <sabbelasichon@users.noreply.github.com>
2022-10-30 00:07:27 +0000

    [automated] Apply Coding Standard

--- tests/Rector/v12/v0/typoscript/RenameMailLinkHandlerKeyRector/RenameMailLinkHandlerKeyRectorTest.php
+++ tests/Rector/v12/v0/typoscript/RenameMailLinkHandlerKeyRector/RenameMailLinkHandlerKeyRectorTest.php
@@ -6,7 +6,6 @@ namespace Ssch\TYPO3Rector\Tests\Rector\v12\v0\typoscript\RenameMailLinkHandlerK
 
 use Iterator;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
-use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class RenameMailLinkHandlerKeyRectorTest extends AbstractRectorTestCase
 {
