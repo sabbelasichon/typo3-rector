c8ea130b5959cf4e641bed4e2b4153d19d23cfdf
Sebastian Schreiber <me@schreibersebastian.de>
2022-11-08 21:15:39 +0100

    TASK: Add some sensitive defaults for the config (#3207)

--- config/config_test.php
+++ config/config_test.php
@@ -3,10 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\Configuration\Typo3Option;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/config.php');
-    $rectorConfig->importNames();
-    $rectorConfig->phpstanConfig(Typo3Option::PHPSTAN_FOR_RECTOR_PATH);
 };
