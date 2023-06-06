85bfe3d6e6d9d88d03ae8920c591e954301e37a2
Philipp Kitzberger <coding@kitze.net>
2023-06-06 16:53:17 +0200

    [FEATURE] Provide rules to migrate v:uri.image and v:media.image

--- config/replace-vhs.php
+++ config/replace-vhs.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs\ReplaceFormatJsonEncodeFluidRector;
 use Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs\ReplaceLFluidRector;
+use Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs\ReplaceMediaImageFluidRector;
+use Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs\ReplaceUriImageFluidRector;
 use Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs\ReplaceVariableSetFluidRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/config.php');
     $rectorConfig->rule(ReplaceFormatJsonEncodeFluidRector::class);
     $rectorConfig->rule(ReplaceLFluidRector::class);
+    $rectorConfig->rule(ReplaceMediaImageFluidRector::class);
+    $rectorConfig->rule(ReplaceUriImageFluidRector::class);
     $rectorConfig->rule(ReplaceVariableSetFluidRector::class);
 };
