c5c0dd90d381b4f806b48014ffe71356a3c266ad
Philipp Kitzberger <coding@kitze.net>
2023-06-06 15:34:25 +0200

    [FEATURE] Provide rule to migrate v:l

--- config/replace-vhs.php
+++ config/replace-vhs.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs\ReplaceFormatJsonEncodeFluidRector;
+use Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs\ReplaceLFluidRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/config.php');
     $rectorConfig->rule(ReplaceFormatJsonEncodeFluidRector::class);
+    $rectorConfig->rule(ReplaceLFluidRector::class);
 };
