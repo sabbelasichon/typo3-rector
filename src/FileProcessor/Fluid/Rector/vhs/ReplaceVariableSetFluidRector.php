fb46b9d2b90241d61128b64401a6cbd3ad5932eb
Philipp Kitzberger <coding@kitze.net>
2023-06-15 10:21:29 +0200

    Tweak replace v:varible.set
    * Only migrate the simple cases by the 'strict' pattern
    * Print warning about left over cases

--- src/FileProcessor/Fluid/Rector/vhs/ReplaceVariableSetFluidRector.php
+++ src/FileProcessor/Fluid/Rector/vhs/ReplaceVariableSetFluidRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ssch\TYPO3Rector\FileProcessor\Fluid\Rector\vhs;
 
 use Nette\Utils\Strings;
+use Rector\Core\Console\Output\RectorOutputStyle;
 use Rector\Core\ValueObject\Application\File;
 use Ssch\TYPO3Rector\Contract\FileProcessor\Fluid\Rector\FluidRectorInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -15,18 +16,41 @@ final class ReplaceVariableSetFluidRector implements FluidRectorInterface
     /**
      * @var string
      */
-    private const PATTERN = '#(v|vhs):variable.set#imsU';
+    private const PATTERN_STRICT = '#(v|vhs):(variable.set)([\s(]name[:=]["\'])([a-z]+)(["\'])#imsU';
 
     /**
      * @var string
      */
-    private const REPLACEMENT = 'f:variable';
+    private const PATTERN_DOT = '#(v|vhs):(variable.set)([\s(]name[:=]["\'])([a-z\.]+)(["\'])#imsU';
+
+    /**
+     * @var string
+     */
+    private const PATTERN_LEFTOVERS = '#(v|vhs):(variable.set)#imsU';
+
+    /**
+     * @readonly
+     */
+    private RectorOutputStyle $rectorOutputStyle;
+
+    public function __construct(RectorOutputStyle $rectorOutputStyle)
+    {
+        $this->rectorOutputStyle = $rectorOutputStyle;
+    }
 
     public function transform(File $file): void
     {
         $content = $file->getFileContent();
 
-        $content = Strings::replace($content, self::PATTERN, self::REPLACEMENT);
+        $content = Strings::replace($content, self::PATTERN_STRICT, 'f:variable$3$4$5');
+
+        if (Strings::matchAll($content, self::PATTERN_DOT)) {
+            $this->rectorOutputStyle->warning('There\'s occurrences of v:variable.set that contain a dot in its name attribute and thus cannot be migrated to f:variable!');
+        }
+
+        if (Strings::matchAll($content, self::PATTERN_LEFTOVERS)) {
+            $this->rectorOutputStyle->warning('There\'s occurrences of v:variable.set that couldn\'t be migrated automatically. Migrate them manually!');
+        }
 
         $file->changeFileContent($content);
     }
