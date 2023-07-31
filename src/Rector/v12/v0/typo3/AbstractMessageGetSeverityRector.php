2cade62c49e8bb9b4f78eafb14ccb04122f8bab1
Sebastian Schreiber <me@schreibersebastian.de>
2023-07-31 15:23:28 +0200

    TASK: Fix non idempotent rector (#3441)
    * TASK: Fix idempotent rector
    
    Resolves: #3234
    
    * [ci-review] Rector Rectify
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/Rector/v12/v0/typo3/AbstractMessageGetSeverityRector.php
+++ src/Rector/v12/v0/typo3/AbstractMessageGetSeverityRector.php
@@ -6,6 +6,7 @@ namespace Ssch\TYPO3Rector\Rector\v12\v0\typo3;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
 use PHPStan\Type\ObjectType;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -35,6 +36,12 @@ final class AbstractMessageGetSeverityRector extends AbstractRector
         }
 
         if (! $this->isNames($node->name, ['getSeverity'])) {
+            return null;
+        }
+
+        $nextNode = $this->betterNodeFinder->resolveNextNode($node);
+
+        if ($nextNode instanceof Identifier && $this->isName($nextNode, 'value')) {
             return null;
         }
 
