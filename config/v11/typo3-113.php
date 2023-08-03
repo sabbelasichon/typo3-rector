11a25149cf04c4258ebb30cafadd34f1b548ff28
Henrik Elsner <helsner@dfau.de>
2023-08-03 21:08:37 +0200

    fix: add missed out rector rules to v11 config
    Resolves: #3447

--- config/v11/typo3-113.php
+++ config/v11/typo3-113.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Ssch\TYPO3Rector\Rector\v11\v3\RemoveBackendUtilityViewOnClickUsageRector;
+use Ssch\TYPO3Rector\Rector\v11\v3\ReplaceStdAuthCodeWithHmacRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SubstituteExtbaseRequestGetBaseUriRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SubstituteMethodRmFromListOfGeneralUtilityRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SwitchBehaviorOfArrayUtilityMethodsRector;
@@ -10,6 +12,8 @@ use Ssch\TYPO3Rector\Rector\v11\v3\typo3\UseNormalizedParamsToGetRequestUrlRecto
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
+    $rectorConfig->rule(RemoveBackendUtilityViewOnClickUsageRector::class);
+    $rectorConfig->rule(ReplaceStdAuthCodeWithHmacRector::class);
     $rectorConfig->rule(SubstituteMethodRmFromListOfGeneralUtilityRector::class);
     $rectorConfig->rule(SwitchBehaviorOfArrayUtilityMethodsRector::class);
     $rectorConfig->rule(SubstituteExtbaseRequestGetBaseUriRector::class);
