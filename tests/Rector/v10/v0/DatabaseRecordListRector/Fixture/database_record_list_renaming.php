38f1a9f7a9a7a8cf38f097a99f197cb56a8c13dc
Henrik Elsner <helsner@dfau.de>
2023-08-09 12:04:50 +0200

    fix: prevent double required config key in TCA (#3451)
    Resolves: #3450

--- tests/Rector/v10/v0/DatabaseRecordListRector/Fixture/database_record_list_renaming.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList;
-
-$databaseRecordList = GeneralUtility::makeInstance(DatabaseRecordList::class);
-$thumbCode = $databaseRecordList->thumbCode([], 'foo', 'bar');
-
-$uri = $databaseRecordList->requestUri();
