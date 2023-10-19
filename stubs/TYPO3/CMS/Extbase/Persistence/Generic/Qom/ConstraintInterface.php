42274087dd64c05b92e9f81e421259a7cc2b1e73
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2023-10-20 00:14:34 +0200

    [TASK] Add more stubs

---
+++ stubs/TYPO3/CMS/Extbase/Persistence/Generic/Qom/ConstraintInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Extbase\Persistence\Generic\Qom;
+
+if (interface_exists('TYPO3\CMS\Extbase\Persistence\Generic\Qom\ConstraintInterface')) {
+    return;
+}
+
+interface ConstraintInterface
+{
+
+}
--- stubs/TYPO3/CMS/Extbase/Persistence/Generic/Query.php
+++ stubs/TYPO3/CMS/Extbase/Persistence/Generic/Query.php
@@ -1,10 +1,12 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
 
 namespace TYPO3\CMS\Extbase\Persistence\Generic;
 
+use TYPO3\CMS\Extbase\Persistence\Generic\Qom\ConstraintInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 
 if (class_exists('TYPO3\CMS\Extbase\Persistence\Generic\Query')) {
     return;
@@ -12,12 +14,111 @@ if (class_exists('TYPO3\CMS\Extbase\Persistence\Generic\Query')) {
 
 final class Query implements QueryInterface
 {
+    /**
+     * @param bool $returnRawQueryResult
+     * @return QueryResultInterface|object[]
+     */
+    public function execute($returnRawQueryResult = false)
+    {
+    }
+
+    public function setOrderings(array $orderings)
+    {
+    }
+
+    public function setLimit($limit)
+    {
+    }
+
+    public function setOffset($offset)
+    {
+    }
+
+    public function matching($constraint)
+    {
+    }
 
     public function logicalAnd($constraint1)
     {
     }
 
     public function logicalOr($constraint1)
+    {
+    }
+
+    public function logicalNot(ConstraintInterface $constraint)
+    {
+    }
+
+    public function equals($propertyName, $operand, $caseSensitive = true)
+    {
+    }
+
+    public function like($propertyName, $operand)
+    {
+    }
+
+    public function contains($propertyName, $operand)
+    {
+    }
+
+    public function in($propertyName, $operand)
+    {
+    }
+
+    public function lessThan($propertyName, $operand)
+    {
+    }
+
+    public function lessThanOrEqual($propertyName, $operand)
+    {
+    }
+
+    public function greaterThan($propertyName, $operand)
+    {
+    }
+
+    public function greaterThanOrEqual($propertyName, $operand)
+    {
+    }
+
+    public function setType(string $type): void
+    {
+    }
+
+    public function getType()
+    {
+    }
+
+    public function setQuerySettings(QuerySettingsInterface $querySettings)
+    {
+    }
+
+    public function getQuerySettings()
+    {
+    }
+
+    public function count()
+    {
+    }
+
+    public function getOrderings()
+    {
+    }
+
+    public function getLimit()
+    {
+    }
+
+    public function getOffset()
+    {
+    }
+
+    public function getConstraint()
+    {
+    }
+
+    public function getStatement()
     {
     }
 }
