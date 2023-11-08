0460161b68040b7a4bc16b25226e84dc16fc66b6
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2023-11-08 11:08:27 +0100

    [BUGFIX] Import correct config file in test

--- tests/Rector/v12/v0/typo3/ReplacePreviewUrlMethodRector/config/configured_rule.php
+++ tests/Rector/v12/v0/typo3/ReplacePreviewUrlMethodRector/config/configured_rule.php
@@ -6,6 +6,6 @@ use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Rector\v12\v0\typo3\ReplacePreviewUrlMethodRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../../../../../../config/config_test.php');
+    $rectorConfig->import(__DIR__ . '/../../../../../../../config/config_test.php');
     $rectorConfig->rule(ReplacePreviewUrlMethodRector::class);
 };
