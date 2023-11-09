2fbea29262be16e11a1bc00c9b01feac330c1c15
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2023-11-09 01:04:51 +0100

    [TASK] Add MigrateEvalIntAndDouble2ToTypeNumberRector
    Resolves #3552

--- tests/Rector/v12/v0/flexform/MigrateEvalIntAndDouble2ToTypeNumberFlexFormRector/config/configured_rule.php
+++ tests/Rector/v12/v0/flexform/MigrateEvalIntAndDouble2ToTypeNumberFlexFormRector/config/configured_rule.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateEvalIntAndDouble2ToTypeNumberFlexFormRector;
-use Ssch\TYPO3Rector\Rector\v12\v0\flexform\RemoveElementTceFormsRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../config.php');
-    $rectorConfig->rule(RemoveElementTceFormsRector::class);
+    $rectorConfig->import(__DIR__ . '/../../../../../../../config/config_test.php');
     $rectorConfig->rule(MigrateEvalIntAndDouble2ToTypeNumberFlexFormRector::class);
 };
---
+++ tests/Rector/v12/v0/tca/MigrateEvalIntAndDouble2ToTypeNumberRector/MigrateEvalIntAndDouble2ToTypeNumberRectorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v12\v0\tca\MigrateEvalIntAndDouble2ToTypeNumberRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class MigrateEvalIntAndDouble2ToTypeNumberRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @return Iterator<array<string>>
+     */
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
