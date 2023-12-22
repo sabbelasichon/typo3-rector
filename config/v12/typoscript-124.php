3e2b822c6983a79166263978ee98e327019d992a
Henrik Elsner <helsner@dfau.de>
2023-12-22 16:16:54 +0100

    [FEATURE] Provide migration for typoscript xhtmlDoctype config
    Resolves: #3616

---
+++ config/v12/typoscript-124.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../config.php');
+    $rectorConfig->rule(\Ssch\TYPO3Rector\Rector\v12\v4\typoscript\MigrateXhtmlDoctypeRector::class);
+};
