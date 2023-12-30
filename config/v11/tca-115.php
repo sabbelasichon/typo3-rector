96c4cefe1ba05350365bb49a950d28567be85f9f
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2023-12-30 18:20:24 +0100

    [FEATURE] Add SimplifyCheckboxItemsTCARector

---
+++ config/v11/tca-115.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Ssch\TYPO3Rector\Rector\v11\v5\tca\SimplifyCheckboxItemsTCARector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../config.php');
+    $rectorConfig->rule(SimplifyCheckboxItemsTCARector::class);
+};
