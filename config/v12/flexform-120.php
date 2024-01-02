b07a6e7dcb9ffea01cd7458addbeae474c01b85a
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-01-02 23:16:04 +0100

    [FEATURE] Add MigrateRenderTypeColorpickerToTypeColorFlexFormRector
    Resolves: #3558

--- config/v12/flexform-120.php
+++ config/v12/flexform-120.php
@@ -7,6 +7,7 @@ use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateEvalIntAndDouble2ToTypeNumber
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateInternalTypeFolderToTypeFolderFlexFormRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateNullFlagFlexFormRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigratePasswordAndSaltedPasswordToPasswordTypeFlexFormRector;
+use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateRenderTypeColorpickerToTypeColorFlexFormRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\RemoveElementTceFormsRector;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -15,5 +16,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(MigrateInternalTypeFolderToTypeFolderFlexFormRector::class);
     $rectorConfig->rule(MigrateNullFlagFlexFormRector::class);
     $rectorConfig->rule(MigratePasswordAndSaltedPasswordToPasswordTypeFlexFormRector::class);
+    $rectorConfig->rule(MigrateRenderTypeColorpickerToTypeColorFlexFormRector::class);
     $rectorConfig->rule(RemoveElementTceFormsRector::class);
 };
---
+++ src/Rector/v12/v0/flexform/MigrateRenderTypeColorpickerToTypeColorFlexFormRector.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Rector\v12\v0\flexform;
+
+use DOMDocument;
+use DOMElement;
+use DOMNodeList;
+use DOMXPath;
+use Ssch\TYPO3Rector\Contract\FileProcessor\FlexForms\Rector\FlexFormRectorInterface;
+use Ssch\TYPO3Rector\Helper\ArrayUtility;
+use Ssch\TYPO3Rector\Helper\FlexFormHelperTrait;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-97271-NewTCATypeColor.html
+ * @see \Ssch\TYPO3Rector\Tests\Rector\v12\v0\flexform\MigrateRenderTypeColorpickerToTypeColorFlexFormRector\MigrateRenderTypeColorpickerToTypeColorFlexFormRectorTest
+ */
+final class MigrateRenderTypeColorpickerToTypeColorFlexFormRector implements FlexFormRectorInterface
+{
+    use FlexFormHelperTrait;
+
+    private bool $domDocumentHasBeenChanged = false;
+
+    public function transform(DOMDocument $domDocument): bool
+    {
+        $xpath = new DOMXPath($domDocument);
+
+        /** @var DOMNodeList<DOMElement> $elements */
+        $elements = $xpath->query('//config');
+
+        if ($elements->count() === 0) {
+            return false;
+        }
+
+        foreach ($elements as $element) {
+            $this->refactorColumn($domDocument, $element);
+        }
+
+        return $this->domDocumentHasBeenChanged;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Migrate renderType colorpicker to type color', [new CodeSample(
+            <<<'CODE_SAMPLE'
+<T3DataStructure>
+    <ROOT>
+        <sheetTitle>aTitle</sheetTitle>
+        <type>array</type>
+        <el>
+            <a_color_field>
+                <label>Color field</label>
+                <config>
+                    <type>input</type>
+                    <renderType>colorpicker</renderType>
+                    <required>1</required>
+                    <size>20</size>
+                    <max>1234</max>
+                    <eval>trim,null</eval>
+                    <valuePicker>
+                        <items type="array">
+                            <numIndex index="0" type="array">
+                                <numIndex index="0">typo3 orange</numIndex>
+                                <numIndex index="1">#FF8700</numIndex>
+                            </numIndex>
+                        </items>
+                    </valuePicker>
+                </config>
+            </a_color_field>
+        </el>
+    </ROOT>
+</T3DataStructure>
+CODE_SAMPLE
+            ,
+            <<<'CODE_SAMPLE'
+<T3DataStructure>
+    <ROOT>
+        <sheetTitle>aTitle</sheetTitle>
+        <type>array</type>
+        <el>
+            <a_color_field>
+                <label>Color field</label>
+                <config>
+                    <type>color</type>
+                    <required>1</required>
+                    <size>20</size>
+                    <valuePicker>
+                        <items type="array">
+                            <numIndex index="0" type="array">
+                                <numIndex index="0">typo3 orange</numIndex>
+                                <numIndex index="1">#FF8700</numIndex>
+                            </numIndex>
+                        </items>
+                    </valuePicker>
+                </config>
+            </a_color_field>
+        </el>
+    </ROOT>
+</T3DataStructure>
+CODE_SAMPLE
+        )]);
+    }
+
+    private function refactorColumn(DOMDocument $domDocument, ?DOMElement $configElement): void
+    {
+        if (! $configElement instanceof DOMElement) {
+            return;
+        }
+
+        if (! $this->isConfigType($configElement, 'input')) {
+            return;
+        }
+
+        if (! $this->configIsOfRenderType($configElement, 'colorpicker')) {
+            return;
+        }
+
+        // Set the TCA type to "color"
+        $this->changeTcaType($domDocument, $configElement, 'color');
+
+        // Remove 'max' and 'renderType' config
+        $this->removeChildElementFromDomElementByKey($configElement, 'max');
+        $this->removeChildElementFromDomElementByKey($configElement, 'renderType');
+
+        $evalDomElement = $this->extractDomElementByKey($configElement, 'eval');
+        if (! $evalDomElement instanceof DOMElement) {
+            return;
+        }
+
+        $evalListValue = $evalDomElement->nodeValue;
+        if (! is_string($evalListValue)) {
+            return;
+        }
+
+        $evalList = ArrayUtility::trimExplode(',', $evalListValue, true);
+
+        if (in_array('null', $evalList, true)) {
+            // Set "eval" to "null", since it's currently defined and the only allowed "eval" for type=color
+            $evalDomElement->nodeValue = '';
+            $evalDomElement->appendChild($domDocument->createTextNode('null'));
+        } elseif ($evalDomElement->parentNode instanceof DOMElement) {
+            // 'eval' is empty, remove whole configuration
+            $evalDomElement->parentNode->removeChild($evalDomElement);
+        }
+
+        $this->domDocumentHasBeenChanged = true;
+    }
+}
