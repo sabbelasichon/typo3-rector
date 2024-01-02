704c3a1f3103c22443ef9195abe354c84d3c4d20
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-01-02 22:33:06 +0100

    [FEATURE] Add MigrateRenderTypeColorpickerToTypeColorRector
    Resolves: #97271

--- config/v12/tca-120.php
+++ config/v12/tca-120.php
@@ -9,6 +9,7 @@ use Ssch\TYPO3Rector\Rector\v12\v0\tca\MigrateInputDateTimeRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\tca\MigrateInternalTypeFolderToTypeFolderRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\tca\MigrateNullFlagRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\tca\MigratePasswordAndSaltedPasswordToPasswordTypeRector;
+use Ssch\TYPO3Rector\Rector\v12\v0\tca\MigrateRenderTypeColorpickerToTypeColorRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\tca\MigrateRequiredFlagRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\tca\MigrateToEmailTypeRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\tca\RemoveCruserIdRector;
@@ -18,14 +19,15 @@ use Ssch\TYPO3Rector\Rector\v12\v0\tca\RemoveTCAInterfaceAlwaysDescriptionRector
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
     $rectorConfig->rule(MigrateColsToSizeForTcaTypeNoneRector::class);
+    $rectorConfig->rule(MigrateEvalIntAndDouble2ToTypeNumberRector::class);
     $rectorConfig->rule(MigrateInputDateTimeRector::class);
     $rectorConfig->rule(MigrateInternalTypeFolderToTypeFolderRector::class);
     $rectorConfig->rule(MigrateNullFlagRector::class);
     $rectorConfig->rule(MigratePasswordAndSaltedPasswordToPasswordTypeRector::class);
+    $rectorConfig->rule(MigrateRenderTypeColorpickerToTypeColorRector::class);
     $rectorConfig->rule(MigrateRequiredFlagRector::class);
     $rectorConfig->rule(MigrateToEmailTypeRector::class);
-    $rectorConfig->rule(RemoveTCAInterfaceAlwaysDescriptionRector::class);
     $rectorConfig->rule(RemoveCruserIdRector::class);
     $rectorConfig->rule(RemoveTableLocalPropertyRector::class);
-    $rectorConfig->rule(MigrateEvalIntAndDouble2ToTypeNumberRector::class);
+    $rectorConfig->rule(RemoveTCAInterfaceAlwaysDescriptionRector::class);
 };
