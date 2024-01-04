2177354a3513f436d019f24e0c1d2c491cf8b632
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-01-04 17:22:38 +0100

    [FEATURE] Add MigrateEmailFlagToEmailTypeFlexFormRector
    Resolves: #3362

--- config/v12/flexform-120.php
+++ config/v12/flexform-120.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateEmailFlagToEmailTypeFlexFormRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateEvalIntAndDouble2ToTypeNumberFlexFormRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateInternalTypeFolderToTypeFolderFlexFormRector;
 use Ssch\TYPO3Rector\Rector\v12\v0\flexform\MigrateNullFlagFlexFormRector;
@@ -12,6 +13,7 @@ use Ssch\TYPO3Rector\Rector\v12\v0\flexform\RemoveElementTceFormsRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
+    $rectorConfig->rule(MigrateEmailFlagToEmailTypeFlexFormRector::class);
     $rectorConfig->rule(MigrateEvalIntAndDouble2ToTypeNumberFlexFormRector::class);
     $rectorConfig->rule(MigrateInternalTypeFolderToTypeFolderFlexFormRector::class);
     $rectorConfig->rule(MigrateNullFlagFlexFormRector::class);
