c3e7db0b7495ee342c0a57d8dcffda558cf4366b
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-01-23 00:22:57 +0100

    [FEATURE] Removed eval=int from TCA type "datetime"
    Resolves: #2923

--- src/Rector/v12/v0/tca/MigrateInputDateTimeRector.php
+++ src/Rector/v12/v0/tca/MigrateInputDateTimeRector.php
@@ -7,6 +7,7 @@ namespace Ssch\TYPO3Rector\Rector\v12\v0\tca;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Scalar\LNumber;
 use PhpParser\Node\Scalar\String_;
 use Ssch\TYPO3Rector\Helper\ArrayUtility;
@@ -17,6 +18,7 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
  * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-97232-NewTCATypeDatetime.html
+ * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-97358-RemovedEvalintFromTCATypeDatetime.html
  * @see \Ssch\TYPO3Rector\Tests\Rector\v12\v0\tca\MigrateInputDateTimeRector\MigrateInputDateTimeRectorTest
  */
 final class MigrateInputDateTimeRector extends AbstractTcaRector
@@ -100,43 +102,27 @@ CODE_SAMPLE
         }
 
         // Early return in case column is not of type=input with renderType=inputDateTime
-        if (! $this->isConfigType($configArray, 'input')) {
-            return;
-        }
-
-        if (! $this->configIsOfRenderType($configArray, self::INPUT_DATE_TIME)) {
+        if (! $this->isConfigType($configArray, 'input')
+            || ! $this->configIsOfRenderType($configArray, self::INPUT_DATE_TIME)
+        ) {
             return;
         }
 
         // Set the TCA type to "datetime"
         $this->changeTcaType($configArray, 'datetime');
 
-        // Remove 'renderType' => 'inputDateTime',
-        $renderTypeArrayItem = $this->extractArrayItemByKey($configArray, 'renderType');
-
-        if (! $renderTypeArrayItem instanceof ArrayItem) {
-            return;
-        }
-
-        $renderTypeValue = $this->valueResolver->getValue($renderTypeArrayItem->value);
-
-        if (! is_string($renderTypeValue)) {
-            return;
-        }
-
-        $this->removeNode($renderTypeArrayItem);
-
-        // Remove 'max' config
-        $maxArrayItem = $this->extractArrayItemByKey($configArray, 'max');
-        if ($maxArrayItem instanceof ArrayItem) {
-            $this->removeNode($maxArrayItem);
-        }
+        // Remove "renderType" and "max"
+        // Note: Also remove "format". This option had been documented but was actually
+        //       never used in the FormEngine element. This migration will set it according
+        //       to the corresponding "eval" value.
+        $this->removeArrayItemFromArrayByKey($configArray, 'renderType');
+        $this->removeArrayItemFromArrayByKey($configArray, 'max');
+        $this->removeArrayItemFromArrayByKey($configArray, 'format');
 
         $evalList = [];
         $evalArrayItem = $this->extractArrayItemByKey($configArray, 'eval');
         if ($evalArrayItem instanceof ArrayItem) {
             $evalString = $this->valueResolver->getValue($evalArrayItem->value);
-
             if (! is_string($evalString)) {
                 return;
             }
@@ -145,12 +131,6 @@ CODE_SAMPLE
 
             // Remove 'eval' config
             $this->removeNode($evalArrayItem);
-        }
-
-        // Remove format config, if set
-        $formatArrayItem = $this->extractArrayItemByKey($configArray, 'format');
-        if ($formatArrayItem instanceof ArrayItem) {
-            $this->removeNode($formatArrayItem);
         }
 
         // Set the "format" based on "eval"
@@ -165,35 +145,62 @@ CODE_SAMPLE
             }
         }
 
+        if (in_array('null', $evalList, true)) {
+            // Set "eval" to "null", since it's currently defined and the only allowed "eval" for type=datetime
+            $configArray->items[] = new ArrayItem(new String_('null'), new String_('eval'));
+        } else {
+            $this->removeArrayItemFromArrayByKey($configArray, 'eval');
+        }
+
         // Removes option [config][default], if the default is the native "empty" value
 
         $defaultArrayItem = $this->extractArrayItemByKey($configArray, 'default');
-
         if ($defaultArrayItem instanceof ArrayItem) {
             $dbTypeValue = null;
-            $defaultValue = $this->valueResolver->getValue($defaultArrayItem->value);
             $dbTypeArrayItem = $this->extractArrayItemByKey($configArray, 'dbType');
-
             if ($dbTypeArrayItem instanceof ArrayItem) {
                 $dbTypeValue = $this->valueResolver->getValue($dbTypeArrayItem->value);
             }
 
+            $defaultValue = $this->valueResolver->getValue($defaultArrayItem->value);
             if (in_array($dbTypeValue, self::DATETIME_TYPES, true)) {
                 if ($defaultValue === self::DATETIME_EMPTY_VALUES[$dbTypeValue]['empty']) {
+                    // Remove default for native datetime fields if the default is the native "empty" value
                     $this->removeNode($defaultArrayItem);
                 }
-            } elseif (! is_int($defaultValue)) {
-                if ($defaultValue === '') {
-                    // Always use int as default (string values are no longer supported for "datetime")
-                    $defaultArrayItem->value = new LNumber(0);
-                } elseif (self::canBeInterpretedAsInteger($defaultValue)) {
-                    // Cast default to int, in case it can be interpreted as integer
-                    $defaultArrayItem->value = new LNumber((int) $defaultValue);
-                } else {
-                    // Unset default in case it's a no longer supported string
-                    $this->removeNode($defaultArrayItem);
-                }
+
+                $this->hasAstBeenChanged = true;
+                return;
             }
+
+            if (is_int($defaultValue)) {
+                $this->hasAstBeenChanged = true;
+                return;
+            }
+
+            if ($defaultValue === '') {
+                // Always use int as default (string values are no longer supported for "datetime")
+                $defaultArrayItem->value = new LNumber(0);
+                $this->hasAstBeenChanged = true;
+                return;
+            }
+
+            if (self::canBeInterpretedAsInteger($defaultValue)) {
+                // Cast default to int, in case it can be interpreted as integer
+                $defaultArrayItem->value = new LNumber((int) $defaultValue);
+                $this->hasAstBeenChanged = true;
+                return;
+            }
+
+            if ($defaultArrayItem->value instanceof FuncCall
+                && $this->getName($defaultArrayItem->value) === 'time'
+            ) {
+                $this->hasAstBeenChanged = true;
+                return;
+            }
+
+            // Unset default in case it's a no longer supported string
+            $this->removeNode($defaultArrayItem);
         }
 
         $this->hasAstBeenChanged = true;
