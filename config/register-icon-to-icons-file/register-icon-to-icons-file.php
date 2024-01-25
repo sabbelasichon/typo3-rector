05ee536b5d587d1f11e0df2d48fa308ec940bd01
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-25 23:42:10 +0100

    TASK: Fix IconsRector

--- config/register-icon-to-icons-file/register-icon-to-icons-file.php
+++ config/register-icon-to-icons-file/register-icon-to-icons-file.php
@@ -6,9 +6,7 @@ use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Rector\v11\v4\RegisterIconToIconFileRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    global $services;
     $rectorConfig->import(__DIR__ . '/../config.php');
 
-    $rectorConfig->services()
-        ->set(RegisterIconToIconFileRector::class)->tag('typo3_rector.icon_rectors');
+    $rectorConfig->rule(RegisterIconToIconFileRector::class);
 };
--- config/v8/typo3-83.php
+++ config/v8/typo3-83.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\FileProcessor\Resources\Icons\IconsFileProcessor;
 use Ssch\TYPO3Rector\FileProcessor\Resources\Icons\Rector\v8\v3\IconsRector;
 use Ssch\TYPO3Rector\Rector\v8\v3\RefactorMethodFileContentRector;
 use Ssch\TYPO3Rector\Rector\v8\v3\RefactorQueryViewTableWrapRector;
@@ -15,6 +14,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(RefactorQueryViewTableWrapRector::class);
 
     $services = $rectorConfig->services();
-    $services->set(IconsFileProcessor::class)->autowire();
-    $services->set(IconsRector::class);
+    $services->set(IconsRector::class)->tag('typo3_rector.icon_rectors');
 };
