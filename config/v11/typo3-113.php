2b347b60b0dba5cf354bebe6b640906041c8d527
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-26 22:15:33 +0100

    TASK: Remove ReplaceStdAuthCodeWithHmacRector

--- config/v11/typo3-113.php
+++ config/v11/typo3-113.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Rector\v11\v3\RemoveBackendUtilityViewOnClickUsageRector;
-use Ssch\TYPO3Rector\Rector\v11\v3\ReplaceStdAuthCodeWithHmacRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SubstituteExtbaseRequestGetBaseUriRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SubstituteMethodRmFromListOfGeneralUtilityRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SwitchBehaviorOfArrayUtilityMethodsRector;
@@ -13,7 +12,6 @@ use Ssch\TYPO3Rector\Rector\v11\v3\typo3\UseNormalizedParamsToGetRequestUrlRecto
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
     $rectorConfig->rule(RemoveBackendUtilityViewOnClickUsageRector::class);
-    $rectorConfig->rule(ReplaceStdAuthCodeWithHmacRector::class);
     $rectorConfig->rule(SubstituteMethodRmFromListOfGeneralUtilityRector::class);
     $rectorConfig->rule(SwitchBehaviorOfArrayUtilityMethodsRector::class);
     $rectorConfig->rule(SubstituteExtbaseRequestGetBaseUriRector::class);
