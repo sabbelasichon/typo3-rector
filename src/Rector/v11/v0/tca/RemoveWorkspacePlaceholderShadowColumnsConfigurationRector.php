589e8b8062235314276a975aa9ea46f20cc2e60e
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-26 23:15:20 +0100

    TASK: Do not use method removeNode

--- src/Rector/v11/v0/tca/RemoveWorkspacePlaceholderShadowColumnsConfigurationRector.php
+++ src/Rector/v11/v0/tca/RemoveWorkspacePlaceholderShadowColumnsConfigurationRector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Ssch\TYPO3Rector\Rector\v11\v0\tca;
 
 use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Expr\ArrayItem;
 use Ssch\TYPO3Rector\Rector\Tca\AbstractTcaRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -43,22 +42,7 @@ CODE_SAMPLE
 
     protected function refactorCtrl(Array_ $ctrlArray): void
     {
-        $shadowColumnsForNewPlaceholdersArrayItem = $this->extractArrayItemByKey(
-            $ctrlArray,
-            'shadowColumnsForNewPlaceholders'
-        );
-        if ($shadowColumnsForNewPlaceholdersArrayItem instanceof ArrayItem) {
-            $this->removeNode($shadowColumnsForNewPlaceholdersArrayItem);
-            $this->hasAstBeenChanged = true;
-        }
-
-        $shadowColumnsForMovePlaceholdersArrayItem = $this->extractArrayItemByKey(
-            $ctrlArray,
-            'shadowColumnsForMovePlaceholders'
-        );
-        if ($shadowColumnsForMovePlaceholdersArrayItem instanceof ArrayItem) {
-            $this->removeNode($shadowColumnsForMovePlaceholdersArrayItem);
-            $this->hasAstBeenChanged = true;
-        }
+        $this->hasAstBeenChanged = $this->removeArrayItemFromArrayByKey($ctrlArray, 'shadowColumnsForNewPlaceholders');
+        $this->hasAstBeenChanged = $this->removeArrayItemFromArrayByKey($ctrlArray, 'shadowColumnsForMovePlaceholders');
     }
 }
