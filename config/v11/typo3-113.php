0771b8ef2a5d88b9afd05a5f6a85bf2f65b1fa5d
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-26 22:23:38 +0100

    TASK: Remove RemoveBackendUtilityViewOnClickUsageRector

--- config/v11/typo3-113.php
+++ config/v11/typo3-113.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\Rector\v11\v3\RemoveBackendUtilityViewOnClickUsageRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SubstituteExtbaseRequestGetBaseUriRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SubstituteMethodRmFromListOfGeneralUtilityRector;
 use Ssch\TYPO3Rector\Rector\v11\v3\SwitchBehaviorOfArrayUtilityMethodsRector;
@@ -11,7 +10,6 @@ use Ssch\TYPO3Rector\Rector\v11\v3\typo3\UseNormalizedParamsToGetRequestUrlRecto
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
-    $rectorConfig->rule(RemoveBackendUtilityViewOnClickUsageRector::class);
     $rectorConfig->rule(SubstituteMethodRmFromListOfGeneralUtilityRector::class);
     $rectorConfig->rule(SwitchBehaviorOfArrayUtilityMethodsRector::class);
     $rectorConfig->rule(SubstituteExtbaseRequestGetBaseUriRector::class);
