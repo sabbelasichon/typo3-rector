8f29fe80a740337231e5b4bbda8db2ce85ba7239
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-26 22:27:25 +0100

    TASK: Remove RefactorTypeInternalTypeFileAndFileReferenceToFalRector

--- config/v9/tca-95.php
+++ config/v9/tca-95.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Rector\v9\v0\RemoveOptionLocalizeChildrenAtParentLocalizationRector;
-use Ssch\TYPO3Rector\Rector\v9\v5\RefactorTypeInternalTypeFileAndFileReferenceToFalRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
     $rectorConfig->rule(RemoveOptionLocalizeChildrenAtParentLocalizationRector::class);
-    $rectorConfig->rule(RefactorTypeInternalTypeFileAndFileReferenceToFalRector::class);
 };
