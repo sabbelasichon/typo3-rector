865dae943947f9f63fdec76e4147db67c38a7b93
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-26 17:25:55 +0100

    TASK: Cleanup config

--- config/config.php
+++ config/config.php
@@ -2,12 +2,8 @@
 
 declare(strict_types=1);
 
-use PhpParser\PrettyPrinter\Standard;
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\Configuration\Typo3Option;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->disableParallel();
@@ -17,11 +13,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importShortClasses(false);
 
     $services = $rectorConfig->services();
-    $services->defaults()
-        ->public()
-        ->autowire();
-
-    $services->set(Filesystem::class);
 
     $services->load('Ssch\\TYPO3Rector\\', __DIR__ . '/../src')
         ->exclude([
@@ -30,9 +21,4 @@ return static function (RectorConfig $rectorConfig): void {
             __DIR__ . '/../src/Set',
             __DIR__ . '/../src/ValueObject',
         ]);
-
-    $services->set(BufferedOutput::class);
-    $services->alias(OutputInterface::class, BufferedOutput::class);
-
-    $services->set(Standard::class);
 };
