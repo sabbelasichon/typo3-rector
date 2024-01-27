d599d12d7830f9b7c9a4e988fbbfad99b9908d31
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-27 23:36:25 +0100

    TASK: Fix ClassAliasMaps

---
+++ stubs/TYPO3/CMS/Backend/View/ArrayBrowser.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace TYPO3\CMS\Backend\View;
+
+if (class_exists('TYPO3\CMS\Backend\View\ArrayBrowser')) {
+    return;
+}
+
+final class ArrayBrowser
+{
+
+}
