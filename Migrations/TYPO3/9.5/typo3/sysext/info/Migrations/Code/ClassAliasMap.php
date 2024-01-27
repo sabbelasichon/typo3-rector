e570164dadbf804b3d6e07f0a53c2135033c7b05
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-27 23:17:29 +0100

    TASK: Remove Migrations for v8 and v10

--- Migrations/TYPO3/9.5/typo3/sysext/info/Migrations/Code/ClassAliasMap.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'TYPO3\\CMS\\Frontend\\Controller\\PageInformationController' => 'TYPO3\CMS\Info\Controller\PageInformationController',
-    'TYPO3\\CMS\\Frontend\\Controller\\TranslationStatusController' => 'TYPO3\CMS\Info\Controller\TranslationStatusController',
-    'TYPO3\\CMS\\InfoPagetsconfig\\Controller\\InfoPageTyposcriptConfigController' => 'TYPO3\CMS\Info\Controller\InfoPageTyposcriptConfigController',
-];
