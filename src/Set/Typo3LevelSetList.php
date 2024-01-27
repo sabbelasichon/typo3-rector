f8479f214be753be8d4ff0b9f9f58ccc81e8dfb7
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-27 21:35:24 +0100

    TASK: Remove all rules lower TYPO3 Version 10

--- src/Set/Typo3LevelSetList.php
+++ src/Set/Typo3LevelSetList.php
@@ -20,19 +20,4 @@ final class Typo3LevelSetList
      * @var string
      */
     public const UP_TO_TYPO3_10 = __DIR__ . '/../../config/level/up-to-typo3-10.php';
-
-    /**
-     * @var string
-     */
-    public const UP_TO_TYPO3_9 = __DIR__ . '/../../config/level/up-to-typo3-9.php';
-
-    /**
-     * @var string
-     */
-    public const UP_TO_TYPO3_8 = __DIR__ . '/../../config/level/up-to-typo3-8.php';
-
-    /**
-     * @var string
-     */
-    public const UP_TO_TYPO3_7 = __DIR__ . '/../../config/level/up-to-typo3-7.php';
 }
