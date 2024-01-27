dc71707ab58feb2c473749e4e3c020c29127d223
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-27 21:37:17 +0100

    TASK: Cleanup SetList.

--- config/level/up-to-typo3-10.php
+++ config/level/up-to-typo3-10.php
@@ -3,9 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
 use Ssch\TYPO3Rector\Set\Typo3SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([Typo3LevelSetList::UP_TO_TYPO3_9, Typo3SetList::TYPO3_104]);
+    $rectorConfig->sets([Typo3SetList::TYPO3_104]);
 };
--- src/Set/Typo3SetList.php
+++ src/Set/Typo3SetList.php
@@ -14,42 +14,10 @@ final class Typo3SetList
     /**
      * @var string
      */
-    public const TCA_104 = __DIR__ . '/../../config/v10/tca-104.php';
-
-    /**
-     * @var string
-     */
     public const TYPO3_11 = __DIR__ . '/../../config/typo3-11.php';
-
-    /**
-     * @var string
-     * @deprecated Use TCA_114 instead.
-     */
-    public const TCA_110 = self::TCA_114;
-
-    /**
-     * @var string
-     * @deprecated Use TCA_114 instead.
-     */
-    public const TCA_113 = self::TCA_114;
-
-    /**
-     * @var string
-     */
-    public const TCA_114 = __DIR__ . '/../../config/v11/tca-114.php';
 
     /**
      * @var string
      */
     public const TYPO3_12 = __DIR__ . '/../../config/typo3-12.php';
-
-    /**
-     * @var string
-     */
-    public const TCA_120 = __DIR__ . '/../../config/v12/tca-120.php';
-
-    /**
-     * @var string
-     */
-    public const TCA_123 = __DIR__ . '/../../config/v12/tca-123.php';
 }
