699648d06b73faa7c02c3cd6e68034bbd9eda5b6
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-27 21:35:24 +0100

    TASK: Remove all rules lower TYPO3 Version 10

--- tests/Rector/v8/v7/BackendUtilityGetRecordRawRector/Fixture/backend_utility_get_record_raw.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use TYPO3\CMS\Backend\Utility\BackendUtility;
-
-$table = 'fe_users';
-$where = 'uid > 5';
-$fields = ['uid', 'pid'];
-$record = BackendUtility::getRecordRaw($table, $where, $fields);
--- tests/Rector/v9/v0/InjectAnnotationRector/Source/InjectionClass.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Ssch\TYPO3Rector\Tests\Rector\v9\v0\InjectAnnotationRector\Source;
-
-final class InjectionClass
-{
-}
