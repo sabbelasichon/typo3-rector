299ca214da6f9acffd418b694850c8a8acdf9fa0
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-30 18:33:47 +0100

    [TASK] Use AbstractTcaRector

--- src/Rector/v12/v0/tca/RemoveTCAInterfaceAlwaysDescriptionRector.php
+++ src/Rector/v12/v0/tca/RemoveTCAInterfaceAlwaysDescriptionRector.php
@@ -8,10 +8,7 @@ use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\ArrayItem;
-use PhpParser\Node\Stmt\Return_;
-use Rector\PhpParser\Node\Value\ValueResolver;
-use Rector\Rector\AbstractRector;
-use Ssch\TYPO3Rector\Rector\Tca\TcaHelperTrait;
+use Ssch\TYPO3Rector\Rector\Tca\AbstractTcaRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -19,76 +16,8 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-97312-RemoveContextSensitiveHelp.html
  * @see \Ssch\TYPO3Rector\Tests\Rector\v12\v0\tca\RemoveTCAInterfaceAlwaysDescriptionRector\RemoveTCAInterfaceAlwaysDescriptionRectorTest
  */
-final class RemoveTCAInterfaceAlwaysDescriptionRector extends AbstractRector
+final class RemoveTCAInterfaceAlwaysDescriptionRector extends AbstractTcaRector
 {
-    use TcaHelperTrait;
-
-    private ValueResolver $valueResolver;
-
-    public function __construct(ValueResolver $valueResolver)
-    {
-        $this->valueResolver = $valueResolver;
-    }
-
-    /**
-     * @return array<class-string<Node>>
-     */
-    public function getNodeTypes(): array
-    {
-        return [Return_::class];
-    }
-
-    /**
-     * @param Return_ $node
-     */
-    public function refactor(Node $node): ?Node
-    {
-        if (! $this->isFullTca($node)) {
-            return null;
-        }
-
-        $interface = $this->extractInterface($node);
-
-        if (! $interface instanceof ArrayItem) {
-            return null;
-        }
-
-        $interfaceItems = $interface->value;
-
-        if (! $interfaceItems instanceof Array_) {
-            if ($node->expr instanceof Array_) {
-                $this->removeArrayItemFromArrayByKey($node->expr, 'interface');
-            }
-
-            return null;
-        }
-
-        $remainingInterfaceItems = count($interfaceItems->items);
-
-        foreach ($interfaceItems->items as $interfaceItemKey => $interfaceItem) {
-            if (! $interfaceItem instanceof ArrayItem) {
-                continue;
-            }
-
-            if (! $interfaceItem->key instanceof Expr) {
-                continue;
-            }
-
-            if ($this->valueResolver->isValue($interfaceItem->key, 'always_description')) {
-                unset($interfaceItems->items[$interfaceItemKey]);
-                --$remainingInterfaceItems;
-                break;
-            }
-        }
-
-        if ($remainingInterfaceItems === 0 && $node->expr instanceof Array_) {
-            $this->removeArrayItemFromArrayByKey($node->expr, 'interface');
-            return $node;
-        }
-
-        return null;
-    }
-
     /**
      * @codeCoverageIgnore
      */
@@ -112,5 +41,32 @@ return [
 ];
 CODE_SAMPLE
         )]);
+    }
+
+    protected function refactorInterface(Array_ $interfaceArray, Node $node): void
+    {
+        $interfaceItems = $interfaceArray;
+        $remainingInterfaceItems = count($interfaceItems->items);
+
+        foreach ($interfaceItems->items as $interfaceItemKey => $interfaceItem) {
+            if (! $interfaceItem instanceof ArrayItem) {
+                continue;
+            }
+
+            if (! $interfaceItem->key instanceof Expr) {
+                continue;
+            }
+
+            if ($this->valueResolver->isValue($interfaceItem->key, 'always_description')) {
+                unset($interfaceItems->items[$interfaceItemKey]);
+                --$remainingInterfaceItems;
+                break;
+            }
+        }
+
+        if ($remainingInterfaceItems === 0 && $node instanceof Array_) {
+            $this->removeArrayItemFromArrayByKey($node, 'interface');
+            $this->hasAstBeenChanged = true;
+        }
     }
 }
