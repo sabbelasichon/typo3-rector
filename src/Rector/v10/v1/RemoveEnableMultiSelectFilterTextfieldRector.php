8a748f8f4ecdb7512e4681459845fb2f848ce1ae
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-30 18:45:23 +0100

    [TASK] Move AbstractTcaRector out of Tca subnamespace

--- src/Rector/v10/v1/RemoveEnableMultiSelectFilterTextfieldRector.php
+++ src/Rector/v10/v1/RemoveEnableMultiSelectFilterTextfieldRector.php
@@ -6,7 +6,7 @@ namespace Ssch\TYPO3Rector\Rector\v10\v1;
 
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Array_;
-use Ssch\TYPO3Rector\Rector\Tca\AbstractTcaRector;
+use Ssch\TYPO3Rector\Rector\AbstractTcaRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
