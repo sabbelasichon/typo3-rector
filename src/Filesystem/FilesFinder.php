283221e74bae979cd6b3c7e74d8cab7d55d5f63c
Sebastian Schreiber <me@schreibersebastian.de>
2024-01-30 20:46:10 +0100

    [TASK] Cleanup FilesFinder

--- src/Filesystem/FilesFinder.php
+++ src/Filesystem/FilesFinder.php
@@ -4,36 +4,8 @@ declare(strict_types=1);
 
 namespace Ssch\TYPO3Rector\Filesystem;
 
-use Rector\Testing\PHPUnit\StaticPHPUnitEnvironment;
-use Symfony\Component\Finder\SplFileInfo;
-
 final class FilesFinder
 {
-    /**
-     * @var int
-     */
-    private const MAX_DIRECTORY_LEVELS_UP = 6;
-
-    /**
-     * @var string
-     */
-    private const EXT_EMCONF_FILENAME = 'ext_emconf.php';
-
-    /**
-     * @readonly
-     */
-    private FileInfoFactory $fileInfoFactory;
-
-    public function __construct(FileInfoFactory $fileInfoFactory)
-    {
-        $this->fileInfoFactory = $fileInfoFactory;
-    }
-
-    public function findExtEmConfRelativeFromGivenFileInfo(SplFileInfo $fileInfo): ?SplFileInfo
-    {
-        return $this->findFileRelativeFromGivenFileInfo($fileInfo, self::EXT_EMCONF_FILENAME);
-    }
-
     public function isExtLocalConf(string $filePath): bool
     {
         return $this->fileEqualsName($filePath, 'ext_localconf.php');
@@ -42,59 +14,6 @@ final class FilesFinder
     public function isExtTables(string $filePath): bool
     {
         return $this->fileEqualsName($filePath, 'ext_tables.php');
-    }
-
-    public function isExtEmconf(string $filePath): bool
-    {
-        return $this->fileEqualsName($filePath, self::EXT_EMCONF_FILENAME);
-    }
-
-    private function findFileRelativeFromGivenFileInfo(SplFileInfo $fileInfo, string $filename): ?SplFileInfo
-    {
-        // special case for tests
-        if (StaticPHPUnitEnvironment::isPHPUnitRun()) {
-            return $fileInfo;
-        }
-
-        $currentDirectory = dirname($fileInfo->getRealPath());
-
-        $smartFileInfo = $this->createSmartFileInfoIfFileExistsInCurrentDirectory($currentDirectory, $filename);
-
-        if ($smartFileInfo instanceof SplFileInfo) {
-            return $smartFileInfo;
-        }
-
-        // Test some levels up.
-        $currentDirectoryLevel = 1;
-
-        while ($currentDirectory = dirname($fileInfo->getPath(), $currentDirectoryLevel)) {
-            $smartFileInfo = $this->createSmartFileInfoIfFileExistsInCurrentDirectory($currentDirectory, $filename);
-
-            if ($smartFileInfo instanceof SplFileInfo) {
-                return $smartFileInfo;
-            }
-
-            if ($currentDirectoryLevel > self::MAX_DIRECTORY_LEVELS_UP) {
-                break;
-            }
-
-            ++$currentDirectoryLevel;
-        }
-
-        return null;
-    }
-
-    private function createSmartFileInfoIfFileExistsInCurrentDirectory(
-        string $currentDirectory,
-        string $filename
-    ): ?SplFileInfo {
-        $filePath = sprintf('%s/%s', $currentDirectory, $filename);
-
-        if (is_file($filePath)) {
-            return $this->fileInfoFactory->createFileInfoFromPath($filePath);
-        }
-
-        return null;
     }
 
     private function fileEqualsName(string $filePath, string $fileName): bool
