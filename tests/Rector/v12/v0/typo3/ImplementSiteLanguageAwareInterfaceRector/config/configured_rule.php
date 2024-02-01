4673241af557cbe5c343943539a150e466b6870f
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-01 14:47:45 +0100

    [TASK] Remove superflous config_test.php file

--- tests/Rector/v12/v0/typo3/ImplementSiteLanguageAwareInterfaceRector/config/configured_rule.php
+++ tests/Rector/v12/v0/typo3/ImplementSiteLanguageAwareInterfaceRector/config/configured_rule.php
@@ -6,6 +6,6 @@ use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\TYPO312\v0\ImplementSiteLanguageAwareInterfaceRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../../../../../../../config/config_test.php');
+    $rectorConfig->import(__DIR__ . '/../../../../../../../config/config.php');
     $rectorConfig->rule(ImplementSiteLanguageAwareInterfaceRector::class);
 };
