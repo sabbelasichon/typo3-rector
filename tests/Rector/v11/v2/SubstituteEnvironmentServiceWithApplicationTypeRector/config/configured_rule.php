8a93979450c6232bb306afdb5f366db2fe6f33c8
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-01 16:40:02 +0100

    [TASK] Move tca or type rectors one directory above

---
+++ tests/Rector/v11/v2/SubstituteEnvironmentServiceWithApplicationTypeRector/config/configured_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Ssch\TYPO3Rector\TYPO311\v2\SubstituteEnvironmentServiceWithApplicationTypeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
+    $rectorConfig->rule(SubstituteEnvironmentServiceWithApplicationTypeRector::class);
+};
