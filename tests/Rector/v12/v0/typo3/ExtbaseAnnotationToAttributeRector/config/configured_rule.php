ee1330dd0ec6b32f6617622056fb6ca85cc18e46
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-01 14:02:01 +0100

    TASK: Fix Attribute conversion

--- tests/Rector/v12/v0/typo3/ExtbaseAnnotationToAttributeRector/config/configured_rule.php
+++ tests/Rector/v12/v0/typo3/ExtbaseAnnotationToAttributeRector/config/configured_rule.php
@@ -6,5 +6,6 @@ use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../../../config/config_test.php');
+    $rectorConfig->phpVersion(\Rector\ValueObject\PhpVersion::PHP_80);
     $rectorConfig->rule(\Ssch\TYPO3Rector\TYPO312\v0\ExtbaseAnnotationToAttributeRector::class);
 };
