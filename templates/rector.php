e5a9d328395d82b8ce0df614dcec257f2f972afe
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-01 14:43:24 +0100

    [BUGFIX] Fix rector template configuration

--- templates/rector.php
+++ templates/rector.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
+use Rector\ValueObject\PhpVersion;
 use Ssch\TYPO3Rector\Set\Typo3SetList;
 
 return static function (RectorConfig $rectorConfig): void {
