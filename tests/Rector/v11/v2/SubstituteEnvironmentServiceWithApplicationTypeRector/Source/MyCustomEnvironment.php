e0ef28d821f085c894ad2347161f2a955f41f5f4
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-01 16:40:02 +0100

    [TASK] Move tca or type rectors one directory above

---
+++ tests/Rector/v11/v2/SubstituteEnvironmentServiceWithApplicationTypeRector/Source/MyCustomEnvironment.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\Tests\Rector\v11\v2\SubstituteEnvironmentServiceWithApplicationTypeRector\Source;
+
+final class MyCustomEnvironment
+{
+    public function isEnvironmentInFrontendMode(): bool
+    {
+        return false;
+    }
+
+    public function isEnvironmentInBackendMode(): bool
+    {
+        return false;
+    }
+}
