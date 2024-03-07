5671fdfdd2c59ffde9bd8cf9e0988750817cfae1
Aleksandra Fey <aleksuroshevska@gmail.com>
2024-03-07 16:46:57 +0100

    [FEATURE] Add Rule for use strict types in Extbase
    Resolves: #4074

---
+++ stubs/TYPO3/CMS/Extbase/Persistence/Generic/ObjectStorage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TYPO3\CMS\Extbase\Persistence\Generic;
+
+if (class_exists('TYPO3\CMS\Extbase\Persistence\Generic\ObjectStorage')) {
+    return;
+}
+
+class ObjectStorage
+{
+
+}
