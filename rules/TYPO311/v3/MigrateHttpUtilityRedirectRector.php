a8339d419c0b5dd72be31ec385a9d73ad5d091b6
Sebastian Schreiber <me@schreibersebastian.de>
2024-03-08 10:04:32 +0100

    [TASK] Fix minor issues

--- rules/TYPO311/v3/MigrateHttpUtilityRedirectRector.php
+++ rules/TYPO311/v3/MigrateHttpUtilityRedirectRector.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace Ssch\TYPO3Rector\TYPO311\v3;
 
+use PhpParser\BuilderFactory;
 use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Nop;
+use PhpParser\Node\Stmt\Throw_;
 use PHPStan\Type\ObjectType;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
-use TYPO3\CMS\Core\Http\PropagateResponseException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\HttpUtility;
 
@@ -21,6 +25,13 @@ use TYPO3\CMS\Core\Utility\HttpUtility;
  */
 final class MigrateHttpUtilityRedirectRector extends AbstractRector
 {
+    private BuilderFactory $builderFactory;
+
+    public function __construct(BuilderFactory $builderFactory)
+    {
+        $this->builderFactory = $builderFactory;
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Migrate HttpUtilty::redirect() to responseFactory', [new CodeSample(
@@ -34,9 +45,10 @@ CODE_SAMPLE
 use Psr\Http\Message\ResponseFactoryInterface;
 use TYPO3\CMS\Core\Http\PropagateResponseException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\HttpUtility;
 
 $response = GeneralUtility::makeInstance(ResponseFactoryInterface::class)
-    ->createResponse(303)
+    ->createResponse(HttpUtility::HTTP_STATUS_303)
     ->withAddedHeader('location', 'https://example.com');
 throw new PropagateResponseException($response);
 CODE_SAMPLE
@@ -71,32 +83,39 @@ CODE_SAMPLE
             return null;
         }
 
-        $parameters = $staticCall->args;
-        $target = $parameters[0];
-        $code = $parameters[1] ?? $this->nodeFactory->createClassConstFetch(HttpUtility::class, 'HTTP_STATUS_303');
-
-        $newNode = $this->nodeFactory->createStaticCall(
-            GeneralUtility::class,
-            'makeInstance',
-            [$this->nodeFactory->createClassConstReference('Psr\Http\Message\ResponseFactoryInterface')]
+        $target = $staticCall->args[0];
+        $httpStatusCode = $staticCall->args[1] ?? $this->nodeFactory->createClassConstFetch(
+            HttpUtility::class,
+            'HTTP_STATUS_303'
         );
-        $createResponseCallNode = $this->nodeFactory->createMethodCall($newNode, 'createResponse', [$code]);
+
+        $createResponseCallNode = $this->nodeFactory->createMethodCall(
+            $this->createResponseFactory(),
+            'createResponse',
+            [$httpStatusCode]
+        );
         $withHeaderCallNode = $this->nodeFactory->createMethodCall($createResponseCallNode, 'withAddedHeader', [
             'location',
             $target,
         ]);
 
-        $responseVariable = new Node\Expr\Variable('response');
-        $assignment = new Expression(new Node\Expr\Assign($responseVariable, $withHeaderCallNode));
+        $responseVariable = new Variable('response');
+        $assignment = new Expression(new Assign($responseVariable, $withHeaderCallNode));
 
-        $exception =
-            new Node\Stmt\Throw_(
-                new Node\Expr\New_(
-                    new Node\Name\FullyQualified(PropagateResponseException::class),
-                    [new Node\Arg($responseVariable)]
-                )
-            );
+        $exception = new Throw_($this->builderFactory->new(
+            '\\TYPO3\\CMS\\Core\\Http\\PropagateResponseException',
+            [$responseVariable]
+        ));
 
-        return [new Node\Stmt\Nop(), $assignment, $exception];
+        return [new Nop(), $assignment, $exception];
+    }
+
+    private function createResponseFactory(): StaticCall
+    {
+        return $this->nodeFactory->createStaticCall(
+            GeneralUtility::class,
+            'makeInstance',
+            [$this->nodeFactory->createClassConstReference('Psr\\Http\\Message\\ResponseFactoryInterface')]
+        );
     }
 }
