32ec2baa2da66c169e9f648ebcb52d62b77754cc
Reiner Kempkes <kempkes.reiner@gmail.com>
2024-03-08 13:37:02 +0100

    [FEATURE] Introduce capabilities bit set
    Resolves: #3661

---
+++ rules/TYPO313/v0/IntroduceCapabilitiesBitSetRector.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\TYPO313\v0;
+
+use PhpParser\Node;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/13.0/Breaking-101291-IntroduceCapabilitiesBitSet.html
+ * @see \Ssch\TYPO3Rector\Tests\Rector\v13\v0\IntroduceCapabilitiesBitSetRector\IntroduceCapabilitiesBitSetRectorTest
+ */
+final class IntroduceCapabilitiesBitSetRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Introduce capabilities bit set', [new CodeSample(
+            <<<'CODE_SAMPLE'
+use TYPO3\CMS\Core\Resource\ResourceStorageInterface;
+
+echo ResourceStorageInterface::CAPABILITY_BROWSABLE;
+echo ResourceStorageInterface::CAPABILITY_PUBLIC;
+echo ResourceStorageInterface::CAPABILITY_WRITABLE;
+echo ResourceStorageInterface::CAPABILITY_HIERARCHICAL_IDENTIFIERS;
+CODE_SAMPLE
+            ,
+            <<<'CODE_SAMPLE'
+use TYPO3\CMS\Core\Resource\Capabilities;
+use TYPO3\CMS\Core\Resource\ResourceStorageInterface;
+
+echo Capabilities::CAPABILITY_BROWSABLE;
+echo Capabilities::CAPABILITY_PUBLIC;
+echo Capabilities::CAPABILITY_WRITABLE;
+echo Capabilities::CAPABILITY_HIERARCHICAL_IDENTIFIERS;
+CODE_SAMPLE
+        )]);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [];
+    }
+
+    public function refactor(Node $node)
+    {
+        return null;
+    }
+}
---
+++ stubs/TYPO3/CMS/Core/Resource/Capabilities.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TYPO3\CMS\Core\Resource;
+
+interface Capabilities
+{
+
+    public const CAPABILITY_BROWSABLE = 'CAPABILITY_BROWSABLE';
+    public const CAPABILITY_PUBLIC = 'CAPABILITY_PUBLIC';
+    public const CAPABILITY_WRITABLE = 'CAPABILITY_WRITABLE';
+    public const CAPABILITY_HIERARCHICAL_IDENTIFIERS = 'CAPABILITY_HIERARCHICAL_IDENTIFIERS';
+
+}
