6d9421fafd209b22d711b0585848a5096990c28a
Sebastian Schreiber <me@schreibersebastian.de>
2024-03-08 19:50:26 +0100

    [FEATURE] Add PARAM_* from Connection class (#4172)
    Resolves: #4148

--- tests/Rector/v13/v0/FileInterfaceStrictTypesRector/config/configured_rule.php
+++ tests/Rector/v13/v0/FileInterfaceStrictTypesRector/config/configured_rule.php
@@ -2,15 +2,7 @@
 
 declare(strict_types=1);
 
-use PHPStan\Type\BooleanType;
-use PHPStan\Type\MixedType;
-use PHPStan\Type\StringType;
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
-use Rector\TypeDeclaration\ValueObject\AddParamTypeDeclaration;
-use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
-use TYPO3\CMS\Core\Resource\FileInterface;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
--- tests/Rector/v13/v0/ResourceInterfaceStrictTypesRector/config/configured_rule.php
+++ tests/Rector/v13/v0/ResourceInterfaceStrictTypesRector/config/configured_rule.php
@@ -2,12 +2,7 @@
 
 declare(strict_types=1);
 
-use PHPStan\Type\ObjectType;
-use PHPStan\Type\StringType;
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
-use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
-use TYPO3\CMS\Core\Resource\ResourceInterface;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../../config/config.php');
