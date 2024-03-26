1658eebf29f9e1688c817c4300bb2c82c7e6bba0
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-03-26 23:42:51 +0100

    [BUGFIX] Fix typo in Rector Rule title

--- rules/TYPO310/v2/ExcludeServiceKeysToArrayRector.php
+++ rules/TYPO310/v2/ExcludeServiceKeysToArrayRector.php
@@ -73,7 +73,7 @@ final class ExcludeServiceKeysToArrayRector extends AbstractRector
 
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('Change parameter $excludeServiceKeys explicity to an array', [
+        return new RuleDefinition('Change parameter $excludeServiceKeys explicitly to an array', [
             new CodeSample(
                 <<<'CODE_SAMPLE'
 GeneralUtility::makeInstanceService('serviceType', 'serviceSubType', 'key1, key2');
