7f77fd480cb61667b7fc5eea259cd8fa853b2aa5
Sebastian Schreiber <me@schreibersebastian.de>
2024-04-16 11:53:25 +0200

    [TASK] Optimize extension key resolution

--- src/ComposerExtensionKeyResolver.php
+++ src/ComposerExtensionKeyResolver.php
@@ -6,6 +6,7 @@ namespace Ssch\TYPO3Rector;
 
 use Rector\ValueObject\Application\File;
 use Ssch\TYPO3Rector\Contract\FilesystemInterface;
+use Ssch\TYPO3Rector\Filesystem\FilesFinder;
 
 final class ComposerExtensionKeyResolver
 {
@@ -14,18 +15,31 @@ final class ComposerExtensionKeyResolver
      */
     private FilesystemInterface $filesystem;
 
-    public function __construct(FilesystemInterface $filesystem)
+    /**
+     * @readonly
+     */
+    private FilesFinder $filesFinder;
+
+    public function __construct(FilesystemInterface $filesystem, FilesFinder $filesFinder)
     {
         $this->filesystem = $filesystem;
+        $this->filesFinder = $filesFinder;
     }
 
     public function resolveExtensionKey(File $file): ?string
     {
+        if (! $this->filesFinder->isExtLocalConf($file->getFilePath())
+            && ! $this->filesFinder->isExtTables($file->getFilePath())
+        ) {
+            return null;
+        }
+
         $directoryName = dirname($file->getFilePath());
+
         $composerJsonFile = $directoryName . '/composer.json';
 
         if (! $this->filesystem->fileExists($composerJsonFile)) {
-            return null;
+            return $directoryName;
         }
 
         $composerJsonContent = $this->filesystem->read($composerJsonFile);
