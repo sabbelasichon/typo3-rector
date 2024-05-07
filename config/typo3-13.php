47260a090f5fe462a9378e4a9f45095246d2d239
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-05-08 00:22:38 +0200

    [FEATURE] Add MigrateGeneralUtilityHmacToHashServiceHmacRector
    Resolves #4248

--- config/typo3-13.php
+++ config/typo3-13.php
@@ -10,4 +10,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/v13/tca-130.php');
     $rectorConfig->import(__DIR__ . '/v13/typo3-130.php');
     $rectorConfig->import(__DIR__ . '/v13/typo3-130-extbase-hash-service-core-hash-service.php');
+    $rectorConfig->import(__DIR__ . '/v13/typo3-131.php');
 };
--- stubs/TYPO3/CMS/Core/Crypto/HashService.php
+++ stubs/TYPO3/CMS/Core/Crypto/HashService.php
@@ -1,28 +1,32 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TYPO3\CMS\Core\Crypto;
 
-if(class_exists('TYPO3\CMS\Core\Crypto\HashService')) {
+if (class_exists('TYPO3\CMS\Core\Crypto\HashService')) {
     return;
 }
 
 final class HashService
 {
-    public function hmac(string $input, string $additionalSecret): void
+    public function hmac(string $input, string $additionalSecret): string
     {
+        return '';
     }
 
-    public function appendHmac(string $string, string $additionalSecret): void
+    public function appendHmac(string $string, string $additionalSecret): string
     {
+        return '';
     }
 
-    public function validateHmac(string $string, string $additionalSecret, string $hmac): void
+    public function validateHmac(string $string, string $additionalSecret, string $hmac): bool
     {
+        return false;
     }
 
-    public function validateAndStripHmac(string $string, string $additionalSecret): void
+    public function validateAndStripHmac(string $string, string $additionalSecret): string
     {
-
+        return '';
     }
 }
