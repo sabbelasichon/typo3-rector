9c49c66ea52412fd039b8c1a826b8c804d7999f8
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-11-23 14:50:41 +0100

    [BUGFIX] Check if property and param names match and rename if not
    Fixes: #4447

--- rules/CodeQuality/General/InjectMethodToConstructorInjectionRector.php
+++ rules/CodeQuality/General/InjectMethodToConstructorInjectionRector.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Ssch\TYPO3Rector\CodeQuality\General;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
 use PHPStan\Type\ObjectType;
 use Rector\NodeManipulator\ClassDependencyManipulator;
@@ -96,7 +99,6 @@ CODE_SAMPLE
             $node->getMethods(),
             static fn ($classMethod) => str_starts_with((string) $classMethod->name, 'inject')
         );
-
         if ($injectMethods === []) {
             return null;
         }
@@ -107,19 +109,35 @@ CODE_SAMPLE
                 continue;
             }
 
-            reset($params);
-
             /** @var Param $param */
             $param = current($params);
-
             if (! $param->type instanceof FullyQualified) {
                 continue;
             }
 
             $paramName = $this->getName($param->var);
-
             if ($paramName === null) {
                 continue;
+            }
+
+            if (isset($injectMethod->stmts[0]) && $injectMethod->stmts[0] instanceof Expression) {
+                // check for the property name and if they match
+                $statement = $injectMethod->stmts[0];
+
+                $assign = $statement->expr;
+                if (! $assign instanceof Assign) {
+                    continue;
+                }
+
+                $propertyFetch = $assign->var;
+                if (! $propertyFetch instanceof PropertyFetch) {
+                    continue;
+                }
+
+                $paramName = $this->getName($propertyFetch->name);
+                if ($paramName === null) {
+                    continue;
+                }
             }
 
             $this->classDependencyManipulator->addConstructorDependency(
