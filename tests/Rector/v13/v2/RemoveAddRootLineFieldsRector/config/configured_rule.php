02606e13fa2ae0c8661afb2054177f93b00aeacd
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-04-22 22:21:03 +0200

    [FEATURE] Add RemoveAddRootLineFieldsRector
    Resolves: #4305

--- tests/Rector/v13/v2/RemoveAddRootLineFieldsRector/config/configured_rule.php
+++ tests/Rector/v13/v2/RemoveAddRootLineFieldsRector/config/configured_rule.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\TYPO313\v2\MigrateRegularExpressionValidatorValidatorOptionErrorMessageRector;
 use Ssch\TYPO3Rector\TYPO313\v2\RemoveAddRootLineFieldsRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../config.php');
-    $rectorConfig->rule(MigrateRegularExpressionValidatorValidatorOptionErrorMessageRector::class);
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config_test.php');
     $rectorConfig->rule(RemoveAddRootLineFieldsRector::class);
 };
