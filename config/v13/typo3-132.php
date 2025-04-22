2d03f50780e350cc5375a2896302424f1e997e07
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-04-23 00:09:11 +0200

    [FEATURE] Add MigrateNamespacedShortHandValidatorRector
    Resolves: #4307

--- config/v13/typo3-132.php
+++ config/v13/typo3-132.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Ssch\TYPO3Rector\TYPO313\v2\MigrateNamespacedShortHandValidatorRector;
 use Ssch\TYPO3Rector\TYPO313\v2\MigrateRegularExpressionValidatorValidatorOptionErrorMessageRector;
 use Ssch\TYPO3Rector\TYPO313\v2\RemoveAddRootLineFieldsRector;
 
@@ -10,4 +11,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
     $rectorConfig->rule(MigrateRegularExpressionValidatorValidatorOptionErrorMessageRector::class);
     $rectorConfig->rule(RemoveAddRootLineFieldsRector::class);
+    $rectorConfig->rule(MigrateNamespacedShortHandValidatorRector::class);
 };
---
+++ rules/TYPO313/v2/MigrateNamespacedShortHandValidatorRector.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ssch\TYPO3Rector\TYPO313\v2;
+
+use PhpParser\Comment\Doc;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Property;
+use Rector\BetterPhpDocParser\PhpDoc\ArrayItemNode;
+use Rector\BetterPhpDocParser\PhpDoc\DoctrineAnnotationTagValueNode;
+use Rector\BetterPhpDocParser\PhpDoc\StringNode;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
+use Rector\BetterPhpDocParser\ValueObject\PhpDocAttributeKey;
+use Rector\Comments\NodeDocBlock\DocBlockUpdater;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/13.2/Deprecation-103965-DeprecateNamespacedShorthandValidatorUsageInExtbase.html
+ * @see \Ssch\TYPO3Rector\Tests\Rector\v13\v2\MigrateNamespacedShortHandValidatorRector\MigrateNamespacedShortHandValidatorRectorTest
+ */
+final class MigrateNamespacedShortHandValidatorRector extends AbstractRector implements DocumentedRuleInterface
+{
+    /**
+     * @readonly
+     */
+    private DocBlockUpdater $docBlockUpdater;
+
+    /**
+     * @readonly
+     */
+    private PhpDocInfoFactory $phpDocInfoFactory;
+
+    public function __construct(DocBlockUpdater $docBlockUpdater, PhpDocInfoFactory $phpDocInfoFactory)
+    {
+        $this->docBlockUpdater = $docBlockUpdater;
+        $this->phpDocInfoFactory = $phpDocInfoFactory;
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Migrate namespaced shorthand validator usage in Extbase', [new CodeSample(
+            <<<'CODE_SAMPLE'
+/**
+ * @Extbase\Validate("TYPO3.CMS.Extbase:NotEmpty")
+ */
+protected $myProperty1;
+
+/**
+ * @Extbase\Validate("Vendor.Extension:Custom")
+ */
+protected $myProperty2;
+CODE_SAMPLE
+            ,
+            <<<'CODE_SAMPLE'
+/**
+ * @Extbase\Validate("NotEmpty")
+ */
+protected $myProperty1;
+
+/**
+ * @Extbase\Validate("Vendor\Extension\Validation\Validator\CustomValidator")
+ */
+protected $myProperty2;
+CODE_SAMPLE
+        )]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Property::class];
+    }
+
+    /**
+     * @param Property $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->getDocComment() instanceof Doc) {
+            return null;
+        }
+
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNode($node);
+        if (! $phpDocInfo instanceof PhpDocInfo) {
+            return null;
+        }
+
+        $this->processAnnotations($phpDocInfo);
+
+        $this->docBlockUpdater->updateRefactoredNodeWithPhpDocInfo($node);
+
+        return $node;
+    }
+
+    private function processAnnotations(PhpDocInfo $phpDocInfo): void
+    {
+        if ($phpDocInfo->getPhpDocNode()->children === []) {
+            return;
+        }
+
+        $validateAnnotations = $phpDocInfo->getByAnnotationClass('TYPO3\CMS\Extbase\Annotation\Validate');
+        if (! $validateAnnotations instanceof DoctrineAnnotationTagValueNode) {
+            return;
+        }
+
+        foreach ($validateAnnotations->values as $validateAnnotation) {
+            if ($validateAnnotation->value === null || ! $validateAnnotation->value instanceof StringNode) {
+                continue;
+            }
+
+            $this->replaceAnnotation($validateAnnotation);
+        }
+    }
+
+    private function replaceAnnotation(ArrayItemNode $valueNode): void
+    {
+        /** @var StringNode $stringNode */
+        $stringNode = $valueNode->value;
+        $value = $stringNode->value;
+
+        if (str_starts_with($value, 'TYPO3.CMS.Extbase:')) {
+            $valueNode->value = '"' . str_replace('TYPO3.CMS.Extbase:', '', $stringNode->value) . '"';
+            $valueNode->setAttribute(PhpDocAttributeKey::ORIG_NODE, null);
+        } elseif (str_contains($value, '.')) {
+            $parts = explode(':', $value);
+            if (count($parts) === 2) {
+                [$packageKey, $validatorName] = $parts;
+                if (str_contains($packageKey, '.')) {
+                    [$vendor, $extension] = explode('.', $packageKey);
+                    $valueNode->value = '"' . $vendor . '\\' . $extension . '\\Validation\\Validator\\' . $validatorName . 'Validator' . '"';
+                    $valueNode->setAttribute(PhpDocAttributeKey::ORIG_NODE, null);
+                }
+            }
+        }
+    }
+}
