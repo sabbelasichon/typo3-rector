d936e08185e68a891a8f31c25f71aea2291f1a6c
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-04-29 18:18:23 +0200

    [FEATURE] Add ExtendExtbaseValidatorsFromAbstractValidatorRector
    Resolves: #4511

--- tests/Rector/v14/v0/ExtendExtbaseValidatorsFromAbstractValidatorRector/config/configured_rule.php
+++ tests/Rector/v14/v0/ExtendExtbaseValidatorsFromAbstractValidatorRector/config/configured_rule.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Ssch\TYPO3Rector\TYPO314\v0\DropFifthParameterForExtensionUtilityConfigurePluginRector;
 use Ssch\TYPO3Rector\TYPO314\v0\ExtendExtbaseValidatorsFromAbstractValidatorRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->import(__DIR__ . '/../config.php');
-    $rectorConfig->rule(DropFifthParameterForExtensionUtilityConfigurePluginRector::class);
+    $rectorConfig->import(__DIR__ . '/../../../../../../config/config_test.php');
     $rectorConfig->rule(ExtendExtbaseValidatorsFromAbstractValidatorRector::class);
 };
