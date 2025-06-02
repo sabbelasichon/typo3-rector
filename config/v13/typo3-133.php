9f1948210b62999ef51b332fb2d34a723a5adaff
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-06-03 01:13:56 +0200

    [BUGFIX] Add MigrateTypoScriptFrontendControllerAddCacheTagsAndGetPageCacheTagsRector
    Resolves: #4332

--- config/v13/typo3-133.php
+++ config/v13/typo3-133.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\TYPO313\v3\MigrateFluidStandaloneMethodsRector;
+use Ssch\TYPO3Rector\TYPO313\v3\MigrateTypoScriptFrontendControllerAddCacheTagsAndGetPageCacheTagsRector;
 use Ssch\TYPO3Rector\TYPO313\v3\MigrateViewHelperRenderStaticRector;
 use Ssch\TYPO3Rector\TYPO313\v3\UseTYPO3CoreViewInterfaceInExtbaseRector;
 
@@ -12,4 +13,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(MigrateViewHelperRenderStaticRector::class);
     $rectorConfig->rule(UseTYPO3CoreViewInterfaceInExtbaseRector::class);
     $rectorConfig->rule(MigrateFluidStandaloneMethodsRector::class);
+    $rectorConfig->rule(MigrateTypoScriptFrontendControllerAddCacheTagsAndGetPageCacheTagsRector::class);
 };
