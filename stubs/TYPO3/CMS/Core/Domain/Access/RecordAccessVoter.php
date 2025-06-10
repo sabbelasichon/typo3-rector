4050593050f30f41c5dc90dd4bc93e5076b1d8f7
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-06-10 19:30:08 +0200

    [TASK] Add check if class exists

--- stubs/TYPO3/CMS/Core/Domain/Access/RecordAccessVoter.php
+++ stubs/TYPO3/CMS/Core/Domain/Access/RecordAccessVoter.php
@@ -4,6 +4,10 @@ namespace TYPO3\CMS\Core\Domain\Access\RecordAccessVoter;
 
 use TYPO3\CMS\Core\Context\Context;
 
+if (class_exists('TYPO3\CMS\Core\Domain\Access\RecordAccessVoter\RecordAccessVoter')) {
+    return;
+}
+
 class RecordAccessVoter
 {
     public function accessGranted(string $table, array $record, Context $context): void
