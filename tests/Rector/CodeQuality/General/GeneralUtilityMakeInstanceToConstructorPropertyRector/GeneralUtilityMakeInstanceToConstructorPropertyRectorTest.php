3fe0d2035fcba73db4b7beb03c8015dd5dac88d1
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-06-14 17:55:32 +0200

    [BUGFIX] Fix GeneralUtilityMakeInstanceToConstructorPropertyRector

--- tests/Rector/CodeQuality/General/GeneralUtilityMakeInstanceToConstructorPropertyRector/GeneralUtilityMakeInstanceToConstructorPropertyRectorTest.php
+++ tests/Rector/CodeQuality/General/GeneralUtilityMakeInstanceToConstructorPropertyRector/GeneralUtilityMakeInstanceToConstructorPropertyRectorTest.php
@@ -10,23 +10,63 @@ use Rector\ValueObject\PhpVersionFeature;
 final class GeneralUtilityMakeInstanceToConstructorPropertyRectorTest extends AbstractRectorTestCase
 {
     /**
-     * @dataProvider provideData()
+     * @dataProvider providePHP74Data()
      */
-    public function test(string $filePath): void
+    public function testPhp74(string $filePath): void
     {
+        if (PHP_VERSION_ID >= PhpVersionFeature::PROPERTY_PROMOTION) {
+            $this->markTestSkipped('Do not execute');
+        }
+
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @dataProvider providePHP80Data()
+     */
+    public function testPhp80(string $filePath): void
+    {
+        if (PHP_VERSION_ID < PhpVersionFeature::PROPERTY_PROMOTION || PHP_VERSION_ID >= PhpVersionFeature::READONLY_PROPERTY) {
+            $this->markTestSkipped('Do not execute');
+        }
+
+        $this->doTestFile($filePath);
+    }
+
+    /**
+     * @dataProvider providePHP81Data()
+     */
+    public function testPhp81(string $filePath): void
+    {
+        if (PHP_VERSION_ID < PhpVersionFeature::READONLY_PROPERTY) {
+            $this->markTestSkipped('Do not execute');
+        }
+
         $this->doTestFile($filePath);
     }
 
     /**
      * @return \Iterator<array<string>>
      */
-    public static function provideData(): \Iterator
+    public static function providePHP74Data(): \Iterator
     {
-        if (PHP_VERSION >= PhpVersionFeature::PROPERTY_PROMOTION) {
-            return self::yieldFilesFromDirectory(__DIR__ . '/Fixture/PHP80');
-        }
-
         return self::yieldFilesFromDirectory(__DIR__ . '/Fixture/PHP74');
+    }
+
+    /**
+     * @return \Iterator<array<string>>
+     */
+    public static function providePHP80Data(): \Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture/PHP80');
+    }
+
+    /**
+     * @return \Iterator<array<string>>
+     */
+    public static function providePHP81Data(): \Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture/PHP81');
     }
 
     public function provideConfigFilePath(): string
