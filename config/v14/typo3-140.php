14cd9119f6125bcc9175970578fc8abde5fbeb2b
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-10-01 18:31:50 +0200

    [FEATURE] Add RemoveParameterInAuthenticationServiceRector
    Resolves: #4588

--- config/v14/typo3-140.php
+++ config/v14/typo3-140.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
+use PHPStan\Type\BooleanType;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\UnionType;
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 use Ssch\TYPO3Rector\TYPO314\v0\DropFifthParameterForExtensionUtilityConfigurePluginRector;
 use Ssch\TYPO3Rector\TYPO314\v0\ExtendExtbaseValidatorsFromAbstractValidatorRector;
 use Ssch\TYPO3Rector\TYPO314\v0\MigrateEnvironmentGetComposerRootPathRector;
 use Ssch\TYPO3Rector\TYPO314\v0\MigrateObsoleteCharsetInSanitizeFileNameRector;
+use Ssch\TYPO3Rector\TYPO314\v0\RemoveParameterInAuthenticationServiceRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
@@ -14,4 +20,11 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ExtendExtbaseValidatorsFromAbstractValidatorRector::class);
     $rectorConfig->rule(MigrateEnvironmentGetComposerRootPathRector::class);
     $rectorConfig->rule(MigrateObsoleteCharsetInSanitizeFileNameRector::class);
+    $rectorConfig->rule(RemoveParameterInAuthenticationServiceRector::class);
+    $rectorConfig->ruleWithConfiguration(
+        AddReturnTypeDeclarationRector::class,
+        [
+            new AddReturnTypeDeclaration('TYPO3\CMS\Core\Authentication\AuthenticationService', 'processLoginData', new UnionType([new BooleanType(), new IntegerType()])),
+        ]
+    );
 };
--- ecs.php
+++ ecs.php
@@ -37,7 +37,10 @@ return static function (ECSConfig $ecsConfig): void {
         __DIR__ . '/tests/Rector/v12/v0/MigrateBackendModuleRegistrationRector/Assertions/install/Configuration/Backend/Modules.php',
         AssignmentInConditionSniff::class,
         DeclareStrictTypesFixer::class => ['*/Fixture/*', '*/Assertions/*'],
-        LineLengthFixer::class => [__DIR__ . '/config/v13/strict-types.php'],
+        LineLengthFixer::class => [
+            __DIR__ . '/config/v13/strict-types.php',
+            __DIR__ . '/config/v14/typo3-140.php',
+        ],
     ]);
 
     $ecsConfig->sets([SetList::PSR_12, SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE]);
