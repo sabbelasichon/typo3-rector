de412ed0f727c2d89bc825f647208dbf4f5f9ac6
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-10-02 14:05:16 +0200

    [FEATURE] Add RemoveFieldSearchConfigOptionsRector
    Resolves: #4619

--- config/v14/tca-140.php
+++ config/v14/tca-140.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\TYPO314\v0\RemoveEvalYearFlagRector;
+use Ssch\TYPO3Rector\TYPO314\v0\RemoveFieldSearchConfigOptionsRector;
 use Ssch\TYPO3Rector\TYPO314\v0\RemoveIsStaticControlOptionRector;
 use Ssch\TYPO3Rector\TYPO314\v0\RemoveMaxDBListItemsRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
-    $rectorConfig->rule(RemoveMaxDBListItemsRector::class);
-    $rectorConfig->rule(RemoveIsStaticControlOptionRector::class);
     $rectorConfig->rule(RemoveEvalYearFlagRector::class);
+    $rectorConfig->rule(RemoveFieldSearchConfigOptionsRector::class);
+    $rectorConfig->rule(RemoveIsStaticControlOptionRector::class);
+    $rectorConfig->rule(RemoveMaxDBListItemsRector::class);
 };
