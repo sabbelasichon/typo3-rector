51f9278abf4a40683aad6fbc6b4312d64ccf8b3e
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2026-01-14 16:26:09 +0100

    [RELEASE] Release TYPO3 Rector 3.11.0

--- bin/typo3-init.php
+++ bin/typo3-init.php
@@ -29,7 +29,7 @@ foreach ($autoloadPaths as $path) {
 
 (new SingleCommandApplication())
     ->setName('Initialize TYPO3-Rector configuration')
-    ->setVersion('3.10.2')
+    ->setVersion('3.11.0')
     ->setDescription('Initializes a bare configuration to start with your TYPO3 upgrade')
     ->setCode(static function (InputInterface $input, OutputInterface $output): int {
         $projectDirectory = getcwd();
