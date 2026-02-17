60f1ec924e97ff4de05a2ce1726ac0a4e5abe26c
Philipp Kitzberger <coding@kitze.net>
2026-02-17 11:31:04 +0100

    [FEATURE] Add MigrateEvaluateConditionToVerdictInAbstractConditionViewHelperRector
    Resolves: #4827

--- config/v13/typo3-133.php
+++ config/v13/typo3-133.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Ssch\TYPO3Rector\TYPO313\v3\MigrateBackendUtilityGetTcaFieldConfigurationRector;
+use Ssch\TYPO3Rector\TYPO313\v3\MigrateEvaluateConditionToVerdictInAbstractConditionViewHelperRector;
 use Ssch\TYPO3Rector\TYPO313\v3\MigrateFluidStandaloneMethodsRector;
 use Ssch\TYPO3Rector\TYPO313\v3\MigrateTypoScriptFrontendControllerAddCacheTagsAndGetPageCacheTagsRector;
 use Ssch\TYPO3Rector\TYPO313\v3\MigrateViewHelperRenderStaticRector;
@@ -13,6 +14,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
     $rectorConfig->rule(MigrateViewHelperRenderStaticRector::class);
     $rectorConfig->rule(UseTYPO3CoreViewInterfaceInExtbaseRector::class);
+    $rectorConfig->rule(MigrateEvaluateConditionToVerdictInAbstractConditionViewHelperRector::class);
     $rectorConfig->rule(MigrateFluidStandaloneMethodsRector::class);
     $rectorConfig->rule(MigrateTypoScriptFrontendControllerAddCacheTagsAndGetPageCacheTagsRector::class);
     $rectorConfig->rule(MigrateBackendUtilityGetTcaFieldConfigurationRector::class);
