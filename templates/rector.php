0dfcfc69758f5d55b92fe1705ad210955a02d42c
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2026-08-31 11:42:35 +0200

    [TASK] Improve rector.php.dist

--- templates/rector.php
+++ templates/rector.php
@@ -4,15 +4,11 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\ValueObject\PhpVersion;
-use Ssch\TYPO3Rector\Set\Typo3SetList;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
-        Typo3SetList::TYPO3_10,
-        Typo3SetList::TYPO3_11,
-        Typo3SetList::TYPO3_12,
-        Typo3SetList::TYPO3_13,
-        Typo3SetList::TYPO3_14,
+        Typo3LevelSetList::UP_TO_TYPO3_14,
     ]);
 
     // Define your target version which you want to support
